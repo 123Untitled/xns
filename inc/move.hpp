21f59161a25954009d62ea553dff12732372cf61
123Untitled <123untitled@pm.me>
2023-01-27 22:11:52 +0100

    add move.hpp

---
+++ inc/move.hpp
@@ -0,0 +1,25 @@
+#ifndef UTILITY_HEADER
+#define UTILITY_HEADER
+
+#include "type_traits.hpp"
+
+// -- N A M E S P A C E -------------------------------------------------------
+
+namespace Xf {
+
+
+	// -- M O V E -------------------------------------------------------------
+
+	/* move */
+	template <typename T>
+	inline constexpr typename remove_reference<T>::type&& move(T&& obj) noexcept {
+		// remove reference from T and return as rvalue reference
+		return static_cast<typename remove_reference<T>::type&&>(obj);
+	}
+
+
+}
+
+
+
+#endif
