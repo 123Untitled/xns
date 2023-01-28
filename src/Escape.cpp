21f28b22085081ffcb733259291ea5c701edde11
123Untitled <123untitled@pm.me>
2023-01-28 08:22:34 +0100

    reformat class

--- src/Escape.cpp
+++ src/Escape.cpp
@@ -1,6 +1,6 @@
 #include "Escape.hpp"
 
-#include <iostream>
+
 bool Escape::requestCursorPosition(UInt32& x, UInt32& y) {
 
 	UInt32*	num		= &y;
@@ -27,11 +27,98 @@ bool Escape::requestCursorPosition(UInt32& x, UInt32& y) {
 
 
 
-SInt32 Escape::resetColor(void) {
-	return (write(STDOUT_FILENO, "\x1b[0m", 4));
+
+
+
+
+// -- M O V E -----------------------------------------------------------------
+
+/* move position */
+void Escape::move_position(UInt32 x, UInt32 y) {
+	UInt8		escape[ESCAPE_BUFFER_SIZE];
+	UInt32		ite;
+	// init iterator
+	ite = ESCAPE_BUFFER_SIZE - 1;
+	// last char
+	escape[ite] = 'H';
+	// integer to ascii X pos
+	while (x) {
+		escape[--ite] = ((x % BASE) ^ ZERO_ASCII);
+		x /= BASE;
+	} // separator char
+	escape[--ite] = ';';
+	// integer to ascii Y pos
+	while (y) {
+		escape[--ite] = ((y % BASE) ^ ZERO_ASCII);
+		y /= BASE;
+	} // ctrl char
+	escape[--ite] = '[';
+	// escape char
+	escape[--ite] = '\x1b';
+
+	// append escape sequence to buffer
+	Buffer::draw((char*)&escape[ite], ESCAPE_BUFFER_SIZE - ite);
 }
 
-Esc Escape::set24bitColor(const UInt32 color, const bool fore) {
+/* move left */
+void Escape::move_left(void) {
+	// append escape sequence to buffer
+	Buffer::draw(MOVE_LEFT, sizeof(MOVE_LEFT));
+}
+
+/* move right */
+void Escape::move_right(void) {
+	// append escape sequence to buffer
+	Buffer::draw(MOVE_RIGHT, sizeof(MOVE_RIGHT));
+}
+
+/* move home */
+void Escape::move_home(void) {
+	// append escape sequence to buffer
+	Buffer::draw(MOVE_HOME, sizeof(MOVE_HOME));
+}
+
+
+// -- E R A S E ---------------------------------------------------------------
+
+//SInt32 Escape::eraseFromCursorToEOL(void) {
+void Escape::eraseFromCursorToEOL(void) {
+	Buffer::draw(ERASE_FROM_CURSOR_TO_EOL, sizeof(ERASE_FROM_CURSOR_TO_EOL));
+	//return (write(STDOUT_FILENO, ERASE_FROM_CURSOR_TO_EOL, sizeof(ERASE_FROM_CURSOR_TO_EOL)));
+}
+
+/* erase line */
+void Escape::erase_line(void) {
+	// append escape sequence to buffer
+	Buffer::draw(ERASE_LINE, sizeof(ERASE_LINE));
+}
+
+
+// -- S C R E E N -------------------------------------------------------------
+
+/* enter screen */
+void Escape::enter_screen(void) {
+	// append escape sequence to buffer
+	Buffer::draw(ENTER_SCREEN, sizeof(ENTER_SCREEN));
+}
+
+/* exit screen */
+void Escape::exit_screen(void) {
+	// append escape sequence to buffer
+	Buffer::draw(EXIT_SCREEN, sizeof(EXIT_SCREEN));
+}
+
+/* clear screen */
+void Escape::clear_screen(void) {
+	// append escape sequence to buffer
+	Buffer::draw(ERASE_SCREEN, sizeof(ERASE_SCREEN));
+}
+
+
+// -- C O L O R ---------------------------------------------------------------
+
+/* color rgb */
+void Escape::color_rgb(const UInt32 color, const bool fore) {
 	// static 24bit color escape sequence
 	static UInt8	escape[] = {	'\x1b', '[',
 									' ', '8', ';', '2', ';',
@@ -60,180 +147,164 @@ Esc Escape::set24bitColor(const UInt32 color, const bool fore) {
 		escape[x + BLUE_OFFSET]		= ((b % BASE) ^ ZERO_ASCII);
 		// remove last digit
 		r /= BASE; g /= BASE; b /= BASE;
-	} // send escape sequence to standard output
-	//return (write(STDOUT_FILENO, escape, 19));
+	} // append escape sequence to buffer
+	Buffer::draw(escape, 19);
+}
 
-	return (Esc{escape, sizeof(escape)});
+/* reset style */
+void Escape::reset_style(void) {
+	// append escape sequence to buffer
+	Buffer::draw(RESET_STYLE, sizeof(RESET_STYLE));
 }
 
 
+// -- C U R S O R -------------------------------------------------------------
 
-SInt32 Escape::moveLeft(void) {
-	return (write(STDOUT_FILENO, "\x1b[1D", 4));
+/* show cursor */
+void Escape::show_cursor(void) {
+	// append escape sequence to buffer
+	Buffer::draw(SHOW_CURSOR, sizeof(SHOW_CURSOR));
 }
 
-SInt32 Escape::moveRight(void) {
-	return (write(STDOUT_FILENO, "\x1b[1C", 4));
-}
-
-SInt32 Escape::movePos(UInt32 x, UInt32 y) {
-	UInt8		escape[ESCAPE_BUFFER_SIZE];
-	UInt32		ite;
-	// init iterator
-	ite = ESCAPE_BUFFER_SIZE - 1;
-	// last char
-	escape[ite] = 'H';
-	// integer to ascii X pos
-	while (x) {
-		escape[--ite] = ((x % BASE) ^ ZERO_ASCII);
-		x /= BASE;
-	} // separator char
-	escape[--ite] = ';';
-	// integer to ascii Y pos
-	while (y) {
-		escape[--ite] = ((y % BASE) ^ ZERO_ASCII);
-		y /= BASE;
-	} // ctrl char
-	escape[--ite] = '[';
-	// escape char
-	escape[--ite] = '\x1b';
-	// send escape sequence to standard output
-	return (write(STDOUT_FILENO, &escape[ite], ESCAPE_BUFFER_SIZE - ite));
+/* hide cursor */
+void Escape::hide_cursor(void) {
+	// append escape sequence to buffer
+	Buffer::draw(NO_CURSOR, sizeof(NO_CURSOR));
 }
 
 
-Esc Escape::movePosEsc(UInt32 x = 0, UInt32 y = 0) {
-	#define BUFF_SIZE 128
-	// static iterator declaration
-	static UInt64	ite;
-	// static buffer declaration
-	static UInt8	escape[BUFF_SIZE + 1];
-	// constant characters initialization
-	//{ [BUFF_SIZE - 1] = 'H', [BUFF_SIZE] = '\0' };
-	escape[BUFF_SIZE - 1] = 'H';
-	escape[BUFF_SIZE] = '\0';
-	// init iterator
-	ite = BUFF_SIZE - 1;
-	// integer to ascii X pos
-	while (x) {
-		escape[--ite] = ((x % BASE) ^ ZERO_ASCII);
-		x /= BASE;
-	} // separator char
-	escape[--ite] = ';';
-	// integer to ascii Y pos
-	while (y) {
-		escape[--ite] = ((y % BASE) ^ ZERO_ASCII);
-		y /= BASE;
-	} // ctrl char
-	escape[--ite] = '[';
-	// escape char
-	escape[--ite] = '\x1b';
-	// return static buffer at start position
-	return (Esc{&escape[ite], BUFF_SIZE - ite});
+// -- C U R S O R  S T Y L E --------------------------------------------------
+
+/* cursor beam */
+void Escape::cursor_beam(void) {
+	// append escape sequence to buffer
+	Buffer::draw(CURSOR_BEAM, sizeof(CURSOR_BEAM));
 }
 
-
-
-// CURSOR //////////////////////////
-// make cursor invisible
-SInt32 Escape::noCursor(void) {
-	return (write(STDOUT_FILENO,
-			NO_CURSOR,
-			sizeof(NO_CURSOR)));
-}
-// make cursor visible
-SInt32 Escape::showCursor(void) {
-	return (write(STDOUT_FILENO,
-			SHOW_CURSOR,
-			sizeof(SHOW_CURSOR)));
+/* cursor underline */
+void Escape::cursor_underline(void) {
+	// append escape sequence to buffer
+	Buffer::draw(CURSOR_UNDERLINE, sizeof(CURSOR_UNDERLINE));
 }
 
-SInt32 Escape::cursorBeam(void) {
-	return (write(STDOUT_FILENO,
-			CURSOR_BEAM,
-			sizeof(CURSOR_BEAM)));
-}
-
-SInt32 Escape::cursorUnderline(void) {
-	return (write(STDOUT_FILENO,
-			CURSOR_UNDERLINE,
-			sizeof(CURSOR_UNDERLINE)));
-}
-
-SInt32 Escape::cursorBlock(void) {
-	return (write(STDOUT_FILENO,
-			CURSOR_BLOCK,
-			sizeof(CURSOR_BLOCK)));
-}
-
-SInt32 Escape::eraseFromCursorToEOL(void) {
-	return (write(STDOUT_FILENO,
-			ERASE_FROM_CURSOR_TO_EOL,
-			sizeof(ERASE_FROM_CURSOR_TO_EOL)));
-}
-
-SInt32 Escape::moveHome(void) {
-	return (write(STDOUT_FILENO,
-			MOVE_HOME,
-			sizeof(MOVE_HOME)));
-}
-
-SInt32 Escape::clearScreen(void) {
-	return (write(STDOUT_FILENO,
-			ERASE_SCREEN,
-			sizeof(ERASE_SCREEN)));
-}
-
-// enables the alternative buffer
-SInt32 Escape::enterScreen(void) {
-	return (write(STDOUT_FILENO,
-			ENTER_SCREEN,
-			sizeof(ENTER_SCREEN)));
-}
-
-// disables the alternative buffer
-SInt32 Escape::exitScreen(void) {
-	return (write(STDOUT_FILENO,
-			EXIT_SCREEN,
-			sizeof(EXIT_SCREEN)));
+/* cursor block */
+void Escape::cursor_block(void) {
+	// append escape sequence to buffer
+	Buffer::draw(CURSOR_BLOCK, sizeof(CURSOR_BLOCK));
 }
 
 
 
 
-Esc::Esc(void)
-: _ptr{nullptr}, _size{0} {
-
-}
-
-Esc::Esc(const UInt8* ptr, const UInt64 size)
-: _ptr{ptr}, _size{size} {
-
-}
-
-Esc::Esc(const Esc& copy)
-: _ptr{copy._ptr}, _size{copy._size} {
-
-}
-
-Esc::~Esc(void) {
-
-};
-
-Esc& Esc::operator=(const Esc& assign) {
-	_ptr	= assign._ptr;
-	_size	= assign._size;
-	return (*this);
-}
-
-const UInt8* Esc::ptr(void) const {
-	return (_ptr);
-}
-
-UInt64 Esc::size(void) const {
-	return (_size);
-}
 
 
 
 
+
+
+
+
+//bool checkEscape(const char c) {
+//	return (c == 27); }
+//
+//bool checkBracket(const char c) {
+//	return (c == '['); }
+//
+//bool checkSemicolon(const char c) {
+//	return (c == ';'); }
+//
+//bool checkEnd(const char c) {
+//	return (c == 'R'); }
+//
+//bool checkNumber(const char c) {
+//	return ((c ^ 48) < 10); }
+//
+//
+//bool Escape::requestCursorPosition(UInt& x, UInt& y) {
+//
+//	using Func = decltype(&checkEnd);
+//
+//	static Func funcs[4] = {
+//		checkEscape,
+//		checkBracket,
+//		checkSemicolon,
+//		checkEnd
+//	};
+//	std::string buff;
+//
+//	UInt*	num		= &y;
+//	char	c		= 0;
+//	UInt	i		= 0;
+//
+//	write(STDOUT_FILENO, REQUEST_CURSOR_POSITION "z",
+//		sizeof(REQUEST_CURSOR_POSITION)+1);
+//	x = y = 0;
+//	// iterate while input available
+//	/*while (read(STDIN_FILENO, &c, 1) > 0 && c != 'R') {
+//		buff.push_back(c);
+//		if (c == 27) c = 'E';
+//		std::cout << "readed: " <<  c << std::endl;
+//	}*/
+//	char bb[128 + 1];
+//	int readed = read(0, bb, 128);
+//	std::cout << "readed char: " << readed << std::endl;
+//
+//	bb[readed] = '\0';
+//
+//	buff.assign(bb);
+//
+//	for (size_t i = 0; i < buff.size(); ++i) {
+//
+//		char ch = buff[i];
+//
+//		if (ch == 27)
+//			write(1, "esc", 3);
+//		else {
+//			write(1, &ch, 1);
+//		}
+//	}
+//
+//	std::cout << "\n" << std::endl;
+//
+//		//if (funcs[i](c))
+//		//	i++;
+//
+//
+//
+//	return (true);
+//
+//
+//
+//
+//
+//
+//
+//		/*
+//
+//		if (escape) {
+//
+//			if (bracket) {
+//
+//				// convert character to integer
+//				if ((c ^ 48) < 10)	{ *num = (*num * 10) + (c - 48); }
+//
+//				// check separator character
+//				else if (c == ';')	{ num = &x; }
+//
+//				else if (c == 'R')	{ break; }
+//
+//				else				{ return (false); }
+//			}
+//
+//			// check control character
+//			else if (c == '[')	{ bracket = true; }
+//			else				{ return (false); }
+//		}
+//
+//		else if (c == 27)	{ escape = true;	}
+//		else				{ return (false);	}
+//		*/
+//
+//	//} // final check sequence
+//	//return (true);
+//}
