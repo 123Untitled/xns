0c4859a4508fed458880b539e46b9bc69ce11884
123Untitled <123untitled@pm.me>
2023-01-31 08:34:28 +0100

    rename include types.hpp

--- inc/Xfunc.hpp
+++ inc/Xfunc.hpp
@@ -4,10 +4,11 @@
 #include "Exec.hpp"
 #include "Directory.hpp"
 #include "Term.hpp"
-#include "Type.hpp"
+#include "Types.hpp"
 #include "Macro.hpp"
 #include "Color.hpp"
 #include "Escape.hpp"
 #include "ClassUtils.hpp"
+#include "File.hpp"
 
 #endif
