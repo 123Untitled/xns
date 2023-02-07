50ac141c955fb5231a71a8230c66c2fc92a3ebbb
123Untitled <123untitled@pm.me>
2023-02-07 15:40:22 +0100

    add loop methods

--- inc/Input.hpp
+++ inc/Input.hpp
@@ -3,77 +3,71 @@
 
 #include "Term.hpp"
 #include "Types.hpp"
+#include "event.hpp"
 
 #include <unistd.h>
 #include <string>
-#include <list>
 
-#define BUFFER_SIZE		256
 
-class Input {
-	public:
-	struct Observer;
-	enum class KeyEvent {
-		KEY_UP, KEY_DOWN, KEY_LEFT,
-		KEY_RIGHT, KEY_ESCAPE, KEY_ENTER,
-		EVNT_NBR
+// -- N A M E S P A C E -------------------------------------------------------
+
+namespace Xf {
+
+	// -- I N P U T  C L A S S ------------------------------------------------
+
+	class Input final {
+
+		public:
+
+			// -- C O N S T R U C T O R S -------------------------------------
+
+			/* non-instanciable class */
+			NON_INSTANCIABLE(Input);
+
+
+			// -- S T A T I C  P U B L I C  M E T H O D S ---------------------
+
+			/* start loop */
+			static void start_loop(void);
+
+			/* stop input loop */
+			static void stop_loop(void);
+
+
+			static void get_event(void);
+
+
+
+
+		private:
+
+			// -- S T A T I C  P R I V A T E  M E T H O D S -------------------
+
+			/* read input from stdin */
+			static SInt64 read_stdin(void);
+
+			static void process_input(void);
+
+			static void processEscape(void);
+			static void processArrow(void);
+
+
+			// -- P R I V A T E  E N U M S ------------------------------------
+
+			enum { BUFFER_SIZE = 1024 };
+
+
+			// -- S T A T I C  P R I V A T E  M E M B E R S -------------------
+
+			static std::string _input;
+			static char        _buff[BUFFER_SIZE + 1];
+			static SInt64      _readed;
+			static bool        _is_running;
+
 	};
 
-	public:
-	static void getEvent(void);
-	static void subscript(Observer* obs, const KeyEvent key);
-	// contructor / operator safe delete
-	Input(void)						= delete;
-	~Input(void)					= delete;
-	Input(const Input&)				= delete;
-	Input(Input&&)					= delete;
-	Input& operator=(const Input&)	= delete;
-	Input& operator=(Input&&)		= delete;
-
-	public:
-	struct Observer {
-		friend class Input;
-		// member functions array typedef
-		typedef void (Observer::*Keyfn)(void);
-		// default constructor
-		Observer(void);
-		// destructor
-		virtual ~Observer(void)					= 0;
-		// abstract member functions
-		virtual void onKeyUp(void)		{ /**/ };
-		virtual void onKeyDown(void)	{ /**/ };
-		virtual void onKeyLeft(void)	{ /**/ };
-		virtual void onKeyRight(void)	{ /**/ };
-		virtual void onKeyEscape(void)	{ /**/ };
-		virtual void onKeyEnter(void)	{ /**/ };
-		// static member functions array
-		static const Keyfn _keyfn[static_cast<UInt64>(KeyEvent::EVNT_NBR)];
-		// contructor / operator safe delete
-		Observer(const Observer&)				= delete;
-		Observer(Observer&& )					= delete;
-		Observer& operator=(const Observer&)	= delete;
-		Observer& operator=(Observer&& )		= delete;
-
-	};
-
-	private:
-	template<class T>
-	using List = std::list<T>;
-	typedef std::string String;
-
-	static String	_input;
-	static char		_buff[BUFFER_SIZE + 1];
-	static SInt64	_readed;
-
-	static List<Observer*> _observers[static_cast<UInt64>(KeyEvent::EVNT_NBR)];
-
-	static void callObservers(const KeyEvent key);
-
-
-	static SInt64 readInput(void);
-	static void processInput(void);
-	static void processEscape(void);
-	static void processArrow(void);
 };
+
+
 
 #endif
