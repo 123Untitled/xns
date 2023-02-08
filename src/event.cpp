10189c31eef1071dec4fe855aa9f786b90025c94
123Untitled <123untitled@pm.me>
2023-02-08 19:05:18 +0100

    remove mode, evntmode classes

--- src/event.cpp
+++ src/event.cpp
@@ -5,11 +5,8 @@
 
 /* default constructor */
 Xf::Event::Event(void)
-: _modes{}, _current{nullptr} {
-	// add default mode
-	add_mode("default");
-	// set default mode
-	set_mode("default");
+: _observers{}, _current{nullptr} {
+	// code here...
 }
 
 /* destructor */
@@ -34,13 +31,18 @@ void Xf::Event::add_mode(std::string&& name) {
 	// check invalid name
 	if (name.empty()) { return; }
 	// loop through all modes
-	for (Size x = 0; x < _modes.size(); ++x) {
+	for (Size x = 0; x < _observers.size(); ++x) {
 		// check mode already exists
-		if (_modes[x].name() == name) {
+		if (_observers[x]._first == name) {
 			// exit method
 			return;
 		} // add mode
-	} _modes.push_back(Xf::move(name));
+	}
+	_observers.emplace_back(Xf::move(Pair{
+				Xf::move(name), Xf::move(Obslist{})
+			}
+		)
+	);
 	// return new mode
 	return;
 }
@@ -48,11 +50,11 @@ void Xf::Event::add_mode(std::string&& name) {
 /* set mode by name */
 void Xf::Event::set_mode(const std::string& name) {
 	// loop through all modes
-	for (Size x = 0; x < _modes.size(); ++x) {
+	for (Size x = 0; x < _observers.size(); ++x) {
 		// check mode name
-		if (_modes[x].name() == name) {
+		if (_observers[x]._first == name) {
 			// set new current mode
-			_current = &_modes[x];
+			_current = &_observers[x]._second;
 			// exit method
 			return;
 		}
@@ -61,101 +63,86 @@ void Xf::Event::set_mode(const std::string& name) {
 
 
 /* subscribe to event */
-void Xf::Event::subscribe(const Evntype type, Evntfunc method, Object instance) {
+void Xf::Event::subscribe(const std::string& mode, const Evntype type, Evntfunc method, Object instance) {
 	// check invalid type and method pointer
-/*	if (type >= Evntype::EVNT_MAX || !method) { return; }
-	// allocate new observer
-	Observer* observer = new Observer(method, instance);
-	// set next observer
-	observer->next(head(type));
-	// set head observer
-	head(type, observer);*/
+	if (type >= Evntype::EVNT_MAX || !method) { return; }
+	// check invalid method
+	if (!method) { return; }
+	// get observer list
+	Xf::Vector<Observer>* obs = get_observers(mode, type);
+	// check observer list
+	if (!obs) { return; }
+	// create new observer
+	//obs->emplace_back(Observer(method, instance));
+	obs->emplace_back(method, instance);
+
 }
 
 /* unsubscribe to event */
-void Xf::Event::unsubscribe(const Evntype type, Evntfunc method, Object instance) {
+void Xf::Event::unsubscribe(const std::string& mode, const Evntype type, Evntfunc method, Object instance) {
 	// check invalid type
-/*	if (type >= Evntype::EVNT_MAX) { return; }
+	if (type >= Evntype::EVNT_MAX) { return; }
+	// get observer list
+	Xf::Vector<Observer>* obs = get_observers(mode, type);
+	// check observer list
+	if (!obs) { return; }
 	// create new observer to be compared
 	Observer search{method, instance};
-	// get observers indexed by type
-	Observer* observer = head(type);
-	// initialize previous observer
-	Observer* prev = nullptr;
 	// loop through all observers
-	while (observer) {
+	for (Size x = 0; x < obs->size(); ++x) {
 		// check observer
-		if (*observer != search) {
-			// get next observer
-			observer = observer->next();
-			// get previous observer
-			prev = observer;
-			// continue loop
-			continue;
-		} // check previous observer
-		if (prev != nullptr) {
-			// set previous observer's next
-			prev->next(observer->next());
-		} // else
-		else {
-			// set head observer
-			head(type, observer->next());
-		} // delete observer
-		delete observer;
-		// exit method
-		return;
-	}*/
+		if ((*obs)[x] == search) {
+			// remove observer
+			//obs->erase(x);
+			// exit method
+			return;
+		}
+	}
+
+
 }
 
 /* call all observers */
 void Xf::Event::call(const Evntype type) {
+	// check current mode
+	if (!_current) { return; }
 	// check invalid type
-/*	if (type >= Evntype::EVNT_MAX) { return; }
+	if (type >= Evntype::EVNT_MAX) { return; }
 	// get observers indexed by type
-	Observer* observer = head(type);
+	Xf::Vector<Observer>& evntobs = (*_current)[(IDX(type))];
 	// loop through all observers
-	while (observer) {
+	for (Size x = 0; x < evntobs.size(); ++x) {
 		// call observer
-		observer->call();
-		// get next observer
-		observer = observer->next();
-	}*/
-}
-
-/* clean up */
-void Xf::Event::clear(void) {
-	// loop through all events
-/*	for (Size x = 0; x < IDX(Evntype::EVNT_MAX); ++x) {
-		// loop through all observers
-		while (_observers[x]) {
-			// get next observer
-			Observer* next = _observers[x]->next();
-			// delete current observer
-			delete _observers[x];
-			// set next observer
-			_observers[x] = next;
-		}
-	}*/
+		evntobs[x].call();
+	}
 }
 
 
-// -- S T A T I C  P R I V A T E  A C C E S S O R S ---------------------------
+// -- P R I V A T E  M E T H O D S --------------------------------------------
 
-/* get head observer */ // NOTE: type is not checked
-/*Xf::Event::Observer* Xf::Event::head(const Evntype type) {
-	// return head observer
-	//return _observers[IDX(type)];
-	return nullptr;
-} */
+/* get mode */
+Xf::Event::Obslist* Xf::Event::get_mode(const std::string& name) {
+	// loop through all modes
+	for (Size x = 0; x < _observers.size(); ++x) {
+		// check mode name
+		if (_observers[x]._first == name) {
+			// return mode
+			return &_observers[x]._second;
+		} // return null
+	} return nullptr;
+}
 
-
-// -- S T A T I C  P R I V A T E  M U T A T O R S -----------------------------
-
-/* set head observer */ // NOTE: type is not checked
-/*void Xf::Event::head(const Evntype type, Observer* observer) {
-	// set head observer
-	//_observers[IDX(type)] = observer;
-} */
+/* get observer list */
+Xf::Vector<Xf::Event::Observer>* Xf::Event::get_observers(const std::string& mode, const Evntype type) {
+	// check invalid type
+	if (type >= Evntype::EVNT_MAX) { return nullptr; }
+	// get mode
+	Obslist* obslist = get_mode(mode);
+	// check mode
+	if (!obslist) { return nullptr; }
+	// return observers
+	return &obslist->idx(type);
+}
 
 
 // -- S T A T I C  P R I V A T E  M E M B E R S -------------------------------
@@ -163,90 +150,6 @@ void Xf::Event::clear(void) {
 /* singleton instance */
 Xf::Event Xf::Event::_instance;
 
-/* observers list */
-//Xf::Event::Observer* Xf::Event::_observers[IDX(Evntype::EVNT_MAX)] = { nullptr };
-
-
-
-
-
-
-
-// -- M O D E  N E S T E D  C L A S S -----------------------------------------
-
-
-/* name copy constructor */
-Xf::Event::Mode::Mode(const std::string& name)
-: _name{name}, _events{} {
-	// code here...
-}
-
-/* name move constructor */
-Xf::Event::Mode::Mode(std::string&& name) noexcept
-: _name{Xf::move(name)}, _events{} {
-	// code here...
-}
-
-/* copy constructor */
-Xf::Event::Mode::Mode(const Mode& other)
-: _name{other._name}, _events{other._events} {
-	// code here...
-}
-
-/* move constructor */
-Xf::Event::Mode::Mode(Mode&& other) noexcept
-: _name{Xf::move(other._name)}, _events{Xf::move(other._events)} {
-	// code here...
-}
-
-/* destructor */
-Xf::Event::Mode::~Mode(void) {
-	// code here...
-}
-
-
-// -- O P E R A T O R S -------------------------------------------------------
-
-/* copy operator */
-Xf::Event::Mode& Xf::Event::Mode::operator=(const Mode& other) {
-	// check for self assignment
-	if (this != &other) {
-		// copy name
-		_name = other._name;
-		// copy events
-		_events = other._events;
-	} // return self reference
-	return *this;
-}
-
-/* move operator */
-Xf::Event::Mode& Xf::Event::Mode::operator=(Mode&& other) {
-	// check for self assignment
-	if (this != &other) {
-		// move name
-		_name = Xf::move(other._name);
-		// move events
-		_events = Xf::move(other._events);
-	} // return self reference
-	return *this;
-}
-
-
-// -- A C C E S S O R S -------------------------------------------------------
-
-/* get name */
-const std::string& Xf::Event::Mode::name(void) const {
-	// return name
-	return _name;
-}
-
-/* subscribe */
-void Xf::Event::Mode::subscribe(const Evntype type, Evntfunc method, Object instance) {
-}
-
-/* unsubscribe */
-void Xf::Event::Mode::unsubscribe(const Evntype type, Evntfunc method, Object instance) {
-}
 
 
 
@@ -265,6 +168,21 @@ Xf::Event::Observer::Observer(Evntfunc method, Object instance)
 	// code here...
 }
 
+/* copy constructor */
+Xf::Event::Observer::Observer(const Observer& other)
+// initializations
+: _method{other._method}, _instance{other._instance} {
+	// code here...
+}
+
+/* move constructor */
+Xf::Event::Observer::Observer(Observer&& other) noexcept
+// initializations
+: Observer{other} {
+	// code here...
+}
+
+
 /* destructor */
 Xf::Event::Observer::~Observer(void) {
 	// code here...
@@ -272,6 +190,22 @@ Xf::Event::Observer::~Observer(void) {
 
 
 // -- O P E R A T O R S -------------------------------------------------------
+
+/* copy operator */
+Xf::Event::Observer& Xf::Event::Observer::operator=(const Observer& other) {
+	// copy method pointer
+	_method = other._method;
+	// copy instance pointer
+	_instance = other._instance;
+	// return this
+	return *this;
+}
+
+/* move operator */
+Xf::Event::Observer& Xf::Event::Observer::operator=(Observer&& other) noexcept {
+	// call copy operator
+	return operator=(other);
+}
 
 /* equality operator */
 bool Xf::Event::Observer::operator==(const Observer& other) const {
@@ -285,6 +219,7 @@ bool Xf::Event::Observer::operator==(const Observer& other) const {
 /* call */
 void Xf::Event::Observer::call(void) {
 	// call method on instance
+//	if (!_method) { std::cout << "method is null" << std::endl; return;}
 	_method(_instance);
 }
 
