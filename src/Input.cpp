02af8fb6595f3eaadc054e40cd1aea3e6e6893aa
123Untitled <123untitled@pm.me>
2023-02-13 02:49:46 +0100

    improve dispatch method, add some characters filters

--- src/Input.cpp
+++ src/Input.cpp
@@ -3,35 +3,13 @@
 
 // -- S T A T I C  P R I V A T E  M E M B E R S -------------------------------
 
-SInt64      Xf::Input::_readed                = 0;
-char        Xf::Input::_buff[BUFFER_SIZE + 1] = { 0 };
-std::string Xf::Input::_input                 = "";
-bool        Xf::Input::_is_running            = false;
+Xf::Input::Readed Xf::Input::_readed                = 0;
+Xf::Input::Char   Xf::Input::_buff[BUFFER_SIZE + 1] = { 0 };
+Xf::Input::String Xf::Input::_input                 = "";
+bool              Xf::Input::_is_running            = false;
 
 
 // -- S T A T I C  P U B L I C  M E T H O D S ---------------------------------
-
-void Xf::Input::get_event(void) {
-	// reset string
-	_input.clear();
-	// read stdin
-	if (read_stdin() > 0) {
-		// append buffer to the string
-		_input.append(_buff, _readed);
-		// buffer is full, loop over reading
-		if (_readed == BUFFER_SIZE) {
-			// set to non-blocking read
-			Term::setRaw(VFlag::NON_BLOCKING);
-			// read stdin again
-			while (read_stdin() > 0) {
-				// append buffer to the string
-				_input.append(_buff, _readed);
-			} // back to blocking read
-			Term::setRaw(VFlag::BLOCKING);
-		} // analyze readed bytes
-		//processInput();
-	}
-}
 
 /* input loop */
 void Xf::Input::start_loop(void) {
@@ -40,9 +18,11 @@ void Xf::Input::start_loop(void) {
 	// loop over reading
 	while (_is_running) {
 		// read stdin
-		get_event();
+		read_input();
+		// filter input
+		filter_extended();
 		// analyze readed bytes
-		process_input();
+		dispatch();
 	}
 }
 
@@ -55,6 +35,33 @@ void Xf::Input::stop_loop(void) {
 
 // -- S T A T I C  P R I V A T E  M E T H O D S -------------------------------
 
+/* read input while available */
+void Xf::Input::read_input(void) {
+
+	// reset previous readed bytes
+	_input.clear();
+
+	// read stdin
+	if (read_stdin() > 0) {
+
+		// append buffer to the string
+		_input.append(_buff, _readed);
+
+		// buffer is full, loop over reading
+		if (_readed == BUFFER_SIZE) {
+			// set to non-blocking read
+			Term::setRaw(VFlag::NON_BLOCKING);
+			// read stdin again
+			while (read_stdin() > 0) {
+				// append buffer to the string
+				_input.append(_buff, _readed);
+			} // back to blocking read
+			Term::setRaw(VFlag::BLOCKING);
+		}
+
+	}
+}
+
 /* read stdin */
 SInt64 Xf::Input::read_stdin(void) {
 	// read bytes from stdin
@@ -65,66 +72,66 @@ SInt64 Xf::Input::read_stdin(void) {
 	return _readed;
 }
 
+/* filter extended ascii codes */
+void Xf::Input::filter_extended(void) {
+	// loop over input
+	for (Size x = 0; x < _input.length(); ++x) {
+		// check if input is superiour to 0x7f (127)
+		if (::String::is_multibyte(_input[x])) {
+			// remove charater
+			_input.erase(x, 1); }
+	}
+}
 
+/* filter control characters */
+void Xf::Input::filter_control(void) {
+	// loop over input
+	for (Size x = 0; x < _input.length(); ++x) {
+		// check if input is in control range
+		if (::String::is_control(_input[x])) {
+			// remove charater
+			_input.erase(x, 1); }
+	}
+}
 
-
-
-void Xf::Input::process_input(void) {
+void Xf::Input::dispatch(void) {
 
 	// get event instance
 	Xf::Event& evnt = Xf::Event::instance();
 
 	using Ev = Xf::Evntype;
 
-	if (_input.length() == 3) {
 
-		// check escape sequence
-		if (_input[0] == 0x1b) {
-			// check ctrl sequence
-			if (_input[1] == 0x5b) {
-				// check arrow sequence
-				switch (_input[2]) {
-					case 'A': // code for arrow up
-						evnt.call_event(Ev::UP);
-						break;
-					case 'B': // code for arrow down
-						evnt.call_event(Ev::DOWN);
-						break;
-					case 'C': // code for arrow right
-						evnt.call_event(Ev::RIGHT);
-						break;
-					case 'D': // code for arrow left
-						evnt.call_event(Ev::LEFT);
-						break;
-					default:
-						break;
-				}
+	if (_input.length() == 3 && _input[0] == 0x1b) {
+
+		// check ctrl sequence
+		if (_input[1] == 0x5b) {
+			// check arrow sequence
+			switch (_input[2]) {
+				case 'A': // code for arrow up
+					evnt.call_event(Ev::UP);
+					return;
+				case 'B': // code for arrow down
+					evnt.call_event(Ev::DOWN);
+					return;
+				case 'C': // code for arrow right
+					evnt.call_event(Ev::RIGHT);
+					return;
+				case 'D': // code for arrow left
+					evnt.call_event(Ev::LEFT);
+					return;
+				default:
+					break;
 			}
 		}
-
 	}
-	else if (_input.length() == 1) {
-
-		switch (_input[0]) {
-			case 0x1b:
-				evnt.call_event(Ev::ESCAPE);
-				break;
-			case 0x0a:
-				evnt.call_event(Ev::ENTER);
-				break;
-			case 0x7f:
-				evnt.call_event(Ev::BACKSPACE);
-				break;
-			case 0x09:
-				evnt.call_event(Ev::TAB);
-				break;
-			case 0x0d:
-				evnt.call_event(Ev::RETURN);
-				break;
-			default:
-				evnt.call_input(_input);
-				break;
-		}
+	if (_input.length() == 1) {
+		const UInt8 _char = _input[0];
+		evnt.call_event(static_cast<Ev>(_char));
 	}
+	filter_control();
+	if (_input.empty()) { return; }
+	evnt.call_input(_input);
 }
+
 
