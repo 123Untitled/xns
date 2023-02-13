7bd931c1664df1492f387d46a630793f28c48877
123Untitled <123untitled@pm.me>
2023-02-13 02:48:39 +0100

    start working on Ascii class

---
+++ src/ascii.hpp
@@ -0,0 +1,23 @@
+#include "Ascii.hpp"
+#include "array.hpp"
+
+//struct Element {
+//
+//	String base8;
+//	String base10;
+//	String base16;
+//	String symbol;
+//	String description;
+//
+//};
+
+
+// ascii table
+
+Xf::Array<Xf::Ascii::Element, UINT_MAX + 1> Ascii::_array = {
+
+	//{ "000", "000", "00", "NUL" },
+
+};
+
+
