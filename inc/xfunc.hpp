c7cc167b4ac9a69136144776b325279753961b43
123Untitled <123untitled@pm.me>
2023-02-17 00:18:28 +0100

    append some includes

--- inc/xfunc.hpp
+++ inc/xfunc.hpp
@@ -1,14 +1,37 @@
 #ifndef MYLIB_HEADER
 #define MYLIB_HEADER
 
-#include "Exec.hpp"
-#include "Directory.hpp"
-#include "Term.hpp"
-#include "Types.hpp"
-#include "Macro.hpp"
-#include "Color.hpp"
-#include "Escape.hpp"
-#include "ClassUtils.hpp"
-#include "File.hpp"
+#include "terminal.hpp"
+#include "escape.hpp"
+#include "buffer.hpp"
+
+#include "string.hpp"
+#include "vector.hpp"
+#include "list.hpp"
+#include "stack.hpp"
+#include "array.hpp"
+
+#include "allocator.hpp"
+
+#include "input.hpp"
+#include "event.hpp"
+
+#include "function.hpp"
+
+#include "unique.hpp"
+
+#include "exec.hpp"
+
+#include "directory.hpp"
+#include "file.hpp"
+
+#include "types.hpp"
+#include "macro.hpp"
+
+#include "color.hpp"
+#include "window.hpp"
+#include "border.hpp"
+
+#include "testclass.hpp"
 
 #endif
