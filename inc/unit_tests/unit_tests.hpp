cb41ce914a6eb38083d4b2f6ba9980fff936c7d4
123Untitled <123untitled@pm.me>
2023-02-16 16:20:35 +0100

    add unit_tests.hpp

---
+++ inc/unit_tests/unit_tests.hpp
@@ -0,0 +1,22 @@
+#ifndef UNIT_TESTS_HPP
+#define UNIT_TESTS_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <climits>
+
+#include "string.hpp"
+
+namespace Xf {
+
+	namespace Tests {
+
+		void test_string(void);
+
+	};
+
+};
+
+
+#endif
