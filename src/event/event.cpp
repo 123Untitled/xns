91318326acee7cddedc08f60828cf4a4d8ea6d2d
123Untitled <123untitled@pm.me>
2023-02-16 16:16:38 +0100

    change mode system, now mode are remote by Evntmode class

--- src/event/event.cpp
+++ src/event/event.cpp
@@ -5,7 +5,7 @@
 
 /* default constructor */
 Xf::Event::Event(void)
-: _modes{ }, _current{nullptr}, _apply{nullptr} {
+: _modes{ }, _current{ }, _next{ } {
 	// code here...
 }
 
@@ -24,112 +24,125 @@ Xf::Event& Xf::Event::instance(void) {
 }
 
 
+// -- S T A T I C  P R I V A T E  M E M B E R S -------------------------------
+
+/* singleton instance */
+Xf::Event Xf::Event::_instance;
+
+
 // -- P U B L I C  M E T H O D S ----------------------------------------------
 
 /* add mode */
-void Xf::Event::add_mode(String&& name) {
-	// check invalid name
-	if (name.empty()) { return; }
+Xf::Evntmode Xf::Event::new_mode(void) {
+	_modes.emplace_back(Mode{ });
+	if (_modes.empty()) {
+		// error
+		Evntmode mode{0};
+		mode._state = false;
+		return mode;
+	}
+	return Evntmode{_modes.size() - 1};
+}
 
-	// check if mode already exists
-	if (mode_exists(name)) { return; }
+/* remove mode */
+void Xf::Event::remove_mode(Evntmode& mode) {
+	// check if index is valid
+	if (!mode._state) { return; }
+	// invalid index
+	mode._state = false;
+	//_modes.erase(mode._idx); WARNING: erase not implemented
+}
 
-	Size idx = 0;
-	// find index of current mode
-	if (_current) { idx = get_current_idx(); }
-
-	_modes.emplace_back(Pair{Xf::move(name), Mode{}});
-
-	// set new current mode
-	if (_current) {
-		// INFO: reason of this is when vector is resized,
-		// all pointers are invalidated
-		_current = &_modes[idx]._second;
+/* set mode */
+void Xf::Event::set_mode(const Evntmode& mode, const Evntopt opt) {
+	// check if index is valid
+	if (mode._state) {
+		// check if mode is forced
+		if (opt == Evntopt::FORCE) {
+			// stack current mode
+			_current.make(mode._idx);
+			return;
+		} // set next mode
+		_next.make(mode._idx);
 	}
 }
 
 /* apply mode */
-void Xf::Event::apply_mode(void) {
-
+void Xf::Event::next_mode(void) {
 	// check if there is a mode query
-	if (!_apply) { return; }
-
-	// set current mode
-	_current = _apply;
-
-	// reset apply mode
-	_apply = nullptr;
+	if (!_next) { return; }
+	// mode to current mode
+	_current = Xf::move(_next);
 }
 
-
-/* set mode by name */
-bool Xf::Event::set_mode(const String& name) {
-
-	// loop through all modes
-	for (Size x = 0; x < _modes.size(); ++x) {
-
-		// check mode name
-		if (_modes[x]._first == name) {
-			// set apply mode
-			_apply = &_modes[x]._second;
-			// exit method
-			return true;
-		}
-	}
-
-	// mode not found
-	return false;
+/* is mode active */
+bool Xf::Event::is_mode(void) const {
+	// check if there is a current mode
+	return _current ? true : false;
 }
 
-/* [PRIVATE] mode exists */
-bool Xf::Event::mode_exists(const std::string& mode) const {
-	// loop through all modes
-	for (Size x = 0; x < _modes.size(); ++x) {
-		// compare mode name
-		if (_modes[x]._first == mode) {
-			// exit method
-			return true; }
-	} // mode not found
-	return false;
+/* is mode active */
+bool Xf::Event::is_mode(const Evntmode& mode) const {
+	// check if there is a current mode and mode is valid
+	if (!_current || !mode._state) { return false; }
+	// check if mode is active
+	return *_current == mode._idx ? true : false;
 }
 
-/* [PRIVATE] get current mode index */
-Size Xf::Event::get_current_idx(void) const {
-	// loop through all modes
-	for (Size x = 0; x < _modes.size(); ++x) {
-		// check mode name
-		if (&_modes[x]._second == _current) {
-			// exit method
-			return x; }
-	} // else return 0
-	return 0;
+/* stack current mode */
+void Xf::Event::stack_mode(void) {
+	// check if there is a current mode
+	if (!_current) { return; }
+	// stack current mode
+	_stack.emplace(*_current);
 }
+
+/* stack mode */
+void Xf::Event::stack_mode(const Evntmode& mode, const Evntopt opt) {
+	// check if mode is valid
+	if (!mode._state) { return; }
+	// stack mode
+	_stack.emplace(mode._idx);
+	// check if mode is forced
+	if (opt == Evntopt::FORCE) {
+		// set next mode
+		_current.make(mode._idx);
+		return;
+	} // set next mode
+	_next.make(mode._idx);
+}
+
+/* unstack mode */
+void Xf::Event::unstack_mode(void) {
+	// check if there is a mode to unstack
+	if (_stack.empty()) { return; }
+	// unstack mode
+	_next.make(*_stack.top());
+	// remove mode from stack
+	_stack.pop();
+}
+
 
 
 /* call all event subscribers */
 void Xf::Event::call_event(const Evntype type) {
-	// exit if no current mode
-	if (!_current) { return; }
-	// check event type
-	if (type < Evntype::EVNT_MAX) {
-		// get subscribers index by event type
-		EventVector& subscribers = _current->_second[IDX(type)];
-		// loop through all observers
-		for (Size x = 0; x < subscribers.size(); ++x) {
-
-			// INFO: need to overload call for PolyMethod instance with no arguments (void)
-			// call subscriber
-			subscribers[x].call();
-		}
+	// exit if no current mode or invalid event type
+	if (!_current || type >= Evntype::EVNT_MAX) { return; }
+	// get subscribers index by event type
+	EventVector& subscribers = _modes[*_current]._second[IDX(type)];
+	// loop through all observers
+	for (Size x = 0; x < subscribers.size(); ++x) {
+		// call subscriber
+		subscribers[x].call();
 	}
 }
 
 /* call all input subscribers */
-void Xf::Event::call_input(const String& input) {
+void Xf::Event::call_input(const Xf::String<char>& input) {
 	// exit if no current mode
 	if (!_current) { return; }
 	// get subscribers
-	InputVector& subscribers = _current->_first;
+	InputVector& subscribers = _modes[*_current]._first;
 	// loop through all observers
 	for (Size x = 0; x < subscribers.size(); ++x) {
 		// call subscriber
@@ -137,63 +150,97 @@ void Xf::Event::call_input(const String& input) {
 	}
 }
 
-/* subscribe to event [function] */
-void Xf::Event::subscribe_event(const String& mode, const Evntype type, EventFunction method) {
-	// check invalid pointers
-	if (!method) { return; }
-	// check event type
-	if (type < Evntype::EVNT_MAX) {
-		// get event subscriber vector
-		EventVector* subscribers = get_event_subscribers(mode, type);
-		// check pointer
-		if (!subscribers) { return; }
-		// add new subscriber
-		subscribers->emplace_back(method);
-	}
-}
 
 // -- P R I V A T E  M E T H O D S --------------------------------------------
 
-/* get mode */
-Xf::Event::Mode* Xf::Event::get_mode(const String& name) {
-	// loop through all modes
-	for (Size x = 0; x < _modes.size(); ++x) {
-		// check mode name
-		if (_modes[x]._first == name) {
-			// return mode
-			return &_modes[x]._second;
-		}
-	} return nullptr;
+/* subscribe function to event */
+void Xf::Event::_subscribe(const Evntmode& mode, const Evntype type, EventFunction function) {
+	// check invalid pointers and event type
+	if (!function || type >= Evntype::EVNT_MAX) { return; }
+	// get event subscriber vector
+	EventVector& subscribers = _modes[mode._idx]._second[IDX(type)];
+	// add new subscriber
+	subscribers.emplace_back(function);
 }
 
-/* get event subscribers */
-Xf::Event::EventVector* Xf::Event::get_event_subscribers(const String& mode, const Evntype type) {
-	// get mode
-	Mode* m = get_mode(mode);
-	// check mode
-	if (!m) { return nullptr; }
-	// check event type
-	if (type < Evntype::EVNT_MAX) {
-		// return subscribers
-		return &m->_second[IDX(type)];
-	} return nullptr;
-}
-
-/* get input subscribers */
-Xf::Event::InputVector* Xf::Event::get_input_subscribers(const String& mode) {
-	// get mode
-	Mode* m = get_mode(mode);
-	// check mode
-	if (!m) { return nullptr; }
-	// return subscribers
-	return &m->_first;
+/* subscribe function to input */
+void Xf::Event::_subscribe(const Evntmode& mode, InputFunction function) {
+	// check invalid pointers
+	if (!function) { return; }
+	// get input subscriber vector
+	InputVector& subscribers = _modes[mode._idx]._first;
+	// add new subscriber
+	subscribers.emplace_back(function);
 }
 
 
-// -- S T A T I C  P R I V A T E  M E M B E R S -------------------------------
 
-/* singleton instance */
-Xf::Event Xf::Event::_instance;
+
+
+
+//     _______  ________  ________  ________  ________  ________   _______  ________
+//   ╱╱       ╲╱    ╱   ╲╱    ╱   ╲╱        ╲╱        ╲╱        ╲_╱       ╲╱        ╲
+//  ╱╱        ╱         ╱         ╱        _╱         ╱         ╱         ╱         ╱
+// ╱        _╱╲        ╱         ╱╱       ╱╱         ╱         ╱         ╱        _╱
+// ╲________╱  ╲______╱╲__╱_____╱ ╲______╱ ╲__╱__╱__╱╲________╱╲________╱╲________╱
+
+
+/* index private constructor */
+Xf::Evntmode::Evntmode(const Size idx)
+: _idx{idx}, _state{true} {
+	// code here...
+}
+
+/* destructor */
+Xf::Evntmode::~Evntmode(void) {
+	// check if mode is active
+	if (_state) {
+		// remove mode from event manager
+		Xf::Event::instance().remove_mode(*this);
+	}
+}
+
+/* move constructor */
+Xf::Evntmode::Evntmode(Evntmode&& other) noexcept
+: _idx{other._idx}, _state{other._state} {
+	// invalidate other
+	other._state = false;
+}
+
+/* move assignment operator */
+Xf::Evntmode& Xf::Evntmode::operator=(Evntmode&& other) noexcept {
+	// check for self-assignment
+	if (this != &other) {
+		// move other
+		_idx   = other._idx;
+		_state = other._state;
+		// invalidate other
+		other._state = false;
+	} // return self reference
+	return *this;
+}
+
+
+// -- P U B L I C  M E T H O D S ----------------------------------------------
+
+/* subscribe function to event */
+void Xf::Evntmode::subscribe(const Evntype type, Event::EventFunction function) {
+	// check if mode is active
+	if (_state) {
+		// subscribe to event
+		Xf::Event::instance()._subscribe(*this, type, function);
+	}
+}
+
+/* subscribe function to input */
+void Xf::Evntmode::subscribe(Event::InputFunction function) {
+	// check if mode is active
+	if (_state) {
+		// subscribe to input
+		Xf::Event::instance()._subscribe(*this, function);
+	}
+}
+
 
 
 
