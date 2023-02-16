eaa5531ff13ef883c9599706cd139689828dc1b1
123Untitled <123untitled@pm.me>
2023-02-16 16:13:54 +0100

    add rect.cpp

---
+++ src/visual/rect.cpp
@@ -0,0 +1,48 @@
+#include "rect.hpp"
+
+/* default constructor */
+Xf::Rect::Rect(void) noexcept
+: x{0}, y{0}, w{0}, h{0} {
+	// code here...
+}
+
+/* size and position constructor */
+Xf::Rect::Rect(const Size x, const Size y, const Size w, const Size h) noexcept
+: x{x}, y{y}, w{w}, h{h} {
+	// code here...
+}
+
+/* copy constructor */
+Xf::Rect::Rect(const Rect& other) noexcept
+: x{other.x}, y{other.y}, w{other.w}, h{other.h} {
+	// code here...
+}
+
+/* move constructor */
+Xf::Rect::Rect(Rect&& other) noexcept
+: Rect{other} {
+	// code here...
+}
+
+/* destructor */
+Xf::Rect::~Rect(void) {
+	// code here...
+}
+
+/* copy assignment operator */
+Xf::Rect& Xf::Rect::operator=(const Rect& other) noexcept {
+	// check for self assignment
+	if (this != &other) {
+		// copy dimension
+		w = other.w; h = other.h;
+		// copy position
+		x = other.x; y = other.y;
+	} // return self reference
+	return *this;
+}
+
+/* move assignment operator */
+Xf::Rect& Xf::Rect::operator=(Rect&& other) noexcept {
+	// return copy assignment
+	return operator=(other);
+}
