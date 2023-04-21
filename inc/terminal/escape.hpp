d16754ddfd40014d5c601e45450fdf7f93e711b9
123Untitled <123untitled@pm.me>
2023-04-21 12:47:02 +0200

    replace by old escape sequence class

--- inc/terminal/escape.hpp
+++ inc/terminal/escape.hpp
@@ -21,268 +21,249 @@
 namespace Xf {
 
 
-	// -- E N U M S ---------------------------------------------------
+	// -- E N U M S -----------------------------------------------------------
 
 	enum class Esctype {
 
-		// -- M O V E -------------------------------------------------
+		// -- M O V E ---------------------------------------------------------
 
 		MOVE_HOME, MOVE_LEFT, MOVE_RIGHT,
 
-		// -- E R A S E -----------------------------------------------
+		// -- E R A S E -------------------------------------------------------
 
-		ERASE_SCREEN,
-		ERASE_LINE,
-		ERASE_TO_END_OF_LINE,
-		// ERASE_START_OF_LINE_TO_CURSOR,
+		ERASE_SCREEN, ERASE_LINE, ERASE_TO_END, ERASE_FROM_START,
 
-		// -- S C R E E N ---------------------------------------------
+		// -- S C R E E N -----------------------------------------------------
 
 		ENTER_SCREEN, EXIT_SCREEN, SAVE_SCREEN, RESTORE_SCREEN,
 
-		// -- C O L O R -----------------------------------------------
+		// -- C O L O R -------------------------------------------------------
 
 		RESET_STYLE,
 
-		// -- C U R S O R ---------------------------------------------
+		// -- C U R S O R -----------------------------------------------------
 
 		SHOW_CURSOR, HIDE_CURSOR, REQUEST_POSITION,
 
-		// -- C U R S O R  S T Y L E ----------------------------------
+		// -- C U R S O R  S T Y L E ------------------------------------------
 
 		CURSOR_BEAM, CURSOR_UNDERLINE, CURSOR_BLOCK,
 
-		// -- M A X ---------------------------------------------------
+		// -- M A X -----------------------------------------------------------
 
 		ESCTYPE_MAX
 	};
-
-
-	// -- E S C A P E  S E Q U E N C E S  C L A S S E S ---------------
-
-	#define MAKE_CLASS(NAME, ESC) \
-	struct NAME final { \
-	static constexpr const Esctype type = Esctype::ESC; \
-	}
-
-	/* move home */
-	MAKE_CLASS(move_home_t,        MOVE_HOME);
-
-	/* move left */
-	MAKE_CLASS(move_left_t,        MOVE_LEFT);
-
-	/* move right */
-	MAKE_CLASS(move_right_t,       MOVE_RIGHT);
-
-	/* erase screen */
-	MAKE_CLASS(erase_screen_t,     ERASE_SCREEN);
-
-	/* erase line */
-	MAKE_CLASS(erase_line_t,       ERASE_LINE);
-
-	/* erase to end of line */
-	MAKE_CLASS(erase_to_end_t,     ERASE_TO_END_OF_LINE);
-
-	/* enter screen */
-	MAKE_CLASS(enter_screen_t,     ENTER_SCREEN);
-
-	/* exit screen */
-	MAKE_CLASS(exit_screen_t,      EXIT_SCREEN);
-
-	/* save screen */
-	MAKE_CLASS(save_screen_t,      SAVE_SCREEN);
-
-	/* restore screen */
-	MAKE_CLASS(restore_screen_t,   RESTORE_SCREEN);
-
-	/* reset style */
-	MAKE_CLASS(reset_style_t,      RESET_STYLE);
-
-	/* show cursor */
-	MAKE_CLASS(show_cursor_t,      SHOW_CURSOR);
-
-	/* hide cursor */
-	MAKE_CLASS(hide_cursor_t,      HIDE_CURSOR);
-
-	/* cursor beam */
-	MAKE_CLASS(cursor_beam_t,      CURSOR_BEAM);
-
-	/* cursor underline */
-	MAKE_CLASS(cursor_underline_t, CURSOR_UNDERLINE);
-
-	/* cursor block */
-	MAKE_CLASS(cursor_block_t,     CURSOR_BLOCK);
-
-
-	#undef MAKE_CLASS
-
-
-	// -- E S C A P E  S E Q U E N C E S  F U N C T I O N S -----------
-
-	struct rgb_color_t {};
-	struct hex_color_t {};
-	struct move_position_t {};
-
-
-
-	// -- E S C A P E  M E T A ----------------------------------------
-
-	/* is escape false */
-	template <typename T>
-	struct is_escape final : Xf::false_t {};
-
-	/* is escape true */
-	template <>
-	struct is_escape<Esctype> : Xf::true_t {};
-
-	/* is escape true */
-	template <>
-	struct is_escape<const Esctype> : Xf::true_t {};
-
-	/* get type of member */
-	template <typename E>
-	using member = is_escape<decltype(E::type)>;
-
-	/* is escape concept */
-	template <typename T>
-	concept is_escape_c = is_escape<decltype(T::type)>::value;
-
-
-
-
-
-	struct hex_color_t;
-	struct rgb_color_t;
-	struct move_position_t;
-
-	/* is dyn escape false */
-	template <typename T>
-	struct is_dyn_escape : Xf::false_t {};
-
-	/* is dyn escape true */
-	template <>
-	struct is_dyn_escape<rgb_color_t> : Xf::true_t {};
-
-	/* is dyn escape true */
-	template <>
-	struct is_dyn_escape<hex_color_t> : Xf::true_t {};
-
-	/* is dyn escape true */
-	template <>
-	struct is_dyn_escape<move_position_t> : Xf::true_t {};
-
-
-	/* is dyn escape concept */
-	template <typename T>
-	concept is_dyn_escape_c = is_dyn_escape<T>::value;
-
-
 
 
 	// -- E S C A P E  C L A S S ----------------------------------------------
 
 	class Escape final {
 
-		private:
-
 		public:
 
-			// -- C O N S T R U C T O R S -------------------------------------
+			// -- P U B L I C  C O N S T R U C T O R S ------------------------
 
 			/* non-instantiable class */
 			NON_INSTANCIABLE(Escape);
 
 
-			// -- S T A T I C  M E T H O D S ----------------------------------
-
+			// -- P U B L I C  S T A T I C  M E T H O D S ---------------------
 
 			/* get escape sequence */
-			template <is_escape_c E>
-			static const Xf::String<char>& get(void) {
-				// return escape sequence
-				return _escapes[IDX(Esctype(E::type))];
-			}
+			static const Xf::CString& get(const Esctype);
 
-			/* draw escape sequence */
-			template <is_escape_c E>
-			static void draw(void) {
-				// draw escape sequence
-				Buffer::draw(_escapes[IDX(E::type)].pointer(),
-						_escapes[IDX(E::type)].size());
-			}
 
-			/* get special escape */
-			template <is_dyn_escape_c E, typename... A>
-			static
-			Xf::String<char>
-			get(A&&... arguments) {
-				if constexpr (Xf::is_same_v<E, move_position_t>) {
-					// move position
-					return _move_position(Xf::forward<A>(arguments)...);
-				}
-				else if constexpr (Xf::is_same_v<E, hex_color_t>) {
-					// hex color
-					return _hex_color(Xf::forward<A>(arguments)...);
-				}
-				else if constexpr (Xf::is_same_v<E, rgb_color_t>) {
-					// rgb color
-					return _rgb_color(Xf::forward<A>(arguments)...);
-				}
-				//return E::call(Xf::forward<A>(arguments)...);
-			}
+			/* move home */
+			static void move_home(void);
 
-			///* draw special escape */
-			template <is_dyn_escape_c E, typename... A>
-			static void draw(A&&... arguments) {
-				Xf::String<char> esc;
-				if constexpr (Xf::is_same_v<E, move_position_t>) {
-					// move position
-					esc = _move_position(Xf::forward<A>(arguments)...);
-				}
-				else if constexpr (Xf::is_same_v<E, hex_color_t>) {
-					// hex color
-					esc = _hex_color(Xf::forward<A>(arguments)...);
-				}
-				else if constexpr (Xf::is_same_v<E, rgb_color_t>) {
-					// rgb color
-					esc = _rgb_color(Xf::forward<A>(arguments)...);
-				}
-				// get private escape sequence index
-				//esc = E::call(Xf::forward<A>(arguments)...);
-				Buffer::draw(esc.pointer(), esc.size());
-			}
+			/* move left */
+			static void move_left(void);
 
-			/* request cursor position */
-			static bool request_position(UInt32& x, UInt32& y);
+			/* move right */
+			static void move_right(void);
+
+
+			/* erase screen */
+			static void erase_screen(void);
+
+			/* erase line */
+			static void erase_line(void);
+
+			/* erase to end of line */
+			static void erase_to_end(void);
+
+			/* erase from start of line */
+			static void erase_from_start(void);
+
+
+			/* enter screen */
+			static void enter_screen(void);
+
+			/* exit screen */
+			static void exit_screen(void);
+
+			/* save screen */
+			static void save_screen(void);
+
+			/* restore screen */
+			static void restore_screen(void);
+
+
+			/* reset style */
+			static void reset_style(void);
+
+
+			/* show cursor */
+			static void show_cursor(void);
+
+			/* hide cursor */
+			static void hide_cursor(void);
+
+			/* request position */
+			static void request_position(void);
+
+
+			/* cursor beam */
+			static void cursor_beam(void);
+
+			/* cursor underline */
+			static void cursor_underline(void);
+
+			/* cursor block */
+			static void cursor_block(void);
+
+
+
+			// -- S P E C I A L  E S C A P E  S E Q U E N C E S ---------------
+
+			/* request position */
+			static bool request_position(UInt32&, UInt32&);
+
+			/* move position */
+			static void move_position(UInt32, UInt32);
+
+			/* hex color */
+			static void hex_color(const UInt32, const bool = true);
+
+			/* rgb color */
+			static void rgb_color(UInt8, UInt8, UInt8, const bool = true);
+
+
+			/* get move position */
+			static Xf::CString get_move_position(UInt32, UInt32);
+
+			/* get hex color */
+			static Xf::CString get_hex_color(const UInt32, const bool = true);
+
+			/* get rgb color */
+			static Xf::CString get_rgb_color(UInt8, UInt8, UInt8, const bool = true);
+
+
 
 
 		private:
 
+			// -- P R I V A T E  A L I A S E S --------------------------------
 
-			/* hex color */
-			static Xf::String<char> _hex_color(const UInt32 color, const bool fore = true);
-
-			/* color rgb */
-			static Xf::String<char> _rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fore = true);
-
-			/* move position */
-			static Xf::String<char> _move_position(UInt32 x, UInt32 y);
+			/* array type */
+			using Array = Xf::Array<Xf::CString, IDX(Esctype::ESCTYPE_MAX)>;
 
 
-			// -- M E M B E R S -----------------------------------------------
+			// -- P R I V A T E  S T A T I C  M E M B E R S -------------------
 
 			/* escape sequences */
-			static const Array<String<char>, IDX(Esctype::ESCTYPE_MAX)> _escapes;
-
-
-		public:
-
-
+			static const Array _escapes;
 
 	};
 
+}
 
-};
+
+
+
+			/* get escape sequence */
+			//template <is_escape_c E>
+			//static const Xf::String<char>& get(void) {
+			//	// return escape sequence
+			//	return _escapes[IDX(Esctype(E::type))];
+			//}
+
+			///* draw escape sequence */
+			//template <is_escape_c E>
+			//static void draw(void) {
+			//	// draw escape sequence
+			//	Buffer::draw(_escapes[IDX(E::type)].pointer(),
+			//			_escapes[IDX(E::type)].size());
+			//}
+
+			///* get special escape */
+			//template <is_dyn_escape_c E, typename... A>
+			//static
+			//Xf::String<char>
+			//get(A&&... arguments) {
+			//	if constexpr (Xf::is_same_v<E, move_position_t>) {
+			//		// move position
+			//		return _move_position(Xf::forward<A>(arguments)...);
+			//	}
+			//	else if constexpr (Xf::is_same_v<E, hex_color_t>) {
+			//		// hex color
+			//		return _hex_color(Xf::forward<A>(arguments)...);
+			//	}
+			//	else if constexpr (Xf::is_same_v<E, rgb_color_t>) {
+			//		// rgb color
+			//		return _rgb_color(Xf::forward<A>(arguments)...);
+			//	}
+			//	//return E::call(Xf::forward<A>(arguments)...);
+			//}
+
+			/////* draw special escape */
+			//template <is_dyn_escape_c E, typename... A>
+			//static void draw(A&&... arguments) {
+			//	Xf::String<char> esc;
+			//	if constexpr (Xf::is_same_v<E, move_position_t>) {
+			//		// move position
+			//		esc = _move_position(Xf::forward<A>(arguments)...);
+			//	}
+			//	else if constexpr (Xf::is_same_v<E, hex_color_t>) {
+			//		// hex color
+			//		esc = _hex_color(Xf::forward<A>(arguments)...);
+			//	}
+			//	else if constexpr (Xf::is_same_v<E, rgb_color_t>) {
+			//		// rgb color
+			//		esc = _rgb_color(Xf::forward<A>(arguments)...);
+			//	}
+			//	// get private escape sequence index
+			//	//esc = E::call(Xf::forward<A>(arguments)...);
+			//	Buffer::draw(esc.pointer(), esc.size());
+			//}
+
+			///* request cursor position */
+			//static bool request_position(UInt32& x, UInt32& y);
+
+
+//		private:
+//
+//
+//			/* hex color */
+//			static Xf::String<char> _hex_color(const UInt32 color, const bool fore = true);
+//
+//			/* color rgb */
+//			static Xf::String<char> _rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fore = true);
+//
+//			/* move position */
+//			static Xf::String<char> _move_position(UInt32 x, UInt32 y);
+//
+//
+//
+//
+//		public:
+//
+//
+//
+//	};
+//
+//
+//};
 
 
 
--- src/escape.cpp
+++ src/escape.cpp
@@ -2,9 +2,7 @@
 
 #define ESC "\x1b"
 
-const Xf::Array<Xf::String<char>,
-				IDX(Xf::Esctype::ESCTYPE_MAX)>
-				Xf::Escape::_escapes = {
+const Xf::Escape::Array Xf::Escape::_escapes = {
 	/* move home */
 	ESC"[H",
 	/* move left */
@@ -18,8 +16,8 @@ const Xf::Array<Xf::String<char>,
 	ESC"[2K",
 	/* erase to end of line */
 	ESC"[0K",
-	/* erase start of line to cursor */
-	//ESC"[1K",
+	/* erase from start of line */
+	ESC"[1K",
 
 	/* enter screen */
 	ESC"[?1049h",
@@ -51,54 +49,114 @@ const Xf::Array<Xf::String<char>,
 #undef ESC
 
 
-
-// ESC[#A	moves cursor up # lines
-// ESC[#B	moves cursor down # lines
-
-// ESC[#E	moves cursor to beginning of next line, # lines down
-// ESC[#F	moves cursor to beginning of previous line, # lines up
-
-// ESC[#G	moves cursor to column #
-// ESC M	moves cursor one line up, scrolling if needed
+/* get escape sequence */
+const Xf::CString& Xf::Escape::get(const Esctype type) {
+	return _escapes[IDX(type) % IDX(Esctype::ESCTYPE_MAX)];
+}
 
 
+/* move home */
+void Xf::Escape::move_home(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::MOVE_HOME)]);
+}
 
-////////////////////////// ERASE FUNCTIONS
-// Erasing the line won't move the cursor,
-// meaning that the cursor will stay at the last position
-// it was at before the line was erased.
-// You can use \r after erasing the line,
-// to return the cursor to the start of the current line.
-/////////////////////////////////////////////////////////
+/* move left */
+void Xf::Escape::move_left(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::MOVE_LEFT)]);
+}
 
-// erase from cursor until end of screen
-#define AAA									ESC"[0J"
-// erase from cursor to beginning of screen
-#define BBB									ESC"[1J"
-// erase saved lines
-#define CCC									ESC"[3J"
-
-
-/////////////////////// COMMON PRIVATE MODES
-// These are some examples of private modes,
-// which are not defined by the specification,
-// but are implemented in most terminals.
-// Refer to the XTerm Control Sequences
-// for a more in-depth list of private modes defined by XTerm.
-// While these modes may be supported by the most terminals,
-// some may not work in multiplexers like tmux.
-///////////////////////////////////////////////
+/* move right */
+void Xf::Escape::move_right(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::MOVE_RIGHT)]);
+}
 
 
-//ESC[0m		reset all modes (styles and colors)
-//ESC[1m	ESC[22m	set bold mode.
-//ESC[2m	ESC[22m	set dim/faint mode.
-//ESC[3m	ESC[23m	set italic mode.
-//ESC[4m	ESC[24m	set underline mode.
-//ESC[5m	ESC[25m	set blinking mode
-//ESC[7m	ESC[27m	set inverse/reverse mode
-//ESC[8m	ESC[28m	set hidden/invisible mode
-//ESC[9m	ESC[29m	set strikethrough mode.
+/* erase screen */
+void Xf::Escape::erase_screen(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::ERASE_SCREEN)]);
+}
+
+/* erase line */
+void Xf::Escape::erase_line(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::ERASE_LINE)]);
+}
+
+/* erase to end of line */
+void Xf::Escape::erase_to_end(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::ERASE_TO_END)]);
+}
+
+/* erase from start of line */
+void Xf::Escape::erase_from_start(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::ERASE_FROM_START)]);
+}
+
+
+/* enter screen */
+void Xf::Escape::enter_screen(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::ENTER_SCREEN)]);
+}
+
+/* exit screen */
+void Xf::Escape::exit_screen(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::EXIT_SCREEN)]);
+}
+
+/* save screen */
+void Xf::Escape::save_screen(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::SAVE_SCREEN)]);
+}
+
+/* restore screen */
+void Xf::Escape::restore_screen(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::RESTORE_SCREEN)]);
+}
+
+
+/* reset style */
+void Xf::Escape::reset_style(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::RESET_STYLE)]);
+}
+
+
+/* show cursor */
+void Xf::Escape::show_cursor(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::SHOW_CURSOR)]);
+}
+
+/* hide cursor */
+void Xf::Escape::hide_cursor(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::HIDE_CURSOR)]);
+}
+
+/* request position */
+void Xf::Escape::request_position(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::REQUEST_POSITION)]);
+}
+
+
+/* cursor beam */
+void Xf::Escape::cursor_beam(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::CURSOR_BEAM)]);
+}
+
+/* cursor underline */
+void Xf::Escape::cursor_underline(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::CURSOR_UNDERLINE)]);
+}
+
+/* cursor block */
+void Xf::Escape::cursor_block(void) {
+	Xf::Output::write(_escapes[IDX(Esctype::CURSOR_BLOCK)]);
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -112,17 +170,17 @@ const Xf::Array<Xf::String<char>,
 
 bool Xf::Escape::request_position(UInt32& x, UInt32& y) {
 
-	UInt32*	num		= &y;
-	bool	bracket	= false;
-	char	c		= 0;
+	UInt32* num     = &y;
+	bool    bracket = false;
+	char    c       = 0;
 
-	const String<char>& esc = _escapes[IDX(Esctype::REQUEST_POSITION)];
+	const Xf::CString& esc = _escapes[IDX(Esctype::REQUEST_POSITION)];
 
 	write(STDOUT_FILENO, esc.pointer(), esc.size());
 	x = y = 0;
 	// iterate while input available
 	while (0 < read(STDIN_FILENO, &c, 1) && c != 'R') {
-		write(1, &c, 1);
+		// check bracket sequence
 		if (bracket) {
 			// convert character to integer
 			if ((c ^ 48) < 10)
@@ -143,12 +201,19 @@ bool Xf::Escape::request_position(UInt32& x, UInt32& y) {
 
 // -- M O V E -----------------------------------------------------------------
 
+
 /* move position */
-Xf::String<char> Xf::Escape::_move_position(UInt32 x, UInt32 y) {
-	UInt8		escape[ESCAPE_BUFFER_SIZE];
-	UInt32		ite;
+void Xf::Escape::move_position(UInt32 x, UInt32 y) {
+	Xf::Output::write(get_move_position(x, y));
+}
+
+/* get move position */
+Xf::CString Xf::Escape::get_move_position(UInt32 x, UInt32 y) {
+	UInt8  escape[ESCAPE_BUFFER_SIZE];
+	UInt32 ite;
 
 	constexpr const UInt32 max = Xf::max<UInt32>();
+
 	x += (x != max);
 	y += (y != max);
 
@@ -172,8 +237,7 @@ Xf::String<char> Xf::Escape::_move_position(UInt32 x, UInt32 y) {
 	escape[--ite] = '\x1b';
 
 	// append escape sequence to buffer
-	//Buffer::draw((char*)&escape[ite], ESCAPE_BUFFER_SIZE - ite);
-	return Xf::String<char>((char*)&escape[ite], ESCAPE_BUFFER_SIZE - ite);
+	return Xf::CString((char*)&escape[ite], ESCAPE_BUFFER_SIZE - ite);
 }
 
 
@@ -181,7 +245,18 @@ Xf::String<char> Xf::Escape::_move_position(UInt32 x, UInt32 y) {
 // -- C O L O R ---------------------------------------------------------------
 
 /* hex color */
-Xf::String<char> Xf::Escape::_hex_color(const UInt32 color, const bool fore) {
+void Xf::Escape::hex_color(const UInt32 color, const bool fore) {
+	Xf::Output::write(get_hex_color(color, fore));
+}
+
+/* rgb color */
+void Xf::Escape::rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fore) {
+	Xf::Output::write(get_rgb_color(r, g, b, fore));
+}
+
+
+/* hex color */
+Xf::CString Xf::Escape::get_hex_color(const UInt32 color, const bool fore) {
 
 	// declare bytes color
 	UInt8 r, g, b;
@@ -190,12 +265,11 @@ Xf::String<char> Xf::Escape::_hex_color(const UInt32 color, const bool fore) {
 	g = (color >> 8)  & 0xFF;
 	b = (color)       & 0xFF;
 
-	return _rgb_color(r, g, b, fore);
-
+	return get_rgb_color(r, g, b, fore);
 }
 
 /* color rgb */
-Xf::String<char> Xf::Escape::_rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fore) {
+Xf::CString Xf::Escape::get_rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fore) {
 	// static 24bit color escape sequence
 	static UInt8	escape[] = {	'\x1b', '[',
 									' ', '8', ';', '2', ';',
@@ -220,8 +294,7 @@ Xf::String<char> Xf::Escape::_rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fo
 		// remove last digit
 		r /= BASE; g /= BASE; b /= BASE;
 	} // append escape sequence to buffer
-	//Buffer::draw(escape, 19);
-	return Xf::String<char>((char*)escape, 19);
+	return Xf::CString((char*)escape, 19);
 }
 
 
@@ -333,3 +406,55 @@ Xf::String<char> Xf::Escape::_rgb_color(UInt8 r, UInt8 g, UInt8 b, const bool fo
 //	//} // final check sequence
 //	//return (true);
 //}
+
+
+
+
+
+// ESC[#A	moves cursor up # lines
+// ESC[#B	moves cursor down # lines
+
+// ESC[#E	moves cursor to beginning of next line, # lines down
+// ESC[#F	moves cursor to beginning of previous line, # lines up
+
+// ESC[#G	moves cursor to column #
+// ESC M	moves cursor one line up, scrolling if needed
+
+
+
+////////////////////////// ERASE FUNCTIONS
+// Erasing the line won't move the cursor,
+// meaning that the cursor will stay at the last position
+// it was at before the line was erased.
+// You can use \r after erasing the line,
+// to return the cursor to the start of the current line.
+/////////////////////////////////////////////////////////
+
+// erase from cursor until end of screen
+//#define AAA									ESC"[0J"
+// erase from cursor to beginning of screen
+//#define BBB									ESC"[1J"
+// erase saved lines
+//#define CCC									ESC"[3J"
+
+
+/////////////////////// COMMON PRIVATE MODES
+// These are some examples of private modes,
+// which are not defined by the specification,
+// but are implemented in most terminals.
+// Refer to the XTerm Control Sequences
+// for a more in-depth list of private modes defined by XTerm.
+// While these modes may be supported by the most terminals,
+// some may not work in multiplexers like tmux.
+///////////////////////////////////////////////
+
+
+//ESC[0m		reset all modes (styles and colors)
+//ESC[1m	ESC[22m	set bold mode.
+//ESC[2m	ESC[22m	set dim/faint mode.
+//ESC[3m	ESC[23m	set italic mode.
+//ESC[4m	ESC[24m	set underline mode.
+//ESC[5m	ESC[25m	set blinking mode
+//ESC[7m	ESC[27m	set inverse/reverse mode
+//ESC[8m	ESC[28m	set hidden/invisible mode
+//ESC[9m	ESC[29m	set strikethrough mode.
