283e8425328d11cdd384b731e0a0103db384cb4e
123Untitled <123untitled@pm.me>
2023-05-07 22:37:32 +0200

    update

--- main.cpp
+++ main.cpp
@@ -10,19 +10,41 @@
 #include <any>
 
 #include "random.hpp"
+#include "trie.hpp"
+
 
 int main(int ac, char** av) {
 
 
-	//UT::array_ut();
+	Xf::Trie<std::string> trie;
+	Xf::CString str = "hello";
+	trie.insert(str, "world");
+	trie.insert(str, "world2");
 
+	Xf::AutoPointer<std::string> p = trie.find(Xf::CString{"hello"});
+
+	std::cout << "check" << std::endl;
+	if (p != nullptr) {
+		std::cout << "found: " << *p << std::endl;
+	}
+	else {
+		std::cout << "not found" << std::endl;
+	}
+
+	//char c = 'a';
+	//int n = 10;
+	//Xf::Tuple<char, int> tuple{'a', n};
+
+	//UT::array_ut();
 	//UT::tuple_ut();
+
+
+
+	return EXIT_SUCCESS;
 
 	for (auto i = 0; i < 10; ++i) {
 		std::cout << Xf::Random::random(10) << std::endl;
 	}
-
-	return EXIT_SUCCESS;
 
 }
 
