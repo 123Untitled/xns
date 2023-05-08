d0af03d244c6f2ea5ba8e18bd66d6c4e5823b3ea
123Untitled <123untitled@pm.me>
2023-05-08 04:38:04 +0200

    re-implement forward like std-lib

--- inc/meta/forward.hpp
+++ inc/meta/forward.hpp
@@ -4,51 +4,57 @@
 #include "enable_if.hpp"
 #include "remove.hpp"
 #include "is_lvalue_reference.hpp"
+#include "identity.hpp"
 
 
 // -- N A M E S P A C E -------------------------------------------------------
 
 namespace Xf {
 
-	// -- I D E N T I T Y -----------------------------------------------------
 
-	// identity
+	/* forward lvalue helper */
 	template <typename T>
-	struct identity { using type = T; };
+	using ToLvalue = Xf::remove_reference_t<T>&;
+
+	/* forward rvalue helper */
+	template <typename T>
+	using ToRvalue = Xf::remove_reference_t<T>&&;
 
 
 	// -- F O R W A R D -------------------------------------------------------
 
-	// forward lvalues as rvalues
-	template <typename T>
-	inline constexpr typename Xf::enable_if<!Xf::is_lvalue_reference<T>::value, T&&>::type
-	forward(typename Xf::identity<T>::type& obj) {
+	// forward lvalues as lvalues
+	template <class T>
+	inline constexpr T&& forward(ToLvalue<T> obj) noexcept {
+		// cast to T&(&&) = T&
 		return static_cast<T&&>(obj);
 	}
 
 	// forward rvalues as rvalues
-	template <typename T>
-	inline constexpr typename Xf::enable_if<!Xf::is_lvalue_reference<T>::value, T&&>::type
-	forward(typename Xf::identity<T>::type&& obj) {
+	template <class T>
+	inline constexpr T&& forward(ToRvalue<T> obj) noexcept {
+		// cast to T&&(&&) = T&&
 		return static_cast<T&&>(obj);
 	}
-
-	// forward lvalues as lvalues.
-	template <typename T>
-	inline constexpr typename Xf::enable_if<Xf::is_lvalue_reference<T>::value, T>::type
-	forward(typename Xf::identity<T>::type obj) {
-		return obj;
-	}
-
-	// prevent forwarding rvalues as const lvalues
-	template <typename T>
-	inline constexpr typename Xf::enable_if<Xf::is_lvalue_reference<T>::value, T>::type
-	forward(typename Xf::remove_reference<T>::type&& obj) = delete;
 
 
 };
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
