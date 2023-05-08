d1e26a363c08dc4ddea45adf60bf34a006656757
123Untitled <123untitled@pm.me>
2023-05-08 04:39:57 +0200

    rename some meta struct

--- inc/meta/is_same.hpp
+++ inc/meta/is_same.hpp
@@ -1,6 +1,7 @@
 #ifndef IS_SAME_HEADER
 #define IS_SAME_HEADER
 
+#include "identity.hpp"
 #include "integral_constant.hpp"
 #include "remove.hpp"
 
@@ -14,43 +15,64 @@ namespace Xf {
 
 	/* is same false */
 	template <class T, class U>
-	struct is_same : Xf::false_t {};
+	struct IsSame_s : Xf::false_t {};
 
 	/* is same true */
 	template <class T>
-	struct is_same<T, T> : Xf::true_t {};
+	struct IsSame_s<T, T> : Xf::true_t {};
 
 
 	template <class T, class U>
-	static constexpr bool is_same_v = is_same<T, U>::value;
+	static constexpr bool is_same = IsSame_s<T, U>::value;
+
+	template <class T, class... U>
+	static constexpr bool is_all_same = (is_same<T, U> && ...);
 
 	/* is same concept */
 	template <class T, class U>
-	concept is_same_c = is_same<T, U>::value;
+	concept IsSame = is_same<T, U>;
 
-	/* is different */
+	/* is same concept remove specifier */
+	//template <class T, class U>
+	//concept IsSameRmCvr = is_same<remove_cvr_t<T>, remove_cvr_t<U>>;
+
+	/* is same concept variadic */
+	template <class T, class... U>
+	concept IsAllSame = is_all_same<T, U...>;
+
+	/* is not same */
 	template <class T, class U>
-	concept is_different_c = !is_same<T, U>::value;
+	concept IsNotSame = !is_same<T, U>;
+
+	/* is not same variadic */
+	template <class T, class... U>
+	concept IsNotAllSame = !is_all_same<T, U...>;
 
 
 
 	// -- I S  A L L  S A M E -------------------------------------------------
 
 	/* is all same */
-	template <typename T, typename...A>
-	using is_all_same = Xf::bool_constant<(... && is_same_v<T, A>)>;
+	//template <typename T, typename...A>
+	//using is_all_same = Xf::bool_constant<(... && is_same_v<T, A>)>;
+
 
 	/* is all same value */
-	template <typename T, typename...A>
-	static constexpr bool is_all_same_v = is_all_same<T, A...>::value;
+	//template <typename T, typename...A>
+	//static constexpr bool is_all_same_v = is_all_same<T, A...>::value;
 
 	/* is all same concept */
-	template <typename T, typename...A>
-	concept is_all_same_c = is_all_same_v<T, A...>;
+	//template <typename T, typename...A>
+	//concept is_all_same_c = is_all_same_v<T, A...>;
 
-	/* is all different */
-	template <typename T, typename...A>
-	concept is_not_all_same = !is_all_same_v<T, A...>;
+	///* is all different */
+	//template <typename T, typename...A>
+	//concept is_not_all_same = !is_all_same_v<T, A...>;
+
+
+
+
+
 
 
 
