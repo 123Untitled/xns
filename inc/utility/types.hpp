0d6fe5de444764f1c7053e9d09b04a78322e6c43
123Untitled <123untitled@pm.me>
2023-05-21 12:12:35 +0200

    rework all types

--- inc/utility/types.hpp
+++ inc/utility/types.hpp
@@ -2,122 +2,11 @@
 #define TYPES_HEADER
 
 #include "conditional.hpp"
-	// preprocessor check operating system
-	//#if defined(__APPLE__) && defined(__MACH__)
-	//	#include <MacTypes.h>
-	//#elif defined(__linux__)
-	//	#include <cstdint>
-	//	#include <cstddef>
-	//#endif
-
-
-// -- N A M E S P A C E -------------------------------------------------------
-
-namespace Xf {
-
-	// -- T Y P E S -------------------------------------------------------------
-
-	/* null pointer */
-	using Nullptr = decltype(nullptr);
-
-}
-
-/* char types */
-using Char    = char;
-using SChar   = signed   char;
-using UChar   = unsigned char;
-
-/* short types */
-using Short   = short;
-using SShort  = signed   short;
-using UShort  = unsigned short;
-
-/* int types */
-using Int     = int;
-using SInt    = signed   int;
-using UInt    = unsigned int;
-
-/* long types */
-using Long    = long;
-using SLong   = signed   long;
-using ULong   = unsigned long;
-
-/* long long types */
-using LLong   = long long;
-using SLLong  = signed   long long;
-using ULLong  = unsigned long long;
-
-/* float types */
-using Float   = float;
-using Double  = double;
-using LDouble = long double;
-
-/* boolean types */
-using Bool    = bool;
-
-
-
-template <decltype(sizeof(char)) N>
-using unsigned_t =	xns::conditional<(sizeof(unsigned char)      == N), unsigned char,
-					xns::conditional<(sizeof(unsigned short)     == N), unsigned short,
-					xns::conditional<(sizeof(unsigned int)       == N), unsigned int,
-					xns::conditional<(sizeof(unsigned long)      == N), unsigned long,
-					xns::conditional<(sizeof(unsigned long long) == N), unsigned long long,
-					void > > > > >;
-
-template <decltype(sizeof(char)) N>
-using signed_t =	xns::conditional<(sizeof(signed char)        == N), signed char,
-					xns::conditional<(sizeof(signed short)       == N), signed short,
-					xns::conditional<(sizeof(signed int)         == N), signed int,
-					xns::conditional<(sizeof(signed long)        == N), signed long,
-					xns::conditional<(sizeof(signed long long)   == N), signed long long,
-					void > > > > >;
-
-using SInt8  = signed_t<1>;
-using UInt8  = unsigned_t<1>;
-
-using SInt16 = signed_t<2>;
-using UInt16 = unsigned_t<2>;
-
-using SInt32 = signed_t<4>;
-using UInt32 = unsigned_t<4>;
-
-using SInt64 = signed_t<8>;
-using UInt64 = unsigned_t<8>;
-
-
-// get number of bytes for current architecture
-#define ARCH_BYTES (sizeof(void*))
-
-
-
-using SizeT = unsigned_t<ARCH_BYTES>;
-
-
-
-
-// compile time get greatest integer type
-template <typename T, typename U>
-struct GetGreatestIntegerType {
-	using Type = xns::conditional<(sizeof(T) > sizeof(U)), T, U>;
-};
-
-template <typename T, typename U>
-using GetGreatestIntegerType_t = typename GetGreatestIntegerType<T, U>::Type;
-
-
-
-
-
-using Size = UInt64;
 
 // include for struct winsize
 #include <sys/ioctl.h>
 // include for declval
 #include <utility>
-
-using TSize = decltype(std::declval<struct winsize>().ws_row);
-
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
@@ -126,14 +15,61 @@ namespace xns {
 
 	// -- T Y P E S -------------------------------------------------------------
 
+	/* terminal size type */
+	using term_size = decltype(std::declval<struct winsize>().ws_row);
+
 	/* null pointer type */
 	using null = decltype(nullptr);
 
 	/* architecture bytes */
 	#define ARCH_BYTES (sizeof(void*))
 
+
+	/* unsigned integer types */
+	template <decltype(sizeof(char)) N>
+	using unsign =  xns::conditional<(sizeof(unsigned char)      == N), unsigned char,
+					xns::conditional<(sizeof(unsigned short)     == N), unsigned short,
+					xns::conditional<(sizeof(unsigned int)       == N), unsigned int,
+					xns::conditional<(sizeof(unsigned long)      == N), unsigned long,
+					xns::conditional<(sizeof(unsigned long long) == N), unsigned long long,
+					void > > > > >;
+
+	/* signed integer types */
+	template <decltype(sizeof(char)) N>
+	using sign =    xns::conditional<(sizeof(signed char)        == N), signed char,
+					xns::conditional<(sizeof(signed short)       == N), signed short,
+					xns::conditional<(sizeof(signed int)         == N), signed int,
+					xns::conditional<(sizeof(signed long)        == N), signed long,
+					xns::conditional<(sizeof(signed long long)   == N), signed long long,
+					void > > > > >;
+
+
+	/* byte types */
+	using sbyte   = sign<1>;
+	using ubyte   = unsign<1>;
+
+	/* lowest integer type */
+	using s8      = sign<1>;
+	using u8      = unsign<1>;
+
+	/* medium integer type */
+	using s16     = sign<2>;
+	using u16     = unsign<2>;
+
+	/* normal integer type */
+	using s32     = sign<4>;
+	using u32     = unsign<4>;
+
+	/* highest integer type */
+	using s64     = sign<8>;
+	using u64     = unsign<8>;
+
+
 	/* maximum unsigned integer type */
-	using size_t = unsigned_t<ARCH_BYTES>;
+	using size_t = unsign<ARCH_BYTES>;
+
+	/* maximum signed integer type */
+	using ssize_t = sign<ARCH_BYTES>;
 
 }
 
