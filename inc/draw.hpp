af4b7a6c035519463592c89bf04894553084911b
123Untitled <123untitled@pm.me>
2023-05-21 11:59:08 +0200

    disable class by comment

--- inc/draw.hpp
+++ inc/draw.hpp
@@ -30,16 +30,16 @@ class Draw final {
 
 
 
-	static void drawLine(const UInt x, const UInt y, const void *ptr, const UInt size);
+	//static void drawLine(const UInt x, const UInt y, const void *ptr, const UInt size);
 
 	/* draw string */
-	static void draw_string(const UInt x, const UInt y, const std::string& str);
+	//static void draw_string(const UInt x, const UInt y, const std::string& str);
 
-	static void vertivalDraw(const UInt x, UInt y, const void *ptr, const UInt size, UInt line);
+	//static void vertivalDraw(const UInt x, UInt y, const void *ptr, const UInt size, UInt line);
 
-	static void clearLine(UInt32 posX, UInt32 posY, UInt32 size);
+	//static void clearLine(UInt32 posX, UInt32 posY, UInt32 size);
 
-	static void drawInteger(UInt number);
+	//static void drawInteger(UInt number);
 
 	//static int drawStringNl(const Str& string);
 
--- src/draw.cpp
+++ src/draw.cpp
@@ -1,61 +1,61 @@
 #include "draw.hpp"
 
-void Draw::clearLine(UInt32 posX, UInt32 posY, UInt32 size) {
-	UInt8	*clear = new UInt8[size];
-	UInt32 i = 0;
-	while (i < size) {
-		clear[i] = ' ';
-		i++;
-	}
-	//Escape::move_position(posX, posY);
-	write(STDOUT_FILENO, clear, size);
-	delete[] clear;
-}
-
-
-/* drawLine */
-void Draw::drawLine(const UInt x, const UInt y, const void *ptr, const UInt size) {
-	//Escape::move_position(x, y);
-	//Buffer::draw(ptr, size);
-}
-
-/* draw string */
-void Draw::draw_string(const UInt x, const UInt y, const std::string& str) {
-	// move to position
-	//Escape::move_position(x, y);
-	// draw string
-	//Buffer::draw(str.data(), str.size());
-}
-
-/* vertivalDraw */
-void Draw::vertivalDraw(const UInt x, UInt y, const void *ptr, const UInt size, UInt line) {
-	while (line) {
-		//Escape::move_position(x, y);
-		//Buffer::draw(ptr, size);
-		--line;
-		++y;
-	}
-}
-
-void Draw::drawInteger(UInt number) {
-
-	constexpr UInt BUFFSIZE = 64;
-
-	static char seq[BUFFSIZE];
-
-	char* ptr = &(seq[BUFFSIZE]);
-
-	while (number) {
-
-		std::cout << "loop" << std::endl;
-		*(--ptr) = (number % 10) + '0';
-		number /= 10;
-	}
-	UInt size = (seq + BUFFSIZE) - ptr;
-	//Buffer::draw(ptr, size);
-
-
-}
+//void Draw::clearLine(unsigned posX, unsigned posY, unsigned size) {
+//	UInt8	*clear = new UInt8[size];
+//	UInt32 i = 0;
+//	while (i < size) {
+//		clear[i] = ' ';
+//		i++;
+//	}
+//	//Escape::move_position(posX, posY);
+//	write(STDOUT_FILENO, clear, size);
+//	delete[] clear;
+//}
+//
+//
+///* drawLine */
+////void Draw::drawLine(const UInt x, const UInt y, const void *ptr, const UInt size) {
+//	//Escape::move_position(x, y);
+//	//Buffer::draw(ptr, size);
+////}
+//
+///* draw string */
+//void Draw::draw_string(const UInt x, const UInt y, const std::string& str) {
+//	// move to position
+//	//Escape::move_position(x, y);
+//	// draw string
+//	//Buffer::draw(str.data(), str.size());
+//}
+//
+///* vertivalDraw */
+//void Draw::vertivalDraw(const UInt x, UInt y, const void *ptr, const UInt size, UInt line) {
+//	while (line) {
+//		//Escape::move_position(x, y);
+//		//Buffer::draw(ptr, size);
+//		--line;
+//		++y;
+//	}
+//}
+//
+//void Draw::drawInteger(UInt number) {
+//
+//	constexpr UInt BUFFSIZE = 64;
+//
+//	static char seq[BUFFSIZE];
+//
+//	char* ptr = &(seq[BUFFSIZE]);
+//
+//	while (number) {
+//
+//		std::cout << "loop" << std::endl;
+//		*(--ptr) = (number % 10) + '0';
+//		number /= 10;
+//	}
+//	UInt size = (seq + BUFFSIZE) - ptr;
+//	//Buffer::draw(ptr, size);
+//
+//
+//}
 
 
 /* int Draw::drawStringNl(const Str& string) {
