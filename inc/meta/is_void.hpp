caf4dc3d7521d86cc94b5889d5c9bca61121dda1
123Untitled <123untitled@pm.me>
2023-05-23 10:36:42 +0200

    rework is_void

--- inc/meta/is_void.hpp
+++ inc/meta/is_void.hpp
@@ -1,7 +1,8 @@
 #ifndef IS_VOID_HEADER
 #define IS_VOID_HEADER
 
-#include "integral_constant.hpp"
+#include "is_same.hpp"
+
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
@@ -10,21 +11,9 @@ namespace xns {
 
 	// -- I S  V O I D --------------------------------------------------------
 
-	/* false type */
-	template <class T>
-	struct _is_void       : xns::no  {};
-
-	/* true type */
-	template <>
-	struct _is_void<void> : xns::yes {};
-
 	/* is void concept */
 	template <class T>
-	concept is_void = _is_void<T>::value;
-
-	/* is not void concept */
-	template <class T>
-	concept is_not_void = !is_void<T>;
+	concept is_void = xns::is_same<xns::remove_cv<T>, void>;
 
 
 }
