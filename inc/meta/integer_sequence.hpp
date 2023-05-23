36119e55ab74f336c07ade396a3772e15fc00dd0
123Untitled <123untitled@pm.me>
2023-05-23 10:38:13 +0200

    xns renaming

--- inc/meta/integer_sequence.hpp
+++ inc/meta/integer_sequence.hpp
@@ -3,100 +3,126 @@
 
 #include "is_integral.hpp"
 
-#include <iostream>
-// -- N A M E S P A C E -------------------------------------------------------
+//#include "output.hpp"
 
-namespace Xf {
+//#include <iostream>
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
 
 	// -- I N T E G E R  S E Q U E N C E --------------------------------------
 
 	template <class T, T... N>
-	struct IntegerSeq final {
+	struct integer_seq final {
+
+
+		// -- A S S E R T I O N S ---------------------------------------------
 
 		/* check if T is an integral type */
-		static_assert(xns::is_integral<T>, "not integral type");
+		static_assert(xns::is_integral<T>, "N MUST BE AN INTEGRAL TYPE");
+
+
+		// -- T Y P E S -------------------------------------------------------
 
 		/* size type */
-		using Size = T;
+		using type = T;
 
 		/* self type */
-		using Self  = IntegerSeq<Size, N...>;
+		using self  = integer_seq<type, N...>;
+
+
+		// -- A C C E S S O R S -----------------------------------------------
 
 		/* size accessor */
-		static inline constexpr Size size(void) noexcept {
+		static inline constexpr type size(void) noexcept {
 			return sizeof...(N);
 		}
 
+
 	};
 
-	/* alias to integer_sequence of type size_t */
-	template <size_t... N>
-	using IndexSeq = IntegerSeq<size_t, N...>;
+
+	/* integer_sequence of size_t */
+	template <xns::size_t... N>
+	using index_seq = integer_seq<xns::size_t, N...>;
 
 
 	// -- M A K E  I N T E G E R  S E Q U E N C E -----------------------------
 
-	template <class IntType, IntType BEGIN, IntType END, IntType STEP = 1>
-	class MakeIntegerSeq {
+	template <class int_type, int_type BEGIN, int_type END, int_type STEP = 1>
+	class _make_integer_seq {
+
+
+		// -- A S S E R T I O N S ---------------------------------------------
 
 		/* check if range is valid */
 		static_assert(BEGIN <= END, "!YOU WILL LOOP UNTIL THE END OF WORLD!");
 
+
 		private:
 
-			// -- P R I V A T E  I M P L E M E N T A T I O N ----------------------
+			// -- P R I V A T E  I M P L E M E N T A T I O N ------------------
 
 			/* forward declaration */
-			template <IntType... SEQUENCE>
-			struct implementation;
+			template <int_type... SEQUENCE>
+			struct impl;
 
 			/* specialization for N == B, which means we have to stop the recursion */
-			template <IntType NUMBER, IntType... SEQUENCE> requires (NUMBER == BEGIN)
-			struct implementation<NUMBER, SEQUENCE...> {
+			template <int_type NUMBER, int_type... SEQUENCE> requires (NUMBER == BEGIN)
+			struct impl<NUMBER, SEQUENCE...> {
 
 				/* type of the integer sequence */
-				using type = IntegerSeq<IntType, SEQUENCE...>;
+				using type = integer_seq<int_type, SEQUENCE...>;
 			};
 
 			/* specialization for N != B, which means we have to continue the recursion */
-			template <IntType NUMBER, IntType... SEQUENCE> requires (NUMBER != BEGIN)
-			struct implementation<NUMBER, SEQUENCE...> {
+			template <int_type NUMBER, int_type... SEQUENCE> requires (NUMBER != BEGIN)
+			struct impl<NUMBER, SEQUENCE...> {
 
 				/* recursive call of type */
-				using type = typename implementation<NUMBER - STEP, NUMBER - STEP, SEQUENCE...>::type;
+				using type = typename impl<NUMBER - STEP, NUMBER - STEP, SEQUENCE...>::type;
 			};
+
 
 		public:
 
-			// -- P U B L I C  I N T E R F A C E ----------------------------------
+			// -- R E C U R S I V E  T Y P E ----------------------------------
 
-			using Type = typename implementation<END>::type;
+			using type = typename impl<END>::type;
 
 	};
 
+
 	/* alias to make an integer sequence of type T with length N */
 	template <class T, T N>
-	using make_integer_sequence = typename MakeIntegerSeq<T, 0, N>::Type;
+	using make_integer_sequence = typename _make_integer_seq<T, 0, N>::type;
 
 	/* alias to make an integer sequence of type T starting from B and ending at E */
 	template <class T, T B, T E>
-	using make_integer_range    = typename MakeIntegerSeq<T, B, E>::Type;
+	using make_integer_range    = typename _make_integer_seq<T, B, E>::type;
+
 
 	/* alias to make an index sequence of length N */
-	template <size_t N>
-	using make_index_sequence   = make_integer_sequence<size_t, N>;
+	template <xns::size_t N>
+	using make_index_sequence   = make_integer_sequence<xns::size_t, N>;
 
 	/* alias to make an index sequence starting from B and ending at E */
-	template <size_t BEGIN, size_t END>
-	using make_index_range      = make_integer_range<size_t, BEGIN, END>;
+	template <xns::size_t B, xns::size_t E>
+	using make_index_range      = make_integer_range<size_t, B, E>;
+
 
 	/* alias to make an index range with a step */
-	template <size_t BEGIN, size_t END, size_t STEP>
-	using make_index_range_step = typename MakeIntegerSeq<size_t, BEGIN, END, STEP>::Type;
+	template <xns::size_t B, xns::size_t E, xns::size_t S>
+	using make_index_range_step = typename _make_integer_seq<xns::size_t, B, E, S>::type;
+
 
 	/* alias to transform a parameter pack into an index sequence */
-	template <class... Types>
-	using IndexSeqFor    = make_index_sequence<sizeof...(Types)>;
+	template <class... A>
+	using index_seq_for    = make_index_sequence<sizeof...(A)>;
+
 
 
 	// -- D E B U G -----------------------------------------------------------
@@ -104,10 +130,13 @@ namespace Xf {
 
 	/* debug function */
 	template<class T, T... ints>
-	void debug_sequence(IntegerSeq<T, ints...> int_seq) {
+	void debug_sequence(integer_seq<T, ints...> int_seq) {
+		/*
+		xns::output::write("The sequence of size ");
 		std::cout << "The sequence of size " << int_seq.size() << ": ";
 		((std::cout << ints << ' '), ...);
 		std::cout << '\n';
+		*/
 	}
 
 }
