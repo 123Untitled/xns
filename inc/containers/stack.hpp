8c91337d1408e018dfbb4dd7726ed7d25280c843
123Untitled <123untitled@pm.me>
2023-05-30 17:46:35 +0200

    xns renaming

--- inc/containers/stack.hpp
+++ inc/containers/stack.hpp
@@ -7,54 +7,60 @@
 #include "allocator.hpp"
 #include <iostream>
 
-// -- N A M E S P A C E -------------------------------------------------------
 
-namespace Xf {
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
 
 	// -- S T A C K  C L A S S ------------------------------------------------
 
-	template <typename T>
-	class Stack final {
+	template <class T>
+	class stack final {
+
 
 		public:
 
-			// -- A L I A S E S -----------------------------------------------
+			// -- T Y P E S ---------------------------------------------------
 
 			/* value type */
-			using Value = T;
+			using value_type      = T;
+
+			/* self type */
+			using self            = xns::stack<T>;
 
 			/* size type */
-			using Size = xns::size_t;
+			using size_type       = xns::size_t;
 
 			/* reference type */
-			using Reference = Value&;
+			using reference       = value_type&;
 
 			/* const reference type */
-			using ConstReference = const Value&;
+			using const_reference = const value_type&;
 
 			/* move reference type */
-			using MoveReference = Value&&;
+			using move_reference  = value_type&&;
 
 			/* pointer type */
-			using Pointer = Value*;
+			using mutable_pointer = value_type*;
 
 			/* const pointer type */
-			using ConstPointer = const Value*;
+			using const_pointer   = const value_type*;
 
 
 		private:
 
 			/* forward declaration */
-			struct Node;
+			struct node;
 
 
 			// -- P R I V A T E  A L I A S E S --------------------------------
 
 			/* node pointer type */
-			using NodePointer = Node*;
+			using node_pointer = node*;
 
 			/* allocator type */
-			using Allocator = xns::allocator<Xf::Stack<T>::Node>;
+			using allocator = xns::allocator<xns::stack<T>::node>;
 
 
 		public:
@@ -62,16 +68,16 @@ namespace Xf {
 			// -- C O N S T R U C T O R S -------------------------------------
 
 			/* default constructor */
-			explicit Stack(void) noexcept
+			explicit stack(void) noexcept
 			: _top{nullptr}, _storage{nullptr}, _size{0} {
 				// code here...
 			}
 
 			/* copy constructor */
-			Stack(const Stack& other)
-			: Stack{} {
+			stack(const self& other)
+			: stack{} {
 				// get other top
-				NodePointer node = other._top;
+				node_pointer node = other._top;
 				// loop through other
 				while (node) {
 					// push node value
@@ -82,24 +88,24 @@ namespace Xf {
 			}
 
 			/* move constructor */
-			Stack(Stack&& other) noexcept
+			stack(self&& other) noexcept
 			: _top{other._top}, _storage{other._storage}, _size{other._size} {
 				// invalidate other
 				other.initialize_members();
 			}
 
 			/* destructor */
-			~Stack(void) {
+			~stack(void) {
 				// get top node
-				NodePointer node = _top;
+				node_pointer node = _top;
 				// loop through stack
 				while (node) {
 					// get next node
-					NodePointer next = node->_next;
+					node_pointer next = node->_next;
 					// destroy node
-					Allocator::destroy(node);
+					allocator::destroy(node);
 					// deallocate node
-					Allocator::deallocate(node);
+					allocator::deallocate(node);
 					// set node to next
 					node = next;
 				} // get storage node
@@ -107,9 +113,9 @@ namespace Xf {
 				// loop through storage
 				while (node) {
 					// get next node
-					NodePointer next = node->_next;
+					node_pointer next = node->_next;
 					// deallocate node
-					Allocator::deallocate(node);
+					allocator::deallocate(node);
 					// set node to next
 					node = next;
 				}
@@ -119,13 +125,13 @@ namespace Xf {
 			// -- O P E R A T O R S -------------------------------------------
 
 			/* copy operator */
-			Stack& operator=(const Stack& other) {
+			self& operator=(const self& other) {
 				// check self assignment
 				if (this != &other) {
 					// clear self (keep own storage)
 					clear();
 					// get other top
-					NodePointer node = other._top;
+					node_pointer node = other._top;
 					// loop through other stack
 					while (node) {
 						// push node value
@@ -137,7 +143,7 @@ namespace Xf {
 			}
 
 			/* move operator */ // WARNING: doesn't move storage
-			Stack& operator=(Stack&& other) noexcept {
+			self& operator=(self&& other) noexcept {
 				// check self assignment
 				if (this != &other) {
 					// clear self (keep own storage)
@@ -158,19 +164,19 @@ namespace Xf {
 			// -- A C C E S S O R S -------------------------------------------
 
 			/* top */
-			Reference top(void) {
+			reference top(void) {
 				// return top value reference
 				return _top->_value;
 			}
 
 			/* const top */
-			ConstReference top(void) const {
+			const_reference top(void) const {
 				// return top value const reference
 				return _top->_value;
 			}
 
 			/* size */
-			Size size(void) const {
+			size_type size(void) const {
 				// return size
 				return _size;
 			}
@@ -186,13 +192,13 @@ namespace Xf {
 			// -- M E T H O D S -----------------------------------------------
 
 			/* push */
-			void push(ConstReference value) {
+			void push(const_reference value) {
 				// node pointer
-				NodePointer node = new_node();
+				node_pointer node = new_node();
 				// check if pointer is valid
 				if (!node) { return; }
 				// construct node
-				Allocator::construct(node, value);
+				allocator::construct(node, value);
 				// link node
 				link(node);
 				// increment size
@@ -200,13 +206,13 @@ namespace Xf {
 			}
 
 			/* move push */
-			void push(MoveReference value) {
+			void push(move_reference value) {
 				// node pointer
-				NodePointer node = new_node();
+				node_pointer node = new_node();
 				// check if pointer is valid
 				if (!node) { return; }
 				// construct node
-				Allocator::construct(node, xns::move(value));
+				allocator::construct(node, xns::move(value));
 				// link node
 				link(node);
 				// increment size
@@ -217,11 +223,11 @@ namespace Xf {
 			template <typename... A>
 			void emplace(A&&... arguments) {
 				// node pointer
-				NodePointer node = new_node();
+				node_pointer node = new_node();
 				// check if pointer is valid
 				if (!node) { return; }
 				// construct node
-				Allocator::construct(node, xns::forward<A>(arguments)...);
+				allocator::construct(node, xns::forward<A>(arguments)...);
 				// link node
 				link(node);
 				// increment size
@@ -231,11 +237,11 @@ namespace Xf {
 			/* pop */
 			void pop(void) {
 				// unlink node
-				NodePointer node = unlink();
+				node_pointer node = unlink();
 				// check if pointer is valid
 				if (!node) { return; }
 				// destroy node
-				Allocator::destroy(node);
+				allocator::destroy(node);
 				// link node to storage
 				link_storage(node);
 				// decrement size
@@ -246,22 +252,24 @@ namespace Xf {
 			/* shrink */
 			void shrink(void) {
 				// node pointer
-				NodePointer node;
+				node_pointer node;
 				// loop through storage
 				while ((node = unlink_storage())) {
 					// deallocate node
-					Allocator::deallocate(node);
+					allocator::deallocate(node);
 				}
+				// reset storage
+				_storage = nullptr;
 			}
 
 			/* clear */
 			void clear(void) {
 				// node pointer
-				NodePointer node;
+				node_pointer node;
 				// loop through stack
 				while ((node = unlink())) {
 					// destroy node
-					Allocator::destroy(node);
+					allocator::destroy(node);
 					// link node to storage
 					link_storage(node);
 				} // reset size
@@ -272,7 +280,7 @@ namespace Xf {
 			void print(void) {
 				std::cout << "size: " << _size << std::endl;
 				// get top node
-				NodePointer node = _top;
+				node_pointer node = _top;
 				// loop through stack
 				while (node) {
 					// print value
@@ -287,24 +295,24 @@ namespace Xf {
 			// -- P R I V A T E  M E T H O D S --------------------------------
 
 			/* new node */
-			NodePointer new_node(void) {
+			node_pointer new_node(void) {
 				// check if free nodes are available
 				if (_storage) { return unlink_storage(); }
 				// else allocate new node
-				return Allocator::allocate();
+				return allocator::allocate();
 			}
 
 
-			void link(NodePointer node) {
+			void link(node_pointer node) {
 				// link node's next pointer to top node
 				node->_next = _top;
 				// set top node
 				_top = node;
 			}
 
-			NodePointer unlink(void) {
+			node_pointer unlink(void) {
 				// get top node
-				NodePointer node = _top;
+				node_pointer node = _top;
 				// check if pointer is valid
 				if (_top) { _top = _top->_next; }
 				// return unlinked node
@@ -312,7 +320,7 @@ namespace Xf {
 			}
 
 			/* link storage */
-			void link_storage(NodePointer node) {
+			void link_storage(node_pointer node) {
 				// link node's next pointer to storage
 				node->_next = _storage;
 				// set storage
@@ -320,9 +328,9 @@ namespace Xf {
 			}
 
 			/* unlink storage */
-			NodePointer unlink_storage(void) {
+			node_pointer unlink_storage(void) {
 				// get storage node
-				NodePointer node = _storage;
+				node_pointer node = _storage;
 				// check if pointer is valid
 				if (_storage) { _storage = _storage->_next; }
 				// return unlinked node
@@ -342,13 +350,13 @@ namespace Xf {
 			// -- P R I V A T E  M E M B E R S --------------------------------
 
 			/* top */
-			NodePointer _top;
+			node_pointer _top;
 
 			/* storage */
-			NodePointer _storage;
+			node_pointer _storage;
 
 			/* size */
-			Size _size;
+			size_type _size;
 
 
 	};
@@ -356,68 +364,68 @@ namespace Xf {
 
 	// -- N O D E  C L A S S --------------------------------------------------
 
-	template <typename T>
-	struct Stack<T>::Node final {
+	template <class T>
+	struct stack<T>::node final {
 
 		public:
 
 			// -- C O N S T R U C T O R S -------------------------------------
 
 			/* default constructor */
-			Node(void)
+			node(void)
 			: _next(nullptr) {
 				// code here...
 			}
 
 			/* copy constructor */
 			//Node(Node& other)
-			Node(const Node& other) = delete;
+			node(const node& other) = delete;
 			/*Node(const Node& other)
 			: _value(other._value), _next(other._next) {
 				// code here...
 			}*/
 
 			/* move constructor */
-			Node(Node&& other)
+			node(node&& other)
 			: _value(xns::move(other._value)), _next(xns::move(other._next)) {
 				// code here...
 			}
 
 			/* value copy constructor */
-			Node(ConstReference value)
+			node(stack<T>::const_reference value)
 			: _value(value), _next(nullptr) {
 				// code here...
 			}
 
 			/* value move constructor */
-			Node(MoveReference value) noexcept
+			node(stack<T>::move_reference value) noexcept
 			: _value(xns::move(value)), _next(nullptr) {
 				// code here...
 			}
 
 			/* value emplace constructor */
 			template <typename... A>
-			Node(A&&... arguments)
+			node(A&&... arguments)
 			: _value(xns::forward<A>(arguments)...), _next(nullptr) {
 				// code here...
 			}
 
 			/* destructor */
-			~Node(void) {
+			~node(void) {
 				// initialize next
 				_next = nullptr;
 			}
 
-			Node& operator=(const Node& other) = delete;
+			node& operator=(const node& other) = delete;
 
 
 			// -- M E M B E R S -----------------------------------------------
 
 			/* value */
-			Value _value;
+			stack<T>::value_type _value;
 
 			/* next */
-			NodePointer _next;
+			stack<T>::node_pointer _next;
 
 
 	};
