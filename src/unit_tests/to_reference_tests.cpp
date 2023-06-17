5248a3446f4683eaa106aa4786d520094c63744c
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for to_reference

---
+++ src/unit_tests/to_reference_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"to_reference">(void) {
+		// start unit test
+		START_UT("to_reference");
+		// code here...
+		return false;
+	}
+	
