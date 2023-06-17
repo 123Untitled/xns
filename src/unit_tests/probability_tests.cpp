ea13304d13191476980733d83116606b252e6b04
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for probability

---
+++ src/unit_tests/probability_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"probability">(void) {
+		// start unit test
+		START_UT("probability");
+		// code here...
+		return false;
+	}
+	
