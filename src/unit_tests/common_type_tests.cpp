189a055a1ddc9767dd1a70765e652e0f146939c4
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for common_type

---
+++ src/unit_tests/common_type_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"common_type">(void) {
+		// start unit test
+		START_UT("common_type");
+		// code here...
+		return false;
+	}
+	
