b97d68251aee90b2b7450869cb9239485653094f
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for terminal

---
+++ src/unit_tests/terminal_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"terminal">(void) {
+		// start unit test
+		START_UT("terminal");
+		// code here...
+		return false;
+	}
+	
