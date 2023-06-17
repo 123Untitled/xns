258aaf762c061b40de0d9ac3cce3f07adcc433f9
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for index_of

---
+++ src/unit_tests/index_of_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"index_of">(void) {
+		// start unit test
+		START_UT("index_of");
+		// code here...
+		return false;
+	}
+	
