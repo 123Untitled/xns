0b7990fb52bd5e84481de4ddfc3ed56579fb93a0
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for make_unsigned

---
+++ src/unit_tests/make_unsigned_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"make_unsigned">(void) {
+		// start unit test
+		START_UT("make_unsigned");
+		// code here...
+		return false;
+	}
+	
