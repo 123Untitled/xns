ce65807615e61e469a8f42f7693bc091e58849a9
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for is_pointer

---
+++ src/unit_tests/is_pointer_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_pointer">(void) {
+		// start unit test
+		START_UT("is_pointer");
+		// code here...
+		return false;
+	}
+	
