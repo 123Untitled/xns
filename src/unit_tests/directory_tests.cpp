a9ed3b0732a964b9e052c264d74a97ef80be1217
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for directory

---
+++ src/unit_tests/directory_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"directory">(void) {
+		// start unit test
+		START_UT("directory");
+		// code here...
+		return false;
+	}
+	
