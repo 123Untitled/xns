b3e754c22df054ad2a8671b252290a01174bce3d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for pack_type

---
+++ src/unit_tests/pack_type_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"pack_type">(void) {
+		// start unit test
+		START_UT("pack_type");
+		// code here...
+		return false;
+	}
+	
