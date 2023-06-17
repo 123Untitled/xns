dc27df79fa1c4a4ba0d561d517a887d407660d0c
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for compare

---
+++ src/unit_tests/compare_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"compare">(void) {
+		// start unit test
+		START_UT("compare");
+		// code here...
+		return false;
+	}
+	
