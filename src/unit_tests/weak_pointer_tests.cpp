68f91daded46f1168fad15c03b69f4e14ef60681
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for weak_pointer

---
+++ src/unit_tests/weak_pointer_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"weak_pointer">(void) {
+		// start unit test
+		START_UT("weak_pointer");
+		// code here...
+		return false;
+	}
+	
