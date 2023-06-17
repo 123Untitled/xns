cce7750382d6ec08352d9a4aac4e3a62e6f8e0b6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for escape

---
+++ src/unit_tests/escape_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"escape">(void) {
+		// start unit test
+		START_UT("escape");
+		// code here...
+		return false;
+	}
+	
