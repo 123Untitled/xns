d0df2b80b1b1869d2c229836f339320e2992d3c7
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_char

---
+++ src/unit_tests/is_char_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_char">(void) {
+		// start unit test
+		START_UT("is_char");
+		// code here...
+		return false;
+	}
+	
