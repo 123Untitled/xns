3b93e7229588496e255e9c37371ff994b887b6c6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for function

---
+++ src/unit_tests/function_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"function">(void) {
+		// start unit test
+		START_UT("function");
+		// code here...
+		return false;
+	}
+	
