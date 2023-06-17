cb9d3533eb43400f6535703e652831b540e79de1
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_floating

---
+++ src/unit_tests/is_floating_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_floating">(void) {
+		// start unit test
+		START_UT("is_floating");
+		// code here...
+		return false;
+	}
+	
