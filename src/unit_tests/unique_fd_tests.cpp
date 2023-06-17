55c182b6ebe04d099178245b938fcfc430414caa
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for unique_fd

---
+++ src/unit_tests/unique_fd_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"unique_fd">(void) {
+		// start unit test
+		START_UT("unique_fd");
+		// code here...
+		return false;
+	}
+	
