d645ae56a8cdf3c2587e6cf08628f2ebc919cb7e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for is_scalar

---
+++ src/unit_tests/is_scalar_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_scalar">(void) {
+		// start unit test
+		START_UT("is_scalar");
+		// code here...
+		return false;
+	}
+	
