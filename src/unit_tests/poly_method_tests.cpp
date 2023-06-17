463fcb3af3e0d482cb04a21256ed96f24c7e8cd9
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for poly_method

---
+++ src/unit_tests/poly_method_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"poly_method">(void) {
+		// start unit test
+		START_UT("poly_method");
+		// code here...
+		return false;
+	}
+	
