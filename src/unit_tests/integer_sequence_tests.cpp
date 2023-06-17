04a5f7679b19087818190d4c8e632d0680d492dc
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for integer_sequence

---
+++ src/unit_tests/integer_sequence_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"integer_sequence">(void) {
+		// start unit test
+		START_UT("integer_sequence");
+		// code here...
+		return false;
+	}
+	
