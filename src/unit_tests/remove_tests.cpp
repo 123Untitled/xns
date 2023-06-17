120037b9260d67b983fd14760205d07b98f4672c
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for remove

---
+++ src/unit_tests/remove_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"remove">(void) {
+		// start unit test
+		START_UT("remove");
+		// code here...
+		return false;
+	}
+	
