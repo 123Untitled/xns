dae48f65fe23cf24584ff3eb40aef1732631df0e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_arithmetic

---
+++ src/unit_tests/is_arithmetic_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_arithmetic">(void) {
+		// start unit test
+		START_UT("is_arithmetic");
+		// code here...
+		return false;
+	}
+	
