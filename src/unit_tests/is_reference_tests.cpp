575f9dcc6026ee906b153d3bb7f180a4bb9bd51d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for is_reference

---
+++ src/unit_tests/is_reference_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_reference">(void) {
+		// start unit test
+		START_UT("is_reference");
+		// code here...
+		return false;
+	}
+	
