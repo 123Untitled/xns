3e8953152d4cc5b9390df1bff54c1053203b652f
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for declval

---
+++ src/unit_tests/declval_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"declval">(void) {
+		// start unit test
+		START_UT("declval");
+		// code here...
+		return false;
+	}
+	
