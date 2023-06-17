8fea852ed23fc45955ae82bd73de32c36660b017
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:29 +0200

    add unit test for variant

---
+++ src/unit_tests/variant_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"variant">(void) {
+		// start unit test
+		START_UT("variant");
+		// code here...
+		return false;
+	}
+	
