f53297b39f1ec84d99a864e72e169d8384680cf2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for integral

---
+++ src/unit_tests/integral_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"integral">(void) {
+		// start unit test
+		START_UT("integral");
+		// code here...
+		return false;
+	}
+	
