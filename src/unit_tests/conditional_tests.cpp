8440d736d202643a4e52000c55473cfebd786b0e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for conditional

---
+++ src/unit_tests/conditional_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"conditional">(void) {
+		// start unit test
+		START_UT("conditional");
+		// code here...
+		return false;
+	}
+	
