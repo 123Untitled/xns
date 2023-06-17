fd3ffb71524bc46ba6e51e7bb9113bc515faad46
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for add_const

---
+++ src/unit_tests/add_const_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"add_const">(void) {
+		// start unit test
+		START_UT("add_const");
+		// code here...
+		return false;
+	}
+	
