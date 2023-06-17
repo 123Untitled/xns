8fb1d1ef0d310c3fb0a94a0c4fca42e3857ea484
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for is_void

---
+++ src/unit_tests/is_void_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_void">(void) {
+		// start unit test
+		START_UT("is_void");
+		// code here...
+		return false;
+	}
+	
