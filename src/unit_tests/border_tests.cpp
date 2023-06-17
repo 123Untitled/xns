5465063e0bb4d7f084580111192fd64c8a9e4758
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for border

---
+++ src/unit_tests/border_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"border">(void) {
+		// start unit test
+		START_UT("border");
+		// code here...
+		return false;
+	}
+	
