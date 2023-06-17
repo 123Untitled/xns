a6aa910c2a76bdc19044f4c611956105bc6ad456
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for parameter_pack

---
+++ src/unit_tests/parameter_pack_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"parameter_pack">(void) {
+		// start unit test
+		START_UT("parameter_pack");
+		// code here...
+		return false;
+	}
+	
