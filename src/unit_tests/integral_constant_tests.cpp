bd9783db9a5dd13e5b4704ffbfc29f6825e4b427
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for integral_constant

---
+++ src/unit_tests/integral_constant_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"integral_constant">(void) {
+		// start unit test
+		START_UT("integral_constant");
+		// code here...
+		return false;
+	}
+	
