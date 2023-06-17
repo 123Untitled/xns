ed48623b280585b5e34863b7364d014fd83a99b8
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:29 +0200

    add unit test for conversions

---
+++ src/unit_tests/conversions_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"conversions">(void) {
+		// start unit test
+		START_UT("conversions");
+		// code here...
+		return false;
+	}
+	
