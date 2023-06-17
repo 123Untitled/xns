537554c5e4c00b1d3839565524149297f0ea1775
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for string_literal

---
+++ src/unit_tests/string_literal_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"string_literal">(void) {
+		// start unit test
+		START_UT("string_literal");
+		// code here...
+		return false;
+	}
+	
