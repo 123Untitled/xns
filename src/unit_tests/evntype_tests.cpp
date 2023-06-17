bdfa14ed65d210687e8d0704b786eca9bdb9553a
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for evntype

---
+++ src/unit_tests/evntype_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"evntype">(void) {
+		// start unit test
+		START_UT("evntype");
+		// code here...
+		return false;
+	}
+	
