aa74d6fad24f7f39ebc0788967d5a56a31470093
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for shared_pointer

---
+++ src/unit_tests/shared_pointer_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"shared_pointer">(void) {
+		// start unit test
+		START_UT("shared_pointer");
+		// code here...
+		return false;
+	}
+	
