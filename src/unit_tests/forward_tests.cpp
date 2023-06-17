5c6bf88f21aba3b3029209c09823da46a129163a
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for forward

---
+++ src/unit_tests/forward_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"forward">(void) {
+		// start unit test
+		START_UT("forward");
+		// code here...
+		return false;
+	}
+	
