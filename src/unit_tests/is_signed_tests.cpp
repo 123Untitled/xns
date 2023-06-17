13ec8209738fa00d672f5536e38a36e21c4cf366
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for is_signed

---
+++ src/unit_tests/is_signed_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_signed">(void) {
+		// start unit test
+		START_UT("is_signed");
+		// code here...
+		return false;
+	}
+	
