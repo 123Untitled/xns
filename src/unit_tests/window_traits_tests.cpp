7c037bdd5d4f90b9a2d2705c86f2c3dfda2bd8f2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for window_traits

---
+++ src/unit_tests/window_traits_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"window_traits">(void) {
+		// start unit test
+		START_UT("window_traits");
+		// code here...
+		return false;
+	}
+	
