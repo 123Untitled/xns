6bc08a171548f3c99c761b755cbee19ad0cf2e28
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_one_of

---
+++ src/unit_tests/is_one_of_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_one_of">(void) {
+		// start unit test
+		START_UT("is_one_of");
+		// code here...
+		return false;
+	}
+	
