9aeefd2b0ed1872d934c9a0afef5774ada73747e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_null

---
+++ src/unit_tests/is_null_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_null">(void) {
+		// start unit test
+		START_UT("is_null");
+		// code here...
+		return false;
+	}
+	
