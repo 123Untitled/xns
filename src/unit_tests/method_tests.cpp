e2f97c2e3726dba0c0e0ae5a21087c15c0ec4c07
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for method

---
+++ src/unit_tests/method_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"method">(void) {
+		// start unit test
+		START_UT("method");
+		// code here...
+		return false;
+	}
+	
