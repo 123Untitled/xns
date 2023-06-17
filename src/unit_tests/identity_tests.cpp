a5b6526bbcc96f0eabe77643e6d019759f94b905
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for identity

---
+++ src/unit_tests/identity_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"identity">(void) {
+		// start unit test
+		START_UT("identity");
+		// code here...
+		return false;
+	}
+	
