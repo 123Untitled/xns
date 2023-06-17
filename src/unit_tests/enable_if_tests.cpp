da82640eb42864d8d78862fe6f4dab7de03a38e0
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for enable_if

---
+++ src/unit_tests/enable_if_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"enable_if">(void) {
+		// start unit test
+		START_UT("enable_if");
+		// code here...
+		return false;
+	}
+	
