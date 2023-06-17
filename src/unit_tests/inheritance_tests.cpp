b89663ee54ee1ce4c53aea2617478eef98090970
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for inheritance

---
+++ src/unit_tests/inheritance_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"inheritance">(void) {
+		// start unit test
+		START_UT("inheritance");
+		// code here...
+		return false;
+	}
+	
