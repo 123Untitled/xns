f40a173781aeb8151e6edbc242a1b55a244ed4f7
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for is_same

---
+++ src/unit_tests/is_same_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_same">(void) {
+		// start unit test
+		START_UT("is_same");
+		// code here...
+		return false;
+	}
+	
