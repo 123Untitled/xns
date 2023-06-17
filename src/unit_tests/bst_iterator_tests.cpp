5e2e7fe3486ac12d954e4e16801d75225f001591
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for bst_iterator

---
+++ src/unit_tests/bst_iterator_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"bst_iterator">(void) {
+		// start unit test
+		START_UT("bst_iterator");
+		// code here...
+		return false;
+	}
+	
