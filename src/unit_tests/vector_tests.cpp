676cca1b7de43d2d962978526fc150bcc9a14ee1
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:29 +0200

    add unit test for vector

---
+++ src/unit_tests/vector_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"vector">(void) {
+		// start unit test
+		START_UT("vector");
+		// code here...
+		return false;
+	}
+	
