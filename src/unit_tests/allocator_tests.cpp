acbd66055f3418ca74289a9cb6432324d1010c59
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for allocator

---
+++ src/unit_tests/allocator_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"allocator">(void) {
+		// start unit test
+		START_UT("allocator");
+		// code here...
+		return false;
+	}
+	
