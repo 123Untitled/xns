45e9e885b0bc88b7f5093901e968c10c2b12a153
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:29 +0200

    add unit test for camera

---
+++ src/unit_tests/camera_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"camera">(void) {
+		// start unit test
+		START_UT("camera");
+		// code here...
+		return false;
+	}
+	
