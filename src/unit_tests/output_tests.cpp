48b3cce84b36e42ea8b2929b26d2f5dbc5f638f6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for output

---
+++ src/unit_tests/output_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"output">(void) {
+		// start unit test
+		START_UT("output");
+		// code here...
+		return false;
+	}
+	
