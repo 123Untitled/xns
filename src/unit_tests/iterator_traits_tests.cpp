ed548f5740529b1dd642d56bc719f75160b496a7
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for iterator_traits

---
+++ src/unit_tests/iterator_traits_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"iterator_traits">(void) {
+		// start unit test
+		START_UT("iterator_traits");
+		// code here...
+		return false;
+	}
+	
