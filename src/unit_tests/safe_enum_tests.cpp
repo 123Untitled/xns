235678ac0cc25d6d83c3b37cc766e320e9835f78
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for safe_enum

---
+++ src/unit_tests/safe_enum_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"safe_enum">(void) {
+		// start unit test
+		START_UT("safe_enum");
+		// code here...
+		return false;
+	}
+	
