61cdba1c913da981de3f46d217fb3374161a7300
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_member_pointer

---
+++ src/unit_tests/is_member_pointer_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_member_pointer">(void) {
+		// start unit test
+		START_UT("is_member_pointer");
+		// code here...
+		return false;
+	}
+	
