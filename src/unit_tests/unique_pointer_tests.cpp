7760034d230c892ad2711e5a8857707cadd3dec0
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for unique_pointer

---
+++ src/unit_tests/unique_pointer_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"unique_pointer">(void) {
+		// start unit test
+		START_UT("unique_pointer");
+		// code here...
+		return false;
+	}
+	
