601518f1c0c21d726630c9e59120246dd8fa3b71
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_array

---
+++ src/unit_tests/is_array_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_array">(void) {
+		// start unit test
+		START_UT("is_array");
+		// code here...
+		return false;
+	}
+	
