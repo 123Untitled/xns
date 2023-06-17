aa266505f1c0619ad2d7a93de66c5a703b50e436
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-17 02:13:01 +0200

    rect is now a template class

--- inc/visual/rect.hpp
+++ inc/visual/rect.hpp
@@ -2,7 +2,11 @@
 #define RECT_HEADER
 
 #include <iostream>
+
 #include "types.hpp"
+#include "is_arithmetic.hpp"
+#include "move.hpp"
+
 
 #define PRECT(rect) std::cout << "Rect(" << rect.x << ", " << rect.y << ", " << rect.w << ", " << rect.h << ")" << std::endl;
 
@@ -11,67 +15,128 @@
 
 namespace xns {
 
+
 	// -- R E C T  S T R U C T ------------------------------------------------
 
+	template <class T>
 	struct rect final {
 
 
-		// -- A L I A S E S ---------------------------------------------------
+		// -- assertions ------------------------------------------------------
+
+		/* check T is an arithmetic type */
+		static_assert(xns::is_arithmetic<T>, "): T MUST BE AN ARITHMETIC TYPE :(");
+
+
+		// -- types -----------------------------------------------------------
+
+		/* self type */
+		using self = rect<T>;
 
 		/* size type */
-		using size_type = xns::size_t;
+		using size_type = T;
 
 
-		// -- M E M B E R S ---------------------------------------------------
+		// -- members ---------------------------------------------------------
 
 		size_type x, y, w, h;
 
 
-		// -- C O N S T R U C T O R S -----------------------------------------
+		// -- constructors ----------------------------------------------------
 
 		/* default constructor */
-		rect(void) noexcept;
+		rect(void) noexcept
+		: x{0}, y{0}, w{0}, h{0} {}
 
 		/* size and position constructor */
 		rect(   const size_type x,
 				const size_type y,
 				const size_type w,
-				const size_type h) noexcept;
+				const size_type h) noexcept
+		: x{x}, y{y}, w{w}, h{h} {}
 
 		/* copy constructor */
-		rect(const rect& other) noexcept;
+		rect(const self& other) noexcept
+		: x{other.x}, y{other.y}, w{other.w}, h{other.h} {}
 
 		/* move constructor */
-		rect(rect&& other) noexcept;
+		rect(self&& other) noexcept
+		: rect{other} {}
 
 		/* destructor */
-		~rect(void) noexcept;
+		~rect(void) noexcept = default;
 
 
-		// -- O P E R A T O R S -----------------------------------------------
+		// -- assignment operators --------------------------------------------
 
 		/* copy assignment operator */
-		rect& operator=(const rect& other) noexcept;
+		self& operator=(const self& other) noexcept {
+			// check for self assignment
+			if (this != &other) {
+				// copy dimension
+				w = other.w; h = other.h;
+				// copy position
+				x = other.x; y = other.y;
+			} // return self reference
+			return *this;
+		}
 
 		/* move assignment operator */
-		rect& operator=(rect&& other) noexcept;
+		self& operator=(self&& other) noexcept {
+			// call copy assignment
+			return operator=(other);
+		}
+
+
+		// -- boolean operators -----------------------------------------------
 
 		/* bool operator */
-		explicit operator bool(void) const noexcept;
+		explicit operator bool(void) const noexcept {
+			// check if empty
+			return !empty();
+		}
 
 		/* bool not operator */
-		bool operator!(void) const noexcept;
+		bool operator!(void) const noexcept {
+			// check if empty
+			return empty();
+		}
+
+
+		// -- comparison operators --------------------------------------------
 
 		/* equality operator */
-		bool operator==(const rect& other) const noexcept;
+		bool operator==(const self& other) const noexcept {
+			// return equality
+			return x == other.x && y == other.y && w == other.w && h == other.h;
+		}
 
-		// -- M E T H O D S ---------------------------------------------------
+
+		// -- accessors -------------------------------------------------------
 
 		/* empty */
-		bool empty(void) const noexcept;
+		bool empty(void) const noexcept {
+			// check if empty
+			return w == 0 || h == 0;
+		}
 
 
 	};
+
+
+	// -- R E C T  T Y P E S --------------------------------------------------
+
+	/* float rect */
+	using rect_float = rect<float>;
+
+	/* double rect */
+	using rect_double = rect<double>;
+
+	/* int rect */
+	using rect_int = rect<int>;
+
+	/* unsigned rect */
+	using rect_uint = rect<unsigned>;
 
 
 }
