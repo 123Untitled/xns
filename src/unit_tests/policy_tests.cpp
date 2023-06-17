5d734a617d876e7cba386c8c694074c0541258b6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for policy

---
+++ src/unit_tests/policy_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"policy">(void) {
+		// start unit test
+		START_UT("policy");
+		// code here...
+		return false;
+	}
+	
