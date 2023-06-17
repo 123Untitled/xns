7f72d56a9970466c0bd12ef43e76a29ed7254f94
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:31 +0200

    add unit test for make_signed

---
+++ src/unit_tests/make_signed_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"make_signed">(void) {
+		// start unit test
+		START_UT("make_signed");
+		// code here...
+		return false;
+	}
+	
