27ed0ffa8f94c9b8048524eac9a5de8081c6d3e0
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:29 +0200

    add unit test for bit_cast

---
+++ src/unit_tests/bit_cast_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"bit_cast">(void) {
+		// start unit test
+		START_UT("bit_cast");
+		// code here...
+		return false;
+	}
+	
