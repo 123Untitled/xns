dfeee0492c6e3eaff8eaa3fef73324c6882d5ebd
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_all_unique

---
+++ src/unit_tests/is_all_unique_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_all_unique">(void) {
+		// start unit test
+		START_UT("is_all_unique");
+		// code here...
+		return false;
+	}
+	
