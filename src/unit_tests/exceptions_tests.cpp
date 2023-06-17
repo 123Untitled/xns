a9c4566b5c6f0758d9d0f22106ce7ac7ebb5aca8
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for exceptions

---
+++ src/unit_tests/exceptions_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"exceptions">(void) {
+		// start unit test
+		START_UT("exceptions");
+		// code here...
+		return false;
+	}
+	
