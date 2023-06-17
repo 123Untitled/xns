644e536c7b902ac47d9c38d4ccda282a4151509b
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for char_sequence

---
+++ src/unit_tests/char_sequence_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"char_sequence">(void) {
+		// start unit test
+		START_UT("char_sequence");
+		// code here...
+		return false;
+	}
+	
