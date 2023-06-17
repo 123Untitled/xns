253e771beaaf3b7fa7dccd3a5dead6f2af0e1488
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 01:01:30 +0200

    add unit test for is_const

---
+++ src/unit_tests/is_const_tests.cpp
@@ -0,0 +1,11 @@
+#include "unit_tests.hpp"
+
+	/* unit test */
+	template <>
+	bool unit_test<"is_const">(void) {
+		// start unit test
+		START_UT("is_const");
+		// code here...
+		return false;
+	}
+	
