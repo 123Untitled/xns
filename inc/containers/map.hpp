adfb951f4a72e503e431029e1d2b9b45b86cb9bb
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-18 16:51:02 +0200

    update map

--- inc/containers/map.hpp
+++ inc/containers/map.hpp
@@ -3,169 +3,186 @@
 
 #include "types.hpp"
 #include "allocator.hpp"
-#include "unique_pointer.hpp"
-#include "weak_pointer.hpp"
+#include "unique_ptr.hpp"
+#include "weak_ptr.hpp"
+#include "tree.hpp"
+#include "literal_map.hpp"
+#include "tuple.hpp"
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
 namespace xns {
 
+	// -- is convertible ------------------------------------------------------
 
-	// -- M A P  C L A S S ----------------------------------------------------
+	template <class T, class U>
+	concept is_convertible = requires(T a) {
+		{ static_cast<U>(a) };
+	};
 
-//	template <class K, class V>
-//	class map final {
-//
-//		public:
-//
-//			// -- T Y P E S ---------------------------------------------------
-//
-//			/* key type */
-//			using key = K;
-//
-//			/* value type */
-//			using value_type = V;
-//
-//			/* size type */
-//			using size_type = xns::size_t;
-//
-//			/* reference type */
-//			using reference = value_type&;
-//
-//			/* const reference type */
-//			using const_reference = const value_type&;
-//
-//			/* move reference type */
-//			using move_reference = value_type&&;
-//
-//			/* pointer type */
-//			using mutable_pointer = value_type*;
-//
-//			/* const pointer type */
-//			using const_pointer = const value_type*;
-//
-//			/* allocator type */
-//			using allocator = xns::allocator<value_type>;
-//
-//
-//		private:
-//
-//			/* forward declaration */
-//			struct node;
-//
-//
-//			// -- P R I V A T E  T Y P E S ------------------------------------
-//
-//			/* node pointer type */
-//			using node_pointer = node*;
-//
-//			/* weak node pointer type */
-//			using weak_node = xns::weak_ptr<node>;
-//
-//			/* unique node pointer type */
-//			using unique_node = xns::unique_ptr<node>;
-//
-//
-//
-//			// -- P R I V A T E  M E M B E R S --------------------------------
-//
-//			/* root node */
-//			unique_node _root;
-//
-//			/* storage */
-//			unique_node _storage;
-//
-//			/* size */
-//			size_type _size;
-//
-//
-//		public:
-//
-//			// -- C O N S T R U C T O R S -------------------------------------
-//
-//			/* default constructor */
-//			map(void)
-//			: _root {nullptr}, _storage {nullptr}, _size {0} {
-//				// code here...
-//			}
-//
-//
-//			// -- I N S E R T -------------------------------------------------
-//
-//
-//			/* copy insert */
-//			void copy_insert(const key& key, const_reference value) {
-//
-//				unique_node* nde  = &_root;
-//				weak_node parent  = nullptr;
-//
-//				// loop until node is null
-//				while (*nde) {
-//					// keep track of parent
-//					parent = *nde;
-//					// less compare
-//					if      (key < (*nde)->_value) { nde = &(*nde)->_left; }
-//					// greater compare
-//					else if (key > (*nde)->_value) { nde = &(*nde)->_right; }
-//					// equal compare, replace value ?
-//					else                         {
-//						//node->_value = value;
-//						return;
-//					}
-//				}
-//
-//				// create new node
-//				//*nde = xns::make_unique_pointer<node>(value, parent);
-//
-//
-//			}
-//
-//			/* move insert */
-//			void move_insert(const key& key, move_reference value) {
-//				// code here...
-//			}
-//
-//			/* emplace insert */
-//			template <class... A>
-//			void emplace_insert(const key& key, A&&... args) {
-//				// code here...
-//			}
-//
-//
-//
-//
-//
-//		private:
-//
-//			// -- N O D E  S T R U C T ----------------------------------------
-//
-//			struct node final {
-//
-//
-//				// -- M E M B E R S -------------------------------------------
-//
-//				/* value */
-//				value_type _value;
-//
-//				/* left node */
-//				unique_node _left;
-//
-//				/* right node */
-//				unique_node _right;
-//
-//				/* parent node */
-//				weak_node _parent;
-//
-//				/* depth */
-//				size_type _depth;
-//
-//
-//			};
-//
-//
-//
-//	};
+	// -- is comparable -------------------------------------------------------
+
+	template <class T>
+	concept is_comparable = requires(T a, T b) {
+		{ a == b } -> is_convertible<bool>;
+		{ a != b } -> is_convertible<bool>;
+		{ a < b } -> is_convertible<bool>;
+		{ a > b } -> is_convertible<bool>;
+		{ a <= b } -> is_convertible<bool>;
+		{ a >= b } -> is_convertible<bool>;
+	};
+
+
+
+	// -- M A P ---------------------------------------------------------------
+
+	template <class K, class V>
+	class map final {
+
+		static_assert(is_comparable<K>, "K must be comparable");
+
+		public:
+
+			// -- public types ------------------------------------------------
+
+			/* key type */
+			using key_type = K;
+
+			/* value type */
+			using value_type = V;
+
+			/* size type */
+			using size_type = xns::size_t;
+
+			/* reference type */
+			using reference = value_type&;
+
+			/* const reference type */
+			using const_reference = const value_type&;
+
+			/* move reference type */
+			using move_reference = value_type&&;
+
+			/* pointer type */
+			using mutable_pointer = value_type*;
+
+			/* const pointer type */
+			using const_pointer = const value_type*;
+
+
+			/* pair type */
+			using pair_type = xns::pair<key_type, value_type>;
+
+			/* tree type */
+			using tree_type = xns::tree<pair_type>;
+
+			/* node pointer type */
+			using node_pointer = typename tree_type::node_pointer;
+
+
+
+		private:
+
+			// -- private members ---------------------------------------------
+
+			/* tree */
+			tree_type _tree;
+
+
+		public:
+
+			// -- C O N S T R U C T O R S -------------------------------------
+
+			/* default constructor */
+			map(void)
+			: _tree{} {
+				// nothing to do...
+			}
+
+
+			// -- I N S E R T -------------------------------------------------
+
+
+			/* copy insert */
+			void insert(const key_type& key, const value_type& value) {
+
+				node_pointer node = _tree.root();
+
+				//node_pointer nde  = &_root;
+				node_pointer parent  = nullptr;
+
+				// loop until node is null
+				while (node) {
+					// keep track of parent
+					// less compare
+					pair_type& pair = node->value();
+
+					if      (key < pair._first) {
+						parent = node;
+						node = node->left();
+					}
+					// greater compare
+					else if (key > pair._first) {
+						parent = node;
+						node = node->right();
+					}
+					// equal compare, replace value ?
+					else                         {
+						//node->_value = value;
+						return;
+					}
+				}
+
+				// create new node
+				node = _tree.new_node(pair_type{key, value});
+				std::cout << "new node..." << std::endl;
+				std::cout << "key: " << node->value()._first << std::endl;
+
+				if (parent == nullptr) {
+					_tree.root(node);
+				}
+				else if (key < parent->value()._first) {
+					parent->set_left(node);
+				}
+				else {
+					parent->set_right(node);
+				}
+
+			}
+
+
+			/* move insert */
+			void move_insert(const key_type& key, move_reference value) {
+				// code here...
+			}
+
+			/* emplace insert */
+			template <class... A>
+			void emplace_insert(const key_type& key, A&&... args) {
+				// code here...
+			}
+
+			static xns::string vcall(const pair_type& pair) {
+				return pair._first;
+			}
+
+			static xns::size_t scall(const pair_type& pair) {
+				return pair._first.size();
+			}
+
+		void print(void) {
+			_tree.print(scall, vcall);
+		}
+
+
+
+
+		private:
+
+
+
+	};
 
 };
 
