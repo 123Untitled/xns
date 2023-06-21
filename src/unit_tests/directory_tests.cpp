492f562fd9ca9920125637aa1d149b2c7212308d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-22 00:04:47 +0200

    update filesystem unit test

--- src/unit_tests/directory_tests.cpp
+++ src/unit_tests/directory_tests.cpp
@@ -4,6 +4,27 @@
 /* unit test */
 template <>
 bool UT::unit_tests<"directory">(void) {
+
+
+
+	xns::filesystem::recursive_iterator it{"./inc"};
+	//xns::filesystem::iterator it{"."};
+	int i = 0;
+
+	while (it != nullptr) {
+
+		auto view = it.name();
+		auto path = it.path();
+
+		//write(1, view.data(), view.size());
+		//write(1, "\n", 1);
+		write(1, path.data(), path.size());
+		write(1, "\n", 1);
+		//if (i > 10) break;
+		//++i;
+		++it;
+	}
+
 	// code here...
 	return false;
 }
