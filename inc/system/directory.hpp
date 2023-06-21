4bb4a14998863e5f69e20098a19ac25e7c912d8e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-22 00:04:36 +0200

    improve file system iterator

--- inc/system/directory.hpp
+++ inc/system/directory.hpp
@@ -1,21 +1,22 @@
 #ifndef DIRECTORY_HEADER
 #define DIRECTORY_HEADER
 
+// local headers
+#include "string_literal.hpp"
 #include "string.hpp"
 #include "types.hpp"
 #include "path.hpp"
+#include "stack.hpp"
+#include "pair.hpp"
+#include "os.hpp"
 
-#include <dirent.h>
-#include <sys/param.h>
-#include <sys/stat.h>
-
-
-//#include <cstdio>
-//#include <string>
-//#include <cstdint>
-//#include <cstring>
-//#include <iostream>
-//#include <unistd.h>
+// macos headers
+#if defined(APPLE_OS)
+	#include <sys/dir.h>
+// linux headers
+#elif defined(LINUX_OS)
+	#include <dirent.h>
+#endif
 
 
 
@@ -33,88 +34,88 @@ namespace xns {
 
 		// -- D I R E C T O R Y  E N T R Y ------------------------------------
 
-		class directory_entry final {
+		//class directory_entry final {
 
-			public:
+		//	public:
 
-				// -- public constructors -------------------------------------
+		//		// -- public constructors -------------------------------------
 
-				/* default constructor */
-				directory_entry(void);
+		//		/* default constructor */
+		//		directory_entry(void);
 
-				/* member constructor */
-				directory_entry(const xns::string& path) {
-					stat(path.pointer(), &_stat);
-				}
+		//		/* member constructor */
+		//		directory_entry(const xns::string& path) {
+		//			stat(path.pointer(), &_stat);
+		//		}
 
-				/* copy constructor */
-				directory_entry(const directory_entry& other)
-				: _name{other._name}, _path{other._path}, _stat{other._stat} {
-				}
+		//		/* copy constructor */
+		//		directory_entry(const directory_entry& other)
+		//		: _name{other._name}, _path{other._path}, _stat{other._stat} {
+		//		}
 
-				/* destructor */
-				~directory_entry(void);
-
-
-			private:
-
-				// -- private types -------------------------------------------
-
-				/* file stat */
-				using filestat   = struct stat;
+		//		/* destructor */
+		//		~directory_entry(void);
 
 
-				// -- private members -----------------------------------------
+		//	private:
 
-				/* name of entity */
-				xns::string _name;
+		//		// -- private types -------------------------------------------
 
-				/* full path of entity */
-				xns::path<xns::string> _path;
-
-				/* file stat */
-				filestat    _stat;
-
-			public:
-
-				// -- public accessors ----------------------------------------
-
-				/* get name of entity */
-				const xns::string& get_name(void) const;
-
-				/* get full path of entity */
-				const xns::path<xns::string>& get_path(void) const;
-
-				/* get file size */
-				xns::size_t get_size(void) const;
+		//		/* file stat */
+		//		//using filestat   = struct stat;
 
 
-				// -- public boolean methods ----------------------------------
+		//		// -- private members -----------------------------------------
 
-				/* is directory */
-				bool is_directory(void) const;
+		//		/* name of entity */
+		//		xns::string _name;
 
-				/* is regular */
-				bool is_regular(void) const;
+		//		/* full path of entity */
+		//		xns::path<xns::string> _path;
 
-				/* is symbolic link */
-				bool is_symlink(void) const;
+		//		/* file stat */
+		//		//filestat    _stat;
 
-				/* is block */
-				bool is_block(void) const;
+		//	public:
 
-				/* is character */
-				bool is_character(void) const;
+		//		// -- public accessors ----------------------------------------
 
-				/* is fifo */
-				bool is_fifo(void) const;
+		//		/* get name of entity */
+		//		const xns::string& get_name(void) const;
 
-		};
+		//		/* get full path of entity */
+		//		const xns::path<xns::string>& get_path(void) const;
+
+		//		/* get file size */
+		//		xns::size_t get_size(void) const;
+
+
+		//		// -- public boolean methods ----------------------------------
+
+		//		/* is directory */
+		//		bool is_directory(void) const;
+
+		//		/* is regular */
+		//		bool is_regular(void) const;
+
+		//		/* is symbolic link */
+		//		bool is_symlink(void) const;
+
+		//		/* is block */
+		//		bool is_block(void) const;
+
+		//		/* is character */
+		//		bool is_character(void) const;
+
+		//		/* is fifo */
+		//		bool is_fifo(void) const;
+
+		//};
 
 
 		// -- I T E R A T O R -------------------------------------------------
 
-		template <bool recursive = false>
+		template <bool recursive>
 		class directory_iterator final {
 
 
@@ -122,77 +123,180 @@ namespace xns {
 
 				// -- private types -------------------------------------------
 
-				/* directory type */
-				using directory  = DIR;
+				/* container type */
+				using container = xns::conditional<recursive,
+												   xns::stack<xns::pair<DIR*, xns::string>>,
+												   xns::pair<DIR*, xns::string>>;
 
 				/* directory entry type */
-				using dir_entity = struct dirent;
+				using entity_ptr = struct dirent*;
+
+
+				// -- private enums -------------------------------------------
+
+				/* symbols enum */
+				enum : char {
+					POINT = 0x2E,
+					SEPARATOR = 0x2F
+				};
 
 
 			public:
+
+				// -- public types --------------------------------------------
+
+				/* self type */
+				using self = directory_iterator<recursive>;
+
 
 				// -- public constructors -------------------------------------
 
 				/* deleted default constructor */
 				directory_iterator(void) = delete;
 
+				/* nullptr constructor */
+				directory_iterator(xns::null)
+				: _container{}, _entity{nullptr} {}
 
-				/* string constructor */
-				directory_iterator(const xns::string& path)
-				: _directory{nullptr}, _entity{nullptr}, _path{path} {
+				/* path constructor (non-recursive) */
+				directory_iterator(const xns::string& path) requires (recursive == false)
+				: _container{}, _entity{nullptr} {
+					// check path validity
+					if (path.empty() == true) { return; }
+					// open directory and set pair
+					_container = xns::pair{::opendir(path.data()), path};
+					// check opening failure
+					if (_container._first == nullptr) {
+						write(STDERR_FILENO, "Error: could not open directory\n", 32);
+						return;
+					} // get first entry
+					operator++();
+				}
 
-					_directory = opendir(_path.get().pointer());
-					if (_directory == nullptr) {
+				/* path constructor (recursive) */
+				directory_iterator(const xns::string& path) requires (recursive == true)
+				: _container{}, _entity{nullptr} {
+
+					DIR* dir = ::opendir(path.data());
+					// check opening failure
+					if (dir == nullptr) {
 						write(STDERR_FILENO, "Error: could not open directory\n", 32);
 						return;
 					}
-					_entity = readdir(_directory);
+					// open directory
+					_container.push(xns::pair{dir, path});
+					operator++();
 				}
 
-				/* destructor */
-				~directory_iterator(void) {
-					if (_directory != nullptr)
-						closedir(_directory);
+				/* non-assignable class */
+				NON_ASSIGNABLE(directory_iterator);
+
+				/* destructor (non-recursive) */
+				~directory_iterator(void) requires (recursive == false) {
+					// close directory
+					if (_container._first != nullptr) {
+						::closedir(_container._first);
+					}
+				}
+
+				/* destructor (recursive) */
+				~directory_iterator(void) requires (recursive == true) {
+					// close all directories
+					while (_container.empty() == false) {
+						::closedir(_container.top()._first);
+						_container.pop();
+					}
 				}
 
 
 				// -- public increment operators ------------------------------
 
-				/* increment operator */
-				directory_iterator& operator++(void) {
-					// check if directory is open
-					if (_directory != nullptr) {
-						// get next entry
-						_entity = readdir(_directory);
-					} // return self reference
+				/* pre-increment operator (non-recursive) */
+				directory_iterator& operator++(void) requires (recursive == false) {
+					// get next entry
+					while ((_entity = ::readdir(_container._first)) != nullptr) {
+						// check for directory '.' or '..'
+						if (_entity->d_type == DT_DIR
+							&& skip() == true) { continue; }
+						// exit loop
+						break;
+					}
 					return *this;
 				}
 
-				/* increment operator */
+
+				/* pre-increment operator (recursive) */
+				directory_iterator& operator++(void) requires (recursive == true) {
+
+					while (_container.empty() == false) {
+
+						// get next entry
+						_entity = ::readdir(_container.top()._first);
+
+						// check for end of directory
+						if (_entity == nullptr) {
+							// close directory
+							::closedir(_container.top()._first);
+							_container.pop();
+							continue;
+						}
+
+						// check for directory
+						if (_entity->d_type == DT_DIR) {
+
+							// skip '.' and '..' directories
+							if (skip() == true) { continue; }
+
+							xns::string path{_container.top()._second};
+							path.append(SEPARATOR);
+							#if defined(APPLE_OS)
+								path.append(_entity->d_name, _entity->d_namlen);
+						    #else
+								path.append(_entity->d_name);
+							#endif
+
+							DIR* dir = ::opendir(path.data());
+
+							// check opening failure (need to set errno like status)
+							if (dir == nullptr) { break; }
+
+							_container.push(xns::pair{dir, xns::move(path)});
+							break;
+						}
+
+						break;
+					}
+					// return self reference
+					return *this;
+				}
+
+
+
+
+				/* post-increment operator */
 				directory_iterator& operator++(int) = delete;
 
-
-				// -- public accessors operators ------------------------------
-
-				/* dereference operator */
-				directory_entry operator*(void) const {
-
-					xns::filesystem::directory_entry entry{_path.get() + '/' + _entity->d_name};
-
-					return entry;
-				}
 
 
 				// -- comparison operators ------------------------------------
 
 				/* equality operator */
 				bool operator==(const directory_iterator& other) const {
-					return _entity == other._entity;
+					// compare inodes of entities
+					#if defined(BSD_OS) || defined(APPLE_OS)
+						return  _entity
+							&&  other._entity
+							&& (_entity->d_ino == other._entity->d_ino);
+					#elif defined(LINUX_OS)
+						return  _entity
+							&&  other._entity
+							&& (_entity->d_fileno == other._entity->d_fileno);
+					#endif
 				}
 
 				/* inequality operator */
 				bool operator!=(const directory_iterator& other) const {
-					return _entity != other._entity;
+					return operator==(other) == false;
 				}
 
 				/* null pointer equality operator */
@@ -226,8 +330,44 @@ namespace xns {
 					return _entity->d_name;
 				}
 
+				/* get path of entity */
+				xns::string path(void) const requires (recursive == true) {
+					xns::string path{_container.top()._second};
+					path.append(SEPARATOR);
+					#if defined(APPLE_OS)
+						path.append(_entity->d_name, _entity->d_namlen);
+					#else
+						path.append(_entity->d_name);
+					#endif
+
+					return path;
+				}
+
+				/* get path of entity */
+				xns::string path(void) const requires (recursive == false) {
+					xns::string path{_container._second};
+					path.append(SEPARATOR);
+					#if defined(APPLE_OS)
+						path.append(_entity->d_name, _entity->d_namlen);
+					#else
+						path.append(_entity->d_name);
+				    #endif
+
+					return path;
+				}
+
 
 				// -- public boolean methods ----------------------------------
+
+				/* is hidden */
+				bool is_hidden(void) const {
+					return _entity->d_name[0] == POINT;
+				}
+
+				/* is unknown */
+				bool is_unknown(void) const {
+					return _entity->d_type == DT_UNKNOWN;
+				}
 
 				/* is directory */
 				bool is_directory(void) const {
@@ -259,22 +399,45 @@ namespace xns {
 					return _entity->d_type == DT_FIFO;
 				}
 
+				/* is socket */
+				bool is_socket(void) const {
+					return _entity->d_type == DT_SOCK;
+				}
+
 
 
 			private:
 
+				// -- private utility methods ---------------------------------
 
-				/* directory */
-				directory* _directory;
+				/* check '.' and '..' directories */
+				inline bool skip(void) const {
+					// branchless ternary
+					return _entity->d_name[0] == POINT  ?
+						   _entity->d_name[1] == '\0' ? true :
+						   _entity->d_name[1] == POINT  ?
+						   _entity->d_name[2] == '\0' ? true : false : false : false;
+				}
+
+
+				// -- private members -----------------------------------------
+
+				/* directory container */
+				container _container;
 
 				/* directory entity */
-				dir_entity* _entity;
-
-				/* directory path */
-				xns::path<xns::string> _path;
-
+				entity_ptr _entity;
 
 		};
+
+
+		// -- iterator types --------------------------------------------------
+
+		/* linear iterator type */
+		using iterator = directory_iterator<false>;
+
+		/* recursive iterator type */
+		using recursive_iterator = directory_iterator<true>;
 
 
 	}
