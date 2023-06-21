a3f65f0a3c9bc2be81128993246673c855c77f4a
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-21 05:20:10 +0200

    use xorshift algorithm

--- inc/random/random.hpp
+++ inc/random/random.hpp
@@ -7,6 +7,14 @@
 
 #include "types.hpp"
 #include "macro.hpp"
+#include "is_integral.hpp"
+#include "is_signed.hpp"
+#include "is_unsigned.hpp"
+#include "make_signed.hpp"
+#include "make_unsigned.hpp"
+#include "numeric_limits.hpp"
+
+#include "xorshift.hpp"
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
@@ -32,6 +40,41 @@ namespace xns {
 
 			/* random boolean */
 			static bool random_bool(void);
+
+
+			/* random integer */
+			template <xns::is_arithmetic T>
+			static auto random_int(void) -> T {
+
+
+				// get random number
+				xns::u64 rnd = xns::xorshift::next64();
+
+				if constexpr (xns::is_floating<T>) {
+					T res = static_cast<T>(rnd) / static_cast<T>(xns::limits::max<xns::u64>());
+					return (T)(rnd % 10) + res;
+					//return static_cast<T>(rnd);
+				}
+
+				if constexpr (xns::is_same<T, decltype(rnd)>) { return rnd; }
+
+
+				if constexpr (xns::is_unsigned<T>) {
+					return static_cast<T>(rnd % (xns::limits::max<T>() + 1));
+				}
+
+
+				/*
+				using unsigned_type = xns::make_unsigned<T>;
+
+				xns::u64 red = rnd % (xns::max<unsigned_type>() + 1);
+				xns::s64 sgn = red - xns::min<T>();
+
+
+				return static_cast<T>(sgn);
+				*/
+				return static_cast<T>(rnd);
+			}
 
 
 		private:
