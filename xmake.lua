8f8d7e9cc6b836efea3bcc2398edc2b41d786006
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-07-22 00:37:53 +0200

    update

--- xmake.lua
+++ xmake.lua
@@ -4,26 +4,24 @@ set_xmakever("2.8.1")
 -- allowed platforms
 set_allowedplats("macosx")
 
+
+-- P R O J E C T --------------------------------------------------------------
+
 -- set project
 set_project("xns")
 
-
 -- set version
 set_version("0.1.0", {build = "%Y%m%d%H%M"})
+
+
+-- R U L E S ------------------------------------------------------------------
 
 -- add debug and release modes
 add_rules("mode.debug", "mode.release")
 
 -- default mode
-set_defaultmode("debug")
+--set_defaultmode("release")
 
--- set optimization: none, faster, fastest, smallest
-if is_mode("debug") then
-	set_optimize("none")
-end
-if is_mode("release") then
-	set_optimize("fastest")
-end
 
 -- set language
 set_languages("cxx20")
@@ -33,48 +31,104 @@ add_cxxflags("-Wall", "-Wextra", "-Werror", "-Wpedantic", "-Wno-unused", "-Wno-u
 		"-Winline", "-Weffc++", "-Wconversion", "-Wsign-conversion", "-Wfloat-conversion", "-Wnarrowing", "-Wshadow")
 
 
-
--- get source files
-local main = os.files("src/main.cpp")
-
--- append files
-
--- get include directories
-local inc = os.dirs("inc/**")
-inc = table.join("inc", inc)
-
 -- output directory
 set_targetdir(".")
 
+-- get include directories
+local inc = os.dirs("inc/**")
+-- append folder
+inc = table.join(inc, "inc")
 
--- static library
-target("xns_static", function()
-	-- set name
-	set_basename("xns")
-	add_files("src/xns/**/*.cpp")
-	add_files("src/xns/*.cpp")
-	set_kind("static")
-	add_includedirs(inc)
-end)
+-- get source files
+local src = os.files("src/xns/**.cpp")
+-- get unit test files
+local usrc = os.files("src/unit_tests/**.cpp")
 
--- executable file
-target("xns_exec", function()
-    -- set name
-	set_basename("xnsexec")
-    set_kind("binary")
-	add_files("src/unit_tests/*.cpp")
-	add_includedirs(inc)
-	add_deps("xns_static")
-	-- link static library
-	add_links("libxns.a")
-end)
+
+-- mode release
+if is_mode("release") then
+
+	-- library target
+	target("library", function()
+
+		-- static library
+		set_kind("static")
+		-- set name
+		set_basename("xns")
+
+		-- on install
+		on_install(function(target)
+			-- build library
+			os.exec("xmake")
+			print("installing %s", target:targetfile())
+			-- create directory
+			os.mkdir("xns")
+			os.mkdir("xns/include")
+			os.mkdir("xns/lib")
+			-- copy target file
+			os.cp(target:targetfile(), "xns/lib")
+			os.cp("inc/xns", "xns/include")
+		end)
+
+
+		add_files(src)
+		add_includedirs(inc)
+
+		set_optimize("fastest")
+
+
+
+		-- on clean
+		on_clean(function(target)
+			print("RELEASE: cleaning %s", target:targetfile())
+			os.rm("xns")
+			os.rm("build")
+			os.rm(target:targetfile())
+		end)
+
+	end)
+end
+
+
+if is_mode("debug") then
+
+	-- generate and update compile_commands.json
+	add_rules("plugin.compile_commands.autoupdate", {outputdir = "."})
+
+	-- executable file
+	target("executable", function()
+		-- set name
+		set_basename("xnsexec")
+		set_kind("binary")
+
+		set_optimize("none")
+
+		add_files(src)
+		add_files(usrc)
+		add_includedirs(inc)
+		-- on install
+		on_install(function()
+			-- print some tips
+			print("cannot install debug version")
+			-- exiting
+			os.exit(1)
+		end)
+
+		-- on clean
+		on_clean(function(target)
+			print("DEBUG: cleaning %s", target:targetfile())
+			os.rm("build")
+			os.rm(target:targetfile())
+			os.rm("compile_commands.json")
+		end)
+
+	end)
+end
 
 -- full clean
 task("fclean")
 	-- Set the run script
 	on_run(function()
-		-- call xmake clean
-		os.exec("xmake clean")
 		os.rm("build")
 		os.rm("compile_commands.json")
 		os.rm(".cache")
@@ -83,14 +137,4 @@ task("fclean")
     -- Set the command line options for the plugin. There are no parameter options here, just the plugin description.
     set_menu {}
 
-
--- compile commands
-task("ccdb")
-	-- Set the run script
-	on_run(function()
-		-- call xmake compile_commands
-		os.exec("xmake project -k compile_commands")
-	end)
-	-- Set the command line options for the plugin. There are no parameter options here, just the plugin description.
-	set_menu {}
 
