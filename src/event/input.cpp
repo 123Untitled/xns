77c95c730d39ae89f8e8f8f038ac6f9e789c05d8
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-07-27 03:16:46 +0200

    now input class handle generic stdin stuff

--- src/event/input.cpp
+++ src/event/input.cpp
@@ -1,156 +1,34 @@
 #include "input.hpp"
 
 
-// -- S T A T I C  P R I V A T E  M E M B E R S -------------------------------
+// -- I N  C L A S S ----------------------------------------------------------
 
-/* readed bytes */
-//xns::input::signed_type xns::input::_readed{0};
-//
-///* buffer */
-//xns::input::char_t xns::input::_buff[BUFFER_SIZE + 1]{'\0'};
-//
-///* input string */
-//xns::input::string xns::input::_input{""};
-//
-///* running flag */
-//bool xns::input::_is_running{false};
-//
-///* windowed mode */
-//bool xns::input::_windowed{false};
-//
-//
-//// -- S T A T I C  P U B L I C  M E T H O D S ---------------------------------
-//
-///* parameters */
-//void xns::input::parameters(const bool windowed) {
-//	// set windowed mode
-//	_windowed = windowed;
-//}
-//
-///* input loop */
-//void xns::input::start_loop(void) {
-//
-//	// get event instance
-//	xns::event& evnt = xns::event::instance();
-//
-//	// activate requested mode
-//	//evnt.next_mode();
-//
-//	// exit if there is no active mode
-//	//if (!evnt.is_mode()) { return; std::cout << "no mode" << std::endl; }
-//
-//	// set running flag
-//	_is_running = true;
-//	// loop over reading
-//	while (_is_running) {
-//		// activate requested mode
-//		evnt.next_mode();
-//
-//		evnt.call_event(xns::evntype::LOOP);
-//
-//		// read stdin
-//		read_input();
-//		// analyze readed bytes
-//		dispatch();
-//	}
-//
-//}
-//
-///* stop input loop */
-//void xns::input::stop_loop(void) {
-//	// unset running flag
-//	_is_running = false;
-//}
-//
-//
-//// -- S T A T I C  P R I V A T E  M E T H O D S -------------------------------
-//
-///* read input while available */
-//void xns::input::read_input(void) {
-//
-//	// reset previous readed bytes
-//	_input.clear();
-//
-//	// read stdin
-//	if (read_stdin() > 0) {
-//
-//		// append buffer to the string
-//		_input.append(_buff, static_cast<size_type>(_readed));
-//
-//		// buffer is full, loop over reading
-//		if (_readed == BUFFER_SIZE) {
-//			// set to non-blocking read
-//			//xns::terminal::raw_terminal(xns::VFlag::NON_BLOCKING);
-//			// read stdin again
-//			while (read_stdin() > 0) {
-//				// append buffer to the string
-//				_input.append(_buff, static_cast<size_type>(_readed));
-//			}
-//			// WARNING: not tested with only blocking read
-//			// back to blocking read
-//			//xns::terminal::raw_terminal(xns::VFlag::BLOCKING);
-//		}
-//
-//	}
-//}
-//
-///* read stdin */
-//xns::input::signed_type xns::input::read_stdin(void) {
-//	// read bytes from stdin
-//	_readed = read(STDIN_FILENO, _buff, BUFFER_SIZE);
-//	// append null terminator
-//	_buff[_readed] = '\0';
-//	// return number of readed bytes
-//	return _readed;
-//}
-//
-//
-//void xns::input::dispatch(void) {
-//
-//	if (_input.empty()) { return; }
-//
-//	// get event instance
-//	xns::event& evnt = xns::event::instance();
-//
-//	using Ev = xns::evntype;
-//
-//	// filter extended ascii codes
-//	_input.filter(xns::string::is_multibyte, false);
-//
-//
-//	if (_input.length() == 3 && _input[0] == 0x1b) {
-//
-//		// check ctrl sequence
-//		if (_input[1] == 0x5b) {
-//			// check arrow sequence
-//			switch (_input[2]) {
-//				case 'A': // code for arrow up
-//					evnt.call_event(Ev::UP);
-//					return;
-//				case 'B': // code for arrow down
-//					evnt.call_event(Ev::DOWN);
-//					return;
-//				case 'C': // code for arrow right
-//					evnt.call_event(Ev::RIGHT);
-//					return;
-//				case 'D': // code for arrow left
-//					evnt.call_event(Ev::LEFT);
-//					return;
-//				default:
-//					break;
-//			}
-//		}
-//	}
-//
-//	if (_input.length() == 1) {
-//		const xns::ubyte _char = (xns::ubyte)_input[0]; // NEED TO FIX CONVERSION
-//		evnt.call_event(static_cast<Ev>(_char));
-//	}
-//
-//	// filter control characters
-//	_input.filter(xns::string::is_control, false);
-//
-//	evnt.call_input(_input);
-//}
-//
-//
+
+// -- static public methods ---------------------------------------------------
+
+/* read stdin */
+xns::in::string xns::in::read(void) {
+
+	in::string input{};
+	size_type size{0};
+	signed_type readed{0};
+
+	do {
+		// resize input
+		input.resize(input.size() + BUFFER_SIZE);
+		// read stdin
+		readed = ::read(STDIN_FILENO, input.data() + size, BUFFER_SIZE);
+		// update total size
+		size += static_cast<size_type>(readed);
+
+	// loop over stdin
+	} while (readed == BUFFER_SIZE);
+
+	if (readed < 0) { /* error */ }
+
+	input.resize(size);
+
+	return input;
+}
+
+
--- test/src/input_tests.cpp
+++ test/src/input_tests.cpp
@@ -1,9 +1,31 @@
 #include "unit_tests.hpp"
 #include "input.hpp"
+#include "terminal.hpp"
 
 template <>
 bool UT::unit_tests<"input">(void) {
 
+
+	xns::terminal::raw_terminal(xns::VFlag::NON_BLOCKING);
+
+	auto str = xns::in::read();
+
+	std::cout << "size: " << str.size() << std::endl;
+	std::cout << "capacity: " << str.capacity() << std::endl;
+
+	for (xns::size_t i = 0; i < str.size(); ++i) {
+
+		if (str[i] == '\0') {
+			std::cout << "c: \\0" << std::endl;
+		}
+		else if (str[i] < 32) {
+			std::cout << "c: " << "? " << static_cast<int>(str[i]) << std::endl;
+		} else {
+			std::cout << "c: " << str[i] << " " << static_cast<int>(str[i]) << std::endl;
+		}
+	}
+
+	xns::terminal::restore_terminal();
 
 	return true;
 }
