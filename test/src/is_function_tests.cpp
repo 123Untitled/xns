5708c29f526f1b7b93f3f3f3e5a758f7b38d9f95
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 14:26:16 +0200

    add is_function tests

--- test/src/is_function_tests.cpp
+++ test/src/is_function_tests.cpp
@@ -1,10 +1,44 @@
 #include "unit_tests.hpp"
 #include "is_function.hpp"
 
+
+
+class A {
+
+	public:
+		A(void) = default;
+		void print(void) const {
+			std::cout << "Hello, World!" << std::endl;
+		}
+};
+
+void print_(const A& a) {
+	a.print();
+}
+
+
 /* unit test */
 template <>
 bool UT::unit_tests<"is_function">(void) {
-	// code here...
+
+
+	std::cout << std::boolalpha;
+
+	std::cout << "is_function<int>::value: "
+		<< xns::is_function<int> << std::endl;
+
+	std::cout << "is_function<A>::value: "
+		<< xns::is_function<A> << std::endl;
+
+	std::cout << "is_member_function<decltype(&A::print)>::value: "
+		<< xns::is_member_function<decltype(&A::print)> << std::endl;
+
+	std::cout << "is_member_function<decltype(print)>::value: "
+		<< xns::is_member_function<decltype(print_)> << std::endl;
+
+
+
+
 	return false;
 }
 
