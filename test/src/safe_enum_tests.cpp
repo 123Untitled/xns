2c12be725a3da4dff59cf302bc678b3b47b285ff
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 05:38:55 +0200

    update tests

--- test/src/safe_enum_tests.cpp
+++ test/src/safe_enum_tests.cpp
@@ -1,10 +1,36 @@
 #include "unit_tests.hpp"
 #include "safe_enum.hpp"
 
+
+struct def {
+	using type = int;
+
+	enum enum_type : type {
+		MAX = 9
+	};
+};
+
+using safe = xns::safe_enum<def>;
+
+
 /* unit test */
 template <>
 bool UT::unit_tests<"safe_enum">(void) {
+
+	safe s1 = safe::MAX;
+
+
+
 	// code here...
 	return false;
 }
 
+int main(void) {
+
+
+
+	UT::unit_tests<"safe_enum">();
+
+
+	return 0;
+}
