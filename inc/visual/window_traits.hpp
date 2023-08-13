9419c2b8c2aec313e4cab9a0e6cdd807747248d1
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 05:17:50 +0200

    xns renaming + now use only concepts

--- inc/visual/window_traits.hpp
+++ inc/visual/window_traits.hpp
@@ -1,11 +1,14 @@
 #ifndef WINDOW_TRAITS_HEADER
 #define WINDOW_TRAITS_HEADER
 
-#include "type_traits.hpp"
+// local headers
+#include "integral_constant.hpp"
+#include "is_same.hpp"
 
-// -- N A M E S P A C E -------------------------------------------------------
 
-namespace Xf {
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
 
 	// -- S P L I T  T Y P E  T R A I T S -------------------------------------
 
@@ -15,42 +18,17 @@ namespace Xf {
 	/* hsplit type */
 	struct hsplit_t {};
 
-	/* is vsplit false */
+	/* is vsplit concept */
 	template <class T>
-	struct is_vsplit : Xf::false_t {};
+	concept is_vsplit = xns::is_same<T, vsplit_t>;
 
-	/* is vsplit true */
-	template <>
-	struct is_vsplit<vsplit_t> : Xf::true_t {};
-
-	/* is hsplit false */
+	/* is hsplit concept */
 	template <class T>
-	struct is_hsplit : Xf::false_t {};
-
-	/* is hsplit true */
-	template <>
-	struct is_hsplit<hsplit_t> : Xf::true_t {};
-
-	/* is vsplit value */
-	template <class T>
-	constexpr bool is_vsplit_v = is_vsplit<T>::value;
-
-	/* is hsplit value */
-	template <class T>
-	constexpr bool is_hsplit_v = is_hsplit<T>::value;
-
-	/* vsplit concept */
-	template <class T>
-	concept vsplit_c = is_vsplit_v<T>;
-
-	/* hsplit concept */
-	template <class T>
-	concept hsplit_c = is_hsplit_v<T>;
+	concept is_hsplit = xns::is_same<T, hsplit_t>;
 
 	/* is split concept */
 	template <class T>
-	concept split_c = vsplit_c<T> || hsplit_c<T>;
-
+	concept is_split = is_vsplit<T> || is_hsplit<T>;
 
 
 
@@ -62,43 +40,17 @@ namespace Xf {
 	/* pane two type */
 	struct pane_two_t {};
 
-	/* is pane one false */
+	/* is pane one concept */
 	template <class T>
-	struct is_pane_one                 : Xf::false_t {};
+	concept is_pane_one = xns::is_same<T, pane_one_t>;
 
-	/* is pane one true */
-	template <>
-	struct is_pane_one<pane_one_t>     : Xf::true_t {};
-
-	/* is pane two false */
+	/* is pane two concept */
 	template <class T>
-	struct is_pane_two                 : Xf::false_t {};
+	concept is_pane_two = xns::is_same<T, pane_two_t>;
 
-	/* is pane two true */
-	template <>
-	struct is_pane_two<pane_two_t>     : Xf::true_t {};
-
-	/* is pane one value */
+	/* is pane concept */
 	template <class T>
-	constexpr bool is_pane_one_v = is_pane_one<T>::value;
-
-	/* is pane two value */
-	template <class T>
-	constexpr bool is_pane_two_v = is_pane_two<T>::value;
-
-	/* pane one concept */
-	template <class T>
-	concept pane_one_c = is_pane_one_v<T>;
-
-	/* pane two concept */
-	template <class T>
-	concept pane_two_c = is_pane_two_v<T>;
-
-	/* pane concept */
-	template <class T>
-	concept pane_c = pane_one_c<T> || pane_two_c<T>;
-
-
+	concept is_pane = is_pane_one<T> || is_pane_two<T>;
 
 
 
