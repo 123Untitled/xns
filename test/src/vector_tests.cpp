ae13bbcdcd055e0d352212125cdae43e894309db
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 15:34:39 +0200

    add main

--- test/src/vector_tests.cpp
+++ test/src/vector_tests.cpp
@@ -4,7 +4,13 @@
 /* unit test */
 template <>
 bool UT::unit_tests<"vector">(void) {
+
+	xns::vector<int> v;
 	// code here...
 	return false;
 }
 
+int main(void) {
+	UT::unit_tests<"vector">();
+	return 0;
+}
