15874a8c35358f6810684a67e37b52aee95e7317
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 05:49:12 +0200

    add in_place helper tag

---
+++ inc/meta/in_place.hpp
@@ -0,0 +1,37 @@
+#ifndef IN_PLACE_HEADER
+#define IN_PLACE_HEADER
+
+// local headers
+#include "types.hpp"
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- I N  P L A C E --------------------------------------------------------
+
+	// disambiguation tag for in place construction
+
+	/* in place */
+	struct in_place {
+		explicit in_place(void) noexcept = default;
+	};
+
+	/* in place type */
+	template <typename T>
+	struct in_place_type {
+		explicit in_place_type(void) noexcept = default;
+	};
+
+	/* in place index */
+	template <xns::size_t T>
+	struct in_place_index {
+		explicit in_place_index(void) noexcept = default;
+	};
+
+
+};
+
+#endif // IN_PLACE_HEADER
