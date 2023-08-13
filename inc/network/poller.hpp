49449a21de95c2882ab4f36d5a61e7e92e669831
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 14:25:54 +0200

    implement poller with kqueue

--- inc/network/poller.hpp
+++ inc/network/poller.hpp
@@ -3,7 +3,11 @@
 
 // local headers
 #include "socket.hpp"
+#include "numeric_limits.hpp"
 
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
 
 
 
@@ -280,13 +284,264 @@ namespace xns {
 
 
 
+		template <typename T>
+		class kvnt final {
+
+			public:
+
+				// -- public types --------------------------------------------
+
+				/* identifier type */
+				using identifier = decltype(xns::declval<struct kevent>().ident);
+
+				/* filter type */
+				using filter = decltype(xns::declval<struct kevent>().filter);
+
+				/* flags type */
+				using flags = decltype(xns::declval<struct kevent>().flags);
+
+				/* filter flags type */
+				using filter_flags = decltype(xns::declval<struct kevent>().fflags);
+
+				/* data type */
+				using data = decltype(xns::declval<struct kevent>().data);
+
+				/* user data type */
+				using user_data = T;
+
+
+
+
+				// -- public lifecycle ----------------------------------------
+
+				/* default constructor */
+				inline constexpr kvnt(void) noexcept
+				: _kevent{} {}
+
+				/* struct kevent constructor */
+				inline constexpr kvnt(struct kevent& kevent) noexcept
+				: _kevent{kevent} {}
+
+				/* copy constructor */
+				inline constexpr kvnt(const kvnt& other) noexcept
+				: _kevent{other._kevent} {}
+
+				/* move constructor */
+				inline constexpr kvnt(kvnt&& other) noexcept
+				: kvnt{other} {}
+
+				/* destructor */
+				inline ~kvnt(void) noexcept = default;
+
+
+				// -- public assignment operators -----------------------------
+
+				/* copy assignment operator */
+				inline constexpr kvnt& operator=(const kvnt& other) noexcept {
+					_kevent = other._kevent;
+					return *this;
+				}
+
+				/* move assignment operator */
+				inline constexpr kvnt& operator=(kvnt&& other) noexcept {
+					return operator=(other);
+				}
+
+				// -- public conversion operators -----------------------------
+
+				/* kevent conversion operator */
+				inline constexpr operator struct kevent&(void) noexcept {
+					return _kevent;
+				}
+
+				/* kevent pointer conversion operator */
+				inline constexpr operator struct kevent*(void) noexcept {
+					return &_kevent;
+				}
+
+				/* to kvnt conversion operator */
+				static inline constexpr auto to_kvnt(struct kevent& kevent) noexcept -> kvnt& {
+					return *static_cast<kvnt*>(static_cast<void*>(&kevent));
+				}
+
+
+				// -- public modifiers ----------------------------------------
+
+				/* setup */
+				inline constexpr auto setup(const xns::network::socket& socket,
+											const filter& filter,
+											const flags& flags,
+											const filter_flags& filter_flags,
+											const data& data,
+											user_data& user_data) noexcept -> void {
+
+					if (socket.valid()) {
+						_kevent.ident = static_cast<identifier>(socket.get());
+					}
+					_kevent.filter = filter;
+					_kevent.flags = flags;
+					_kevent.fflags = filter_flags;
+					_kevent.data = data;
+					_kevent.udata = static_cast<void*>(&user_data);
+				}
+
+
+				// -- public accessors ----------------------------------------
+
+				/* get ident */
+				inline constexpr auto get_ident(void) const noexcept -> const identifier& {
+					return _kevent.ident;
+				}
+
+				/* get filter */
+				inline constexpr auto get_filter(void) const noexcept -> const filter& {
+					return _kevent.filter;
+				}
+
+				/* get flags */
+				inline constexpr auto get_flags(void) const noexcept -> const flags& {
+					return _kevent.flags;
+				}
+
+				/* get filter flags */
+				inline constexpr auto get_filter_flags(void) const noexcept -> const filter_flags& {
+					return _kevent.fflags;
+				}
+
+				/* get data */
+				inline constexpr auto get_data(void) const noexcept -> const data& {
+					return _kevent.data;
+				}
+
+				/* get user data */
+				inline constexpr auto get_user_data(void) const noexcept -> const user_data& {
+					return *static_cast<user_data*>(_kevent.udata);
+				}
+
+
+			private:
+
+				// -- private members -----------------------------------------
+
+				/* kevent */
+				struct kevent _kevent;
+
+		};
+
+
+
 		class bsd_poller final {
 
 			public:
 
+				// -- public lifecycle ----------------------------------------
+
+				/* default constructor */
+				inline bsd_poller(void) noexcept
+				: _kqueue{::kqueue()},
+				  _kevents{} {
+					_kevents.reserve(128);
+				}
+
+				auto add_socket(network::socket& socket) noexcept -> void {
+
+					xns::network::kvnt<network::socket> event{};
+					// setup event
+					event.setup(socket, EVFILT_READ, EV_ADD, 0, 0, socket);
+					// add event
+					if (::kevent(_kqueue.get(), event, 1, nullptr, 0, nullptr) == -1) {
+						std::cout << "error: " << std::strerror(errno) << std::endl;
+					} // increase buffer size
+					_kevents.push_back();
+				}
+
+				auto remove_socket(network::socket& socket) noexcept -> void {
+
+					xns::network::kvnt<network::socket> event{};
+					// setup event
+					event.setup(socket, EVFILT_READ, EV_DELETE, 0, 0, socket);
+					// add event
+					if (kevent(_kqueue.get(), event, 1, nullptr, 0, nullptr) == -1) {
+						std::cout << "error: " << std::strerror(errno) << std::endl;
+					} // decrease buffer size
+					_kevents.pop_back();
+				}
+
+				auto poll(void) noexcept -> void {
+
+					bool range = xns::limits::in_range<int>(_kevents.size());
+
+					if (range == false) { return; }
+
+					int nev = kevent(_kqueue.get(), nullptr, 0, _kevents.data(),
+									 static_cast<int>(_kevents.size()), nullptr);
+
+					if (nev < 0) {
+						std::cout << "error: " << std::strerror(errno) << std::endl;
+						return;
+					}
+
+					for (size_type i = 0; i < static_cast<size_type>(nev); ++i) {
+
+						auto& event = kvnt<network::socket>::to_kvnt(_kevents[i]);
+
+
+						if (event.get_filter() == EVFILT_READ) {
+
+							auto& socket = event.get_user_data();
+
+						}
+
+
+
+
+						std::cout << "event: " << _kevents[i].ident << std::endl;
+					}
+				}
+
+				// return the number of events placed in the eventlist, up to the value given by nevents.
+				// If an error occurs while processing an element of the changelist and there is enough room in the eventlist,
+				// then the event will be placed in the eventlist with EV_ERROR set in flags and the system error in data.
+				// Otherwise, -1 will be returned, and errno will be set to indicate the error condition.
+				// If the time limit expires, then kevent(), kevent64() and kevent_qos() return 0.
+
+				//struct kevent {
+				//	uintptr_t ident;        /* identifier for this event */
+				//	uint32_t  filter;       /* filter for event */
+				//	uint32_t  flags;        /* action flags for kqueue */
+				//	uint32_t  fflags;       /* filter flag value */
+				//	int64_t   data;         /* filter data value */
+				//	void     *udata;        /* opaque user data identifier */
+				//};
+
+
+			private:
+
+				// -- private types -------------------------------------------
+
+				/* kevent type */
+				using kevent_t = struct kevent;
+
+				/* kevent vector type */
+				using kevent_vector = xns::vector<kevent_t>;
+
+				/* size type */
+				using size_type = typename kevent_vector::size_type;
+
+
+				// -- private members -----------------------------------------
+
+				/* kqueue */
+				xns::unique_descriptor _kqueue;
+
+				/* kevent vector */
+				kevent_vector _kevents;
 
 
 		};
+
+
+
 
 
 	}
