994307231090c5afbe49371085061fdba8b352b7
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 14:24:23 +0200

    implement apply

---
+++ inc/utility/apply.hpp
@@ -0,0 +1,46 @@
+#ifndef APPLY_HEADER
+#define APPLY_HEADER
+
+// local headers
+#include "is_function.hpp"
+#include "tuple.hpp"
+#include "integer_sequence.hpp"
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- A P P L Y -----------------------------------------------------------
+
+	namespace impl {
+
+		/* apply implementation */
+		template <typename F, typename T, xns::size_t... I>
+		constexpr auto apply(F&& function, T&& tuple, xns::index_seq<I...>) noexcept {
+			return function(tuple.template get<I>()...);
+		}
+
+	}
+	template <typename F, typename T>
+	constexpr auto apply(F&& function, T&& tuple) noexcept -> auto {
+		// check T is a tuple
+		static_assert(xns::is_tuple<T>, "): APPLY: T IS NOT A TUPLE! :(");
+
+		constexpr auto size = xns::tuple_size<T>;
+
+		// check F is a function
+		//static_assert(xns::is_function<F>, "): APPLY: F IS NOT A FUNCTION! :(");
+
+		return impl::apply(xns::forward<F>(function),
+						   xns::forward<T>(tuple),
+						   xns::make_index_sequence<size>());
+	}
+
+
+
+}
+
+#endif // APPLY_HEADER
+
