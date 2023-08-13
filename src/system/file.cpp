88ca918b307ddfb9c27e6829bb4f5671267a1739
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 07:09:21 +0200

    deleted all content, will be implemented

--- src/system/file.cpp
+++ src/system/file.cpp
@@ -1,78 +1,9 @@
 #include "file.hpp"
 
-xns::file::file(void)
-: _file{ }, _path{ }, _fd{ }, _data{ }, _state{false} {
-	// code here...
-}
 
-/* path constructor */
-xns::file::file(const xns::string& path)
-: _file{}, _path{path}, _fd{}, _data{}, _state{false} {
-	// code here...
-}
-
-xns::file::~file(void) {
-}
-
-void xns::file::open(void) {
-
-	if (_path.empty()) { return; }
-
-	// check file exist
-	if (stat(_path.pointer(), &_data) == -1) { return; }
-
-	// check is regular file
-	if (!S_ISREG(_data.st_mode)) { return; }
-
-	// check error while opening file
-	_fd = xns::unique_fd{_path, O_RDONLY};
-
-	if (!_fd) { return; }
-
-	_state = true;
-	std::cout << "NO ERROR WHILE OPENING FILE: " << _path.pointer() << std::endl;
-}
-
-/* get path */
-const xns::string& xns::file::path(void) const {
-	// return file path
-	return _path;
-}
-
-
-void xns::file::file_name(xns::string&& path) {
-	//_path = static_cast<String&&>(path);
-}
 
 void xns::file::content(void) {
 
-	if (!_state) { return; }
-
-	_state = false;
-
-	off_t size = _data.st_size;
-
-	if (size <= 0) { return; }
-
-	xns::size_t usize = static_cast<xns::size_t>(size);
-
-	_file.reserve(usize);
-
-	if (_file.capacity() != (xns::size_t)size) {
-		std::cout << "RESERVE FAILED" << std::endl;
-		_state = false;
-		return;
-	}
-
-	// WARNING: need to set size in String class !!!
-	if (read(_fd.get(), _file.pointer(), usize) != size) {
-		_state = false;
-		return;
-	}
-
-	std::cout << "FILE CORRECTLY READED" << std::endl;
-	std::cout << "size: " << size << std::endl;
-
-	write(1, _file.pointer(), usize);
+	if (_descriptor.invalid()) { return; }
 
 }
