8fc17d0d898c28c26269e8071ffad2d83b619672
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 05:48:42 +0200

    implement is_trivially_destructible with compiler builtin

---
+++ inc/is_trivially_destructible.hpp
@@ -0,0 +1,37 @@
+#ifndef IS_TRIVIALLY_DESTRUCTIBLE_HEADER
+#define IS_TRIVIALLY_DESTRUCTIBLE_HEADER
+
+// local headers
+#include "config.hpp"
+#include "is_destructible.hpp"
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- I S  T R I V I A L L Y  D E S T R U C T I B L E ----------------------
+
+#if __has_builtin(__is_trivially_destructible)
+
+	/* is trivially destructible concept */
+	template <class T>
+	concept is_trivially_destructible = __is_trivially_destructible(T);
+
+#elif __has_builtin(__has_trivial_destructor)
+
+	/* is trivially destructible concept */
+	template <class T>
+	concept is_trivially_destructible = xns::is_destructible<T> && __has_trivial_destructor(T);
+
+#else
+
+#error "Compiler does not support __is_trivially_destructible or __has_trivial_destructor"
+
+#endif
+
+
+}
+
+#endif // IS_TRIVIALLY_DESTRUCTIBLE_HEADER
