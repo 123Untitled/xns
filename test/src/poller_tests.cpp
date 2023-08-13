88123e158da3dfe25acac135fcf011ff8f32abcf
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 15:46:26 +0200

    move tests from main

--- test/src/poller_tests.cpp
+++ test/src/poller_tests.cpp
@@ -1,9 +1,72 @@
 #include "unit_tests.hpp"
 #include "poller.hpp"
+#include "socket.hpp"
+#include "network.hpp"
+
 
 /* unit test */
 template <>
 bool UT::unit_tests<"poller">(void) {
+
+	xns::network::poller poller;
+
+	xns::network::socket sss[10] {
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+		::socket(AF_INET, SOCK_STREAM, 0),
+	};
+
+
+	for (auto& s : sss) {
+		poller.add_socket(s);
+	}
+
+	for (auto& s : sss) {
+		poller.remove_socket(s);
+	}
+
+
+
+
+
+
+	return 0;
+	xns::network::port port{8080};
+
+	auto sock = xns::network::make_socket<xns::network::ipv4, xns::stream>();
+
+	xns::network::ipv4_address addr{xns::string_view{"127.0.0.1"}, port};
+
+	xns::network::bind(sock, addr);
+
+	xns::network::listen(sock, 1);
+
+
+	return 0;
+
+
+	/*
+	xns::socket<xns::local, xns::stream> sock{};
+
+	sock.create();
+	sock.bind(xns::string_view{"./debug.sock"});
+	sock.listen(1);
+	sock.accept();
+	*/
+
+	xns::socket<xns::ipv4, xns::stream> sock2{};
+
+	sock2.create();
+	sock2.bind("127.0.0.1", (unsigned short)8080);
+	sock2.listen(1);
+	//sock2.accept();
 	// code here...
 	return false;
 }
--- test/src/vector_tests.cpp
+++ test/src/vector_tests.cpp
@@ -1,6 +1,37 @@
 #include "unit_tests.hpp"
 #include "vector.hpp"
 
+
+
+static void dichotic_test(void) {
+	xns::vector<int> v = xns::make_vector<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+	// initialize seed
+	std::srand((unsigned)std::time(nullptr));
+
+	xns::vector<int> vvv{};
+	for (int i = 0; i < 100; ++i) {
+		//vvv.copy_back(std::rand() % 1000);
+		vvv.dichotomic_insert(std::rand() % 100000);
+	}
+
+	for (auto& i : vvv) {
+		std::cout << i << ", ";
+	}
+	std::cout << std::endl;
+
+
+	auto it = vvv.dichotomic_search(1000);
+
+	if (it != vvv.end()) {
+		std::cout << "found: " << *it << std::endl;
+	} else {
+		std::cout << "not found" << std::endl;
+	}
+}
+
+
+
 /* unit test */
 template <>
 bool UT::unit_tests<"vector">(void) {
