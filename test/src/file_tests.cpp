f2a6a3abd6af68885a047bcc2d5cf4f17652ca21
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 07:16:33 +0200

    update tests

--- test/src/file_tests.cpp
+++ test/src/file_tests.cpp
@@ -4,7 +4,18 @@
 /* unit test */
 template <>
 bool UT::unit_tests<"file">(void) {
+
+
+	xns::file file{"test.txt", O_RDWR | O_CREAT, 0644};
+
+
 	// code here...
 	return false;
 }
 
+int main(void) {
+
+	UT::unit_tests<"file">();
+
+	return 0;
+}
