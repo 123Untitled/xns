6f53a4881dfdf5a573b8248c60c076ec42c3f243
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 14:26:03 +0200

    add thread tests

--- test/src/thread_tests.cpp
+++ test/src/thread_tests.cpp
@@ -8,7 +8,42 @@ bool UT::unit_tests<"thread">(void) {
 	return false;
 }
 
+
+#include <thread>
+#include "apply.hpp"
+
+class A {
+
+	public:
+		A(void) = default;
+		void print(void) const {
+			std::cout << "Hello, World!" << std::endl;
+		}
+};
+
+void printer(const A& a, int i) {
+	a.print();
+	std::cout << i << std::endl;
+}
+
 int main(void) {
+
+	/*
+	const xns::tuple<A, int> t{A{}, 123};
+	xns::apply(printer, t);
+	*/
+
+	void(&f)(const A&, int) = printer;
+
+	const A a;
+	xns::thread t0{f, a, 123};
+
+	t0.join();
+
+	return EXIT_SUCCESS;
+
+
+
 	return UT::unit_tests<"thread">()
 		? EXIT_SUCCESS : EXIT_FAILURE;
 }
