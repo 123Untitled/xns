0f9affcaa04b7b9e2f02f43e4601dffb03e52111
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-13 07:16:43 +0200

    file as friend

--- inc/system/unique_descriptor.hpp
+++ inc/system/unique_descriptor.hpp
@@ -21,97 +21,6 @@
 namespace xns {
 
 
-	// -- U N I Q U E  F D  C L A S S -----------------------------------------
-
-	//class unique_fd final {
-
-
-	//	public:
-
-	//		// -- public types ------------------------------------------------
-
-	//		/* file descriptor type */
-	//		using fd_type = int;
-
-	//		/* self type */
-	//		using self = unique_fd;
-
-
-	//		// -- public lifecycle --------------------------------------------
-
-	//		/* default constructor */
-	//		unique_fd(void) noexcept;
-
-	//		/* file descriptor constructor */
-	//		explicit unique_fd(const fd_type fd) noexcept;
-
-	//		/* variadic constructor */
-	//		template <xns::is_string S, class... A>
-	//		explicit unique_fd(const S& path, A&&... args) noexcept
-	//		: _fd{::open(path.data(), args...)} {
-	//		}
-
-	//		/* non-copyable class */
-	//		NON_COPYABLE(unique_fd);
-
-	//		/* move constructor */
-	//		unique_fd(self&& other) noexcept;
-
-	//		/* destructor */
-	//		~unique_fd(void) noexcept;
-
-
-	//		// -- public assignment operators ---------------------------------
-
-	//		/* move assignment operator */
-	//		self& operator=(unique_fd&& other) noexcept;
-
-
-	//		// -- public boolean operators ------------------------------------
-
-	//		/* boolean operator */
-	//		explicit operator bool(void) const;
-
-	//		/* not operator */
-	//		bool operator!(void) const;
-
-
-	//		// -- public accessors --------------------------------------------
-
-	//		/* valid */
-	//		bool valid(void) const noexcept;
-
-	//		/* get file descriptor */
-	//		fd_type get(void) const noexcept;
-
-
-	//		// -- M E T H O D S -----------------------------------------------
-
-
-	//		/* duplicate */
-	//		unique_fd duplicate(void) const;
-
-	//		/* duplicate 2 */
-	//		void duplicate(self& other) const;
-
-
-
-	//	private:
-
-	//		// -- private enums -----------------------------------------------
-
-	//		/* invalid file descriptor */
-	//		enum : xns::s8  { NULLFD = -1 };
-
-
-	//		// -- private members ---------------------------------------------
-
-	//		/* file descriptor */
-	//		fd_type _fd;
-
-	//};
-
-
 	// -- T R U S T  T A G  S T R U C T ---------------------------------------
 
 	struct trust {
@@ -122,11 +31,22 @@ namespace xns {
 	};
 
 
+	// -- forward declarations ------------------------------------------------
+
+	/* file class */
+	class file;
+
 
 	// -- U N I Q U E  D E S C R I P T O R  -----------------------------------
 
 	/* unique descriptor */
 	class unique_descriptor {
+
+
+		// -- friends ---------------------------------------------------------
+
+		/* file class as friend */
+		friend class file;
 
 
 		public:
