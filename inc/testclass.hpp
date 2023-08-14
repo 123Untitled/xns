9912eb040427d4f8b18827aab321d9c39ea0af03
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-14 09:26:04 +0200

    new templated test class

--- inc/testclass.hpp
+++ inc/testclass.hpp
@@ -2,6 +2,7 @@
 #define TESTCLASS_HEADER
 
 #include <iostream>
+#include "string_literal.hpp"
 #include "move.hpp"
 
 #define TESTCLASS_DEBUG
@@ -169,6 +170,81 @@ namespace tests {
 
 }
 
+
+
+template <typename T, xns::basic_string_literal CLASS_NAME,
+					  xns::basic_string_literal TYPE_NAME>
+class X {
+	public:
+
+		X(void)
+		: _x{} {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " default constructor" << std::endl;
+		}
+
+		X(const T& x)
+		: _x{x} {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " "
+					  << TYPE_NAME.runtime_data()
+					  << " constructor" << std::endl;
+		}
+
+		X(const X& other)
+		: _x{other._x} {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " copy constructor" << std::endl;
+		}
+
+		X(X&& other)
+		: _x{xns::move(other._x)} {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " move constructor" << std::endl;
+		}
+
+		X& operator=(const X& other) {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " copy assignment" << std::endl;
+			_x = other._x;
+			return *this;
+		}
+
+		X& operator=(X&& other) {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " move assignment" << std::endl;
+			_x = xns::move(other._x);
+			return *this;
+		}
+
+		~X(void) {
+			std::cout << CLASS_NAME.runtime_data()
+					  << " destructor" << std::endl;
+		}
+
+	private:
+		T _x;
+
+	template <typename U, xns::basic_string_literal CNAME,
+					      xns::basic_string_literal TNAME>
+	friend std::ostream& operator<<(std::ostream& os, const X& x);
+};
+
+
+template <typename U, xns::basic_string_literal CNAME,
+					  xns::basic_string_literal TNAME>
+inline std::ostream& operator<<(std::ostream& os, const X<U, CNAME, TNAME>& x) {
+	return os << CNAME.runtime_data() << " " << TNAME.runtime_data() << " " << x._x;
+}
+
+
+using B = X<bool,  "B", "bool">;
+using C = X<char,  "C", "char">;
+using I = X<int,   "I", "int">;
+using L = X<long,  "L", "long">;
+using F = X<float, "F", "float">;
+using D = X<double,"D", "double">;
+using S = X<std::string, "S", "string">;
 
 
 
