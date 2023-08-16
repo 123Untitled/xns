8e52afc6da1d6124f716f7b04fe7cd9daad44d07
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-16 13:41:57 +0200

    add always false

---
+++ inc/meta/always_false.hpp
@@ -0,0 +1,25 @@
+#ifndef ALWAYS_FALSE_HEADER
+#define ALWAYS_FALSE_HEADER
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- A L W A Y S  F A L S E ----------------------------------------------
+
+	namespace impl {
+
+		template <typename>
+		struct always_false {
+			static constexpr bool value{false};
+		};
+	}
+
+	template <typename T>
+	concept always_false = impl::always_false<T>::value;
+
+}
+
+#endif // ALWAYS_FALSE_HEADER
