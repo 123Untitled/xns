063f532fe3b3be06745691316374d78617e4181d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-17 09:18:28 +0200

    update tests

--- test/src/output_tests.cpp
+++ test/src/output_tests.cpp
@@ -4,7 +4,23 @@
 /* unit test */
 template <>
 bool UT::unit_tests<"output">(void) {
+
+	xns::string str{ "Hello, world!" };
+
+	xns::out::write(str);
+
+	xns::out::newline();
+
+	xns::out::flush();
+
 	// code here...
 	return false;
 }
 
+int main(void) {
+
+	UT::unit_tests<"output">();
+
+
+	return 0;
+}
