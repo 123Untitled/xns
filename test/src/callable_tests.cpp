394879689872307a5e08b9d28ecc77b48161e3e4
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-19 23:42:06 +0200

    update tests

--- test/src/callable_tests.cpp
+++ test/src/callable_tests.cpp
@@ -1,10 +1,49 @@
 #include "unit_tests.hpp"
 #include "callable.hpp"
 
+class toto {
+
+	public:
+		toto(void) = default;
+		void print(int i) volatile & {
+			std::cout << i << std::endl;
+		}
+		~toto(void) = default;
+};
+
+template <typename T>
+class method_;
+
+template <typename R, typename C, typename... A>
+class method_<R(C::*)(A...)> {
+	public:
+		method_(void) = default;
+};
+
+
 /* unit test */
 template <>
 bool UT::unit_tests<"callable">(void) {
-	// code here...
+
+	toto t;
+	using proto = void(toto::*)(int) volatile;
+	//proto p = &toto::print;
+
+	//method_<proto> m;
+
+
+
+	//xns::callable<proto> c1{&toto::print, t};
+
 	return false;
 }
 
+
+
+
+int main(void) {
+
+	UT::unit_tests<"callable">();
+
+	return 0;
+}
