6e358a8dc911e1eb86632eb9492fb545f9509b0d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-19 10:33:18 +0200

    add memory file (future malloc implementation)

---
+++ inc/memory/memory.hpp
@@ -0,0 +1,35 @@
+#ifndef MEMORY_HEADER
+#define MEMORY_HEADER
+
+// local headers
+#include "os.hpp"
+
+// operating system headers
+#include <sys/mman.h>
+
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+     // void* mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+	// int munmap(void *addr, size_t len);
+
+
+	// -- M E M O R Y  C L A S S ----------------------------------------------
+
+	class memory final {
+
+
+
+
+
+
+
+
+	};
+
+}
+
+#endif // MEMORY_HEADER
