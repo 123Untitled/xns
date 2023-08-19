17a5770698148405c49788631628716d6406f6a6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-19 23:39:31 +0200

    define new XNS_OS

--- inc/os.hpp
+++ inc/os.hpp
@@ -4,12 +4,15 @@
 
 #if defined(__linux__)
     #define LINUX_OS
+	#define XNS_LINUX
 #elif defined(__unix__) || (!defined(__APPLE__) && defined(__MACH__))
     #if defined(BSD)
 		#define BSD_OS
+		#define XNS_BSD
     #endif
 #elif defined(__APPLE__) && defined(__MACH__) // Apple OSX and iOS (Darwin)
 	#define APPLE_OS
+	#define XNS_APPLE
 #endif
 
 
