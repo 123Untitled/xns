a7edaecebce14788b18ec69779fa1a70d126e9a2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-26 21:09:33 +0200

    move comparaison into separate file

--- inc/integer/integer.hpp
+++ inc/integer/integer.hpp
@@ -9,11 +9,14 @@
 #include "is_unsigned.hpp"
 
 #include "integer_conversion.hpp"
+#include "integer_comparaison.hpp"
+#include "integer_arithmetic.hpp"
 
 
 #include "string.hpp"
 
 #include <iostream>
+
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
@@ -110,7 +113,6 @@ namespace xns {
 				str.to_string(_value);
 				std::cout << (SIGNED ? "int" : "uint") << BYTES << ": " <<
 					str.data() << std::endl;
-
 			}
 
 
@@ -228,90 +230,6 @@ namespace xns {
 			 ******************************************************************/
 
 
-			// -- same sign equality ------------------------------------------
-
-			/* equality */
-			template <xns::is_integral L,
-					  xns::is_integral R> requires ((xns::is_unsigned<L> and xns::is_unsigned<R>) or
-												    (xns::is_signed<L>   and xns::is_signed<R>))
-			static inline constexpr auto equality(const L& left, const R& right) noexcept -> bool {
-				using safe = xns::larger<L, R>;
-				return static_cast<safe>(left) == static_cast<safe>(right);
-			}
-
-
-
-			// -- different sign equality -------------------------------------
-
-			/* equality */
-			template <xns::is_signed_integral S,
-					  xns::is_unsigned_integral U>
-			static inline constexpr auto equality(const S& sign, const U& unsign) noexcept -> bool {
-
-
-				if constexpr (sizeof(S) > sizeof(U)) {
-					// case ex: signed int == unsigned char
-					return sign == static_cast<S>(unsign);
-				}
-				// case ex: signed char == unsigned int
-				//          signed char == unsigned char
-				return sign < 0 ? false
-					: static_cast<U>(sign) == unsign;
-
-			}
-
-			/* equality */
-			template <xns::is_unsigned_integral U,
-					  xns::is_signed_integral S>
-			static inline constexpr auto equality(const U& left, const S& right) noexcept -> bool {
-				return equality(right, left);
-			}
-
-
-
-			/******************************************************************
-			 *                                                                *
-			 *  L E S S   T H A N                                             *
-			 *                                                                *
-			 ******************************************************************/
-
-			// -- same sign less than -----------------------------------------
-
-			/* less than */
-			template <xns::is_integral L,
-					  xns::is_integral R> requires ((xns::is_unsigned<L> and xns::is_unsigned<R>) or
-												    (xns::is_signed<L>   and xns::is_signed<R>))
-			static inline constexpr auto less_than(const L& left, const R& right) noexcept -> bool {
-				using safe = xns::larger<L, R>;
-				return static_cast<safe>(left) < static_cast<safe>(right);
-			}
-
-
-			// -- different sign less than ------------------------------------
-
-			/* less than */
-			template <xns::is_signed_integral S,
-					  xns::is_unsigned_integral U>
-			static inline constexpr auto less_than(const S& sign, const U& unsign) noexcept -> bool {
-
-				if constexpr (sizeof(S) > sizeof(U)) {
-					// case ex: signed int < unsigned char
-					return sign < static_cast<S>(unsign);
-				}
-				// case ex: signed char < unsigned int
-				//          signed char < unsigned char
-				return sign < 0 ? true
-					: static_cast<U>(sign) < unsign;
-
-			}
-
-			/* less than */
-			template <xns::is_unsigned_integral U,
-					  xns::is_signed_integral S>
-			static inline constexpr auto less_than(const U& left, const S& right) noexcept -> bool {
-				return less_than(right, left);
-			}
-
 
 
 			/******************************************************************
@@ -330,12 +248,6 @@ namespace xns {
 				return left > (xns::limits::max<T>() - right) ? xns::limits::max<T>()
 					: left + right;
 			}
-
-
-
-
-
-
 
 
 		private:
