84cea945168a5f2c7bf023609e57f6d1bd336a58
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-08-30 04:05:35 +0200

    add endianness_tests

--- test/src/endianness_tests.cpp
+++ test/src/endianness_tests.cpp
@@ -9,6 +9,18 @@ bool UT::unit_tests<"endianness">(void) {
 }
 
 int main(void) {
+
+	if constexpr (xns::endianness::is_big()) {
+		std::cout << "big endian" << std::endl;
+	}
+	else if constexpr (xns::endianness::is_little()) {
+		std::cout << "little endian" << std::endl;
+	}
+	else {
+		std::cout << "unknown endian" << std::endl;
+	}
+
+
 	return UT::unit_tests<"endianness">()
 		? EXIT_SUCCESS : EXIT_FAILURE;
 }
