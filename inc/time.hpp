5b7e885d3d5c103139ad635c4a0cab0bba3b9e64
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-09-04 08:21:34 +0200

    implement time class

--- inc/time.hpp
+++ inc/time.hpp
@@ -1,8 +1,18 @@
 #ifndef TIME_HEADER
 #define TIME_HEADER
 
-#include "macro.hpp"
+// local headers
 #include "types.hpp"
+#include "macro.hpp"
+#include "os.hpp"
+
+
+// operating system headers
+#if defined(XNS_APPLE)
+# include <mach/mach_time.h>
+#elif defined(XNS_LINUX) || defined(XNS_BSD)
+# include <sys/time.h>
+#endif
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
@@ -14,42 +24,101 @@ namespace xns {
 
 	class time final {
 
+
 		public:
 
-			// -- public constructors -----------------------------------------
 
-			/* non-instanciable class */
-			NON_INSTANCIABLE(time);
+			// -- public types ------------------------------------------------
+
+			/* self type */
+			using self = xns::time;
 
 
 			// -- public static methods ---------------------------------------
 
+			#if defined(XNS_APPLE)
+
 			/* get current time */
-			static void now(void);
+			static auto now(void) -> xns::size_t {
+				return ::mach_absolute_time()
+					* shared()._abs_to_nano;
+			}
+
+
+			#elif defined(XNS_LINUX) || defined(XNS_BSD)
+
+			/* get current time */
+			static auto now(void) -> xns::size_t {
+
+				struct ::timespec ts;
+				::clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+				return (static_cast<xns::size_t>(ts.tv_sec) * 1'000'000'000U)
+					+ ts.tv_nsec;
+			}
+
+			#endif
 
 
 		private:
 
-			// -- private static members --------------------------------------
+			// -- private lifecycle -------------------------------------------
 
-			/* hours */
-			static xns::u8 _h;
+			#if defined(XNS_APPLE)
 
-			/* minutes */
-			static xns::u8 _m;
+			/* default constructor */
+			inline time(void) noexcept
+			: _abs_to_nano{0.0}, _nano_to_abs{0.0} {
 
-			/* seconds */
-			static xns::u8 _s;
+				// declare timebase info
+				::mach_timebase_info_data_t timebase;
+				// get timebase info
+				::mach_timebase_info(&timebase);
 
-			/* milliseconds */
-			static xns::u16 _ms;
+				// absolute clock to nanoseconds
+				_abs_to_nano = static_cast<xns::f64>(timebase.numer)
+							/ static_cast<xns::f64>(timebase.denom);
 
-			/* microseconds */
-			static xns::u16 _us;
+				// nanoseconds to absolute clock
+				_nano_to_abs = static_cast<xns::f64>(timebase.denom)
+							/ static_cast<xns::f64>(timebase.numer);
 
-			/* nanoseconds */
-			static xns::u16 _ns;
+			}
 
+			#elif defined(XNS_LINUX) || defined(XNS_BSD)
+
+			/* default constructor */
+			inline time(void) noexcept = default;
+
+			#endif
+
+			/* non-assignable class */
+			NON_ASSIGNABLE(time);
+
+			/* destructor */
+			inline ~time(void) noexcept = default;
+
+
+			// -- private static methods --------------------------------------
+
+			/* get instance */
+			static inline auto shared(void) noexcept -> self& {
+				static xns::time instance;
+				return instance;
+			}
+
+
+			// -- private members ---------------------------------------------
+
+			#if defined(XNS_APPLE)
+
+			/* absolute clock to nanoseconds */
+			xns::f64 _abs_to_nano;
+
+			/* nanoseconds to absolute clock */
+			xns::f64 _nano_to_abs;
+
+
+			#endif
 
 	};
 
