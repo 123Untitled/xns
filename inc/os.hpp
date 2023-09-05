5d78d4906c506052c93440088855f0e037a188fe
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-09-06 01:38:19 +0200

    detect aarch and simd

--- inc/os.hpp
+++ inc/os.hpp
@@ -2,18 +2,31 @@
 #define OS_HEADER
 
 
+// -- operating system check --------------------------------------------------
+
+/* linux */
 #if defined(__linux__)
-    #define LINUX_OS
 	#define XNS_LINUX
+/* bsd */
 #elif defined(__unix__) || (!defined(__APPLE__) && defined(__MACH__))
-    #if defined(BSD)
-		#define BSD_OS
+	#if defined(BSD)
 		#define XNS_BSD
-    #endif
+	#endif
+/* apple */
 #elif defined(__APPLE__) && defined(__MACH__) // Apple OSX and iOS (Darwin)
-	#define APPLE_OS
 	#define XNS_APPLE
 #endif
 
 
+// -- architecture check ------------------------------------------------------
+
+/* x86_64 */
+#if defined(__x86_64__) || defined(_M_X64)
+	#define XNS_X86_64
+/* arm64 */
+#elif defined(__aarch64__)
+	#define XNS_ARM64
 #endif
+
+
+#endif // OS_HEADER
