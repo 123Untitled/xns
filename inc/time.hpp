c34bb3bbc1966fe57c1e78b4e0bbb884783e8d36
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-09-06 01:33:54 +0200

    add size_type

--- inc/time.hpp
+++ inc/time.hpp
@@ -33,6 +33,9 @@ namespace xns {
 			/* self type */
 			using self = xns::time;
 
+			/* size type */
+			using size_type = xns::size_t;
+
 
 			// -- public static methods ---------------------------------------
 
