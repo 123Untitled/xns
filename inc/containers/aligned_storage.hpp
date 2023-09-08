fc16dd067cbf8a83e18942a7f492792be4d73a90
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-09-08 02:58:14 +0200

    implement non-member functions for safe cast to any ref/ptr type

--- inc/containers/aligned_storage.hpp
+++ inc/containers/aligned_storage.hpp
@@ -10,10 +10,16 @@
 namespace xns {
 
 
-	// -- A L I G N E D  S T O R A G E  S T R U C T ---------------------------
+	// -- A L I G N E D  S T O R A G E ----------------------------------------
 
 	template <xns::size_t L, decltype(L) A>
 	struct aligned_storage final {
+
+
+		// -- public types ----------------------------------------------------
+
+		/* self type */
+		using self = xns::aligned_storage<L, A>;
 
 
 		// -- public members --------------------------------------------------
@@ -23,45 +29,51 @@ namespace xns {
 
 		// -- public accessors ------------------------------------------------
 
-		/* data */
-		inline constexpr auto data(void) noexcept -> xns::ubyte* {
-			return _data;
-		}
-
 		/* size */
 		inline consteval auto size(void) const noexcept -> decltype(L) {
 			return L;
 		}
 
-
-		// -- public conversion -----------------------------------------------
-
-		/* cast to type reference */
-		template <class T>
-		inline constexpr auto to_reference(void) noexcept -> T& {
-			return *static_cast<T*>(static_cast<void*>(_data));
-		}
-
-		/* cast to const type reference */
-		template <class T>
-		inline constexpr auto to_reference(void) const noexcept -> const T& {
-			return *static_cast<const T*>(static_cast<const void*>(_data));
-		}
-
-		/* cast to type pointer */
-		template <class T>
-		inline constexpr auto to_pointer(void) noexcept -> T* {
-			return static_cast<T*>(static_cast<void*>(_data));
-		}
-
-		/* cast to const type pointer */
-		template <class T>
-		inline constexpr auto to_pointer(void) const noexcept -> const T* {
-			return static_cast<const T*>(static_cast<const void*>(_data));
-		}
-
-
 	};
+
+
+	// -- non-member functions ------------------------------------------------
+
+	/* cast to pointer */
+	template <typename T, xns::size_t L, decltype(L) A>
+	inline constexpr auto ptr(xns::aligned_storage<L, A>& storage) noexcept -> T* {
+		// assert that the type is not too big
+		static_assert(sizeof(T) <= L,
+				"): ALIGNED STORAGE: IS TOO SMALL! :(");
+		return static_cast<T*>(static_cast<void*>(storage._data));
+	}
+
+	/* cast to const pointer */
+	template <typename T, xns::size_t L, decltype(L) A>
+	inline constexpr auto ptr(const xns::aligned_storage<L, A>& storage) noexcept -> const T* {
+		// assert that the type is not too big
+		static_assert(sizeof(T) <= L,
+				"): ALIGNED STORAGE: IS TOO SMALL! :(");
+		return static_cast<const T*>(static_cast<const void*>(storage._data));
+	}
+
+	/* cast to reference */
+	template <typename T, xns::size_t L, decltype(L) A>
+	inline constexpr auto ref(xns::aligned_storage<L, A>& storage) noexcept -> T& {
+		// assert that the type is not too big
+		static_assert(sizeof(T) <= L,
+				"): ALIGNED STORAGE: IS TOO SMALL! :(");
+		return *(static_cast<T*>(static_cast<void*>(storage._data)));
+	}
+
+	/* cast to const reference */
+	template <typename T, xns::size_t L, decltype(L) A>
+	inline constexpr auto ref(const xns::aligned_storage<L, A>& storage) noexcept -> const T& {
+		// assert that the type is not too big
+		static_assert(sizeof(T) <= L,
+				"): ALIGNED STORAGE: IS TOO SMALL! :(");
+		return *(static_cast<const T*>(static_cast<const void*>(storage._data)));
+	}
 
 
 }
