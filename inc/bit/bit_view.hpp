85a5302b2e5908e82bb0a878db39c1b2c4ea718a
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-09-10 06:40:20 +0200

    rename typedefs and change bit order

--- inc/bit/bit_view.hpp
+++ inc/bit/bit_view.hpp
@@ -18,9 +18,8 @@ namespace xns {
 
 	// -- B I T  V I E W ------------------------------------------------------
 
-	template <class T>
+	template <typename T>
 	class bit_view final {
-
 
 
 		public:
@@ -31,22 +30,13 @@ namespace xns {
 			using self = bit_view<T>;
 
 			/* value type */
-			using value_type = T;
+			using type = T;
+
+			/* reference type */
+			using mut_ref = type&;
 
 			/* storage type */
 			using storage = xns::ubyte*;
-
-			/* pointer type */
-			using mutable_pointer = value_type*;
-
-			/* reference type */
-			using mutable_reference = value_type&;
-
-			/* const pointer type */
-			using const_pointer = const value_type*;
-
-			/* const reference type */
-			using const_reference = const value_type&;
 
 			/* size type */
 			using size_type = xns::size_t;
@@ -57,7 +47,7 @@ namespace xns {
 			// -- private constants -------------------------------------------
 
 			/* number of bits in value type */
-			static constexpr size_type _bits = sizeof(value_type) * XNS_CHAR_BIT;
+			static constexpr size_type _bits = sizeof(type) * XNS_CHAR_BIT;
 
 
 		public:
@@ -68,7 +58,7 @@ namespace xns {
 			constexpr bit_view(void) = delete;
 
 			/* reference constructor */
-			constexpr bit_view(mutable_reference value) noexcept
+			constexpr bit_view(mut_ref value) noexcept
 			: _data{reinterpret_cast<storage>(&value)} {}
 
 			/* copy constructor */
@@ -115,7 +105,7 @@ namespace xns {
 			// -- public accessors --------------------------------------------
 
 			/* get bit count */
-			consteval size_type bits(void) const noexcept {
+			static consteval size_type bits(void) noexcept {
 				// return number of bits
 				return _bits;
 			}
@@ -140,15 +130,20 @@ namespace xns {
 
 			template <xns::size_t N> requires (N > 0)
 			void print_impl(void) const noexcept {
+
+
+				if (get<N>()) {
+					::write(1, "\x1b[34m1\x1b[0m", 10);
+				} else {
+					::write(1, "0", 1);
+				}
+
 				if constexpr ((N % XNS_CHAR_BIT == 0) && (N != 0)) {
 					write(1, " ", 1);
 				}
 
-				int bit = get<N>();
-				if (bit) {
-					write(1, "1", 1);
-				} else {
-					write(1, "0", 1);
+				if constexpr ((N % (XNS_CHAR_BIT * sizeof(void*)) == 0) && (N != 0)) {
+					write(1, "\n", 1);
 				}
 
 				print_impl<N - 1>();
@@ -156,14 +151,10 @@ namespace xns {
 
 			template <xns::size_t N> requires (N == 0)
 			void print_impl(void) const noexcept {
-				int bit = get<0>();
-				if (bit) {
-					write(1, "1", 1);
-				} else {
-					write(1, "0", 1);
-				}
-
-				write(1, "\n", 1);
+				if (get<0>()) {
+					::write(1, "\x1b[34m1\x1b[0m\n", 11);
+					return; }
+				::write(1, "0\n", 2);
 			}
 
 
@@ -179,7 +170,13 @@ namespace xns {
 			/* get byte index (little endian) */
 			template <size_type N> requires (xns::endianness::is_little())
 			static consteval auto byte_index(void) noexcept -> size_type {
-				return sizeof(value_type) - 1 - (N / XNS_CHAR_BIT);
+				// return byte index
+				return sizeof(type) - 1 - (N / XNS_CHAR_BIT);
+			}
+
+			/* get byte index (big endian) */
+			template <size_type N> requires (xns::endianness::is_big())
+			static consteval auto byte_index(void) noexcept -> size_type {
 				// return byte index
 				return N / XNS_CHAR_BIT;
 			}
@@ -187,16 +184,8 @@ namespace xns {
 			/* get bit index (within byte) (little endian) */
 			template <size_type N> requires (xns::endianness::is_little())
 			static consteval auto bit_index(void) noexcept -> size_type {
-				return XNS_CHAR_BIT - 1 - (N % XNS_CHAR_BIT);
 				// return bit index
 				return N % XNS_CHAR_BIT;
-			}
-
-			/* get byte index (big endian) */
-			template <size_type N> requires (xns::endianness::is_big())
-			static consteval auto byte_index(void) noexcept -> size_type {
-				// return byte index
-				return sizeof(value_type) - 1 - (N / XNS_CHAR_BIT);
 			}
 
 			/* get bit index (within byte) (big endian) */
