da65d3ed7576d344397976db21233d006ba24aa9
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-09-10 06:41:40 +0200

    add type_compare

---
+++ inc/type_compare.hpp
@@ -0,0 +1,28 @@
+#ifndef TYPE_COMPARE_HEADER
+#define TYPE_COMPARE_HEADER
+
+// local headers
+#include "conditional.hpp"
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- S M A L L E R -------------------------------------------------------
+
+	/* smaller */
+	template <typename A, typename B>
+	using smaller = xns::conditional<(sizeof(A) < sizeof(B)), A, B>;
+
+
+	// -- B I G G E R ---------------------------------------------------------
+
+	/* larger */
+	template <typename A, typename B>
+	using bigger = xns::conditional<(sizeof(A) > sizeof(B)), A, B>;
+
+}
+
+#endif // TYPE_COMPARE_HEADER
