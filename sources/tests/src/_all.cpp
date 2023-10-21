15f1002882666bab511aeaf8da4914810daf4878
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-10-21 15:23:19 +0200

    minor

--- sources/tests/src/_all.cpp
+++ sources/tests/src/_all.cpp
@@ -1,11 +1,31 @@
+#include "input.hpp"
+#include "integer_sequence.hpp"
+#include "tuple.hpp"
 #include "unit_tests.hpp"
 #include "exceptions.hpp"
+#include "vector.hpp"
+#include <__bit/bit_ceil.h>
+#include <cstddef>
+#include <cstdio>
+#include <functional>
 #include <iostream>
 
 // launch all tests 🚀
+#include <map>
+#include <memory>
+#include <set>
+#include <strings.h>
+#include <tuple>
+#include <unistd.h>
+#include <utility>
+#include <vector>
+#include <string>
+
+
 
 #if defined(XNS_TEST_ALL)
 int main(const int ac, const char** av) {
+
 	// start main function
 	try { UT::launcher(); }
 	// catch memory errors
