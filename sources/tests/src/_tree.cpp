c7118ae13b765d8a4c9da7436915304c1348b6d2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-11-03 01:56:20 +0100

    update tests

--- sources/tests/src/_tree.cpp
+++ sources/tests/src/_tree.cpp
@@ -35,6 +35,64 @@ auto random_vector(const xns::size_t size) -> std::vector<xns::size_t> {
 }
 
 
+static void test_iterators(void) {
+
+	xns::tree<int> tree {};
+
+	for (xns::size_t i = 0; i < 1'000'000; ++i) {
+		tree.insert(xns::random::integral<int>());
+	}
+
+	constexpr xns::size_t N = 10;
+
+	{
+		auto start = xns::time::now();
+		// bfs iterator
+		for (xns::size_t i = 0; i < N; ++i) {
+			auto it = tree.bfs_begin();
+			while (it) { ++it; }
+		}
+		auto end = xns::time::now();
+		std::cout << "       bfs iterator -> " << end - start << std::endl;
+	}
+
+	{
+		auto start = xns::time::now();
+		// pre-order iterator
+		for (xns::size_t i = 0; i < N; ++i) {
+			auto it = tree.pre_order_begin();
+			while (it) { ++it; }
+		}
+		auto end = xns::time::now();
+		std::cout << " pre-order iterator -> " << end - start << std::endl;
+	}
+
+	{
+		auto start = xns::time::now();
+		// in-order iterator
+		for (xns::size_t i = 0; i < N; ++i) {
+			auto it = tree.in_order_begin();
+			while (it) { ++it; }
+		}
+		auto end = xns::time::now();
+		std::cout << "  in-order iterator -> " << end - start << std::endl;
+	}
+
+	{
+		auto start = xns::time::now();
+		// post-order iterator
+		for (xns::size_t i = 0; i < N; ++i) {
+			auto it = tree.post_order_begin();
+			while (it) { ++it; }
+		}
+		auto end = xns::time::now();
+		std::cout << "post-order iterator -> " << end - start << std::endl;
+	}
+
+
+}
+
+
 static void benchmark01(void) {
 
 	using type = xns::size_t;
