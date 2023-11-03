e1228f0c9bc52b13adebd23d9dd97ff1c9be352e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-11-03 01:59:23 +0100

    update

--- includes/other/config.hpp
+++ includes/other/config.hpp
@@ -5,4 +5,16 @@
 #define __has_builtin(__x) 0
 #endif
 
+// check if __always_inline__
+#if not defined(XNS_FORCE_INLINE)
+	#if (defined(__GNUC__) && __GNUC__ >= 4) || defined(__clang__)
+		#define XNS_FORCE_INLINE __attribute__((__always_inline__))
+	#elif defined(_MSC_VER)
+		#define XNS_FORCE_INLINE __forceinline
+	#else
+		#define XNS_FORCE_INLINE inline
+	#endif
+#endif
+
+
 #endif // CONFIG_HEADER
