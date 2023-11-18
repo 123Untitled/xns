b00435c693a97b172f83e23a61f746f634f8b383
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-11-18 16:45:06 +0100

    add basic functions

--- includes/math/math.hpp
+++ includes/math/math.hpp
@@ -9,6 +9,8 @@
 #include "is_comparable.hpp"
 
 
+
+
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
 namespace xns {
@@ -29,7 +31,7 @@ namespace xns {
 	template <typename T>
 	inline auto diff(const T& a, const T& b) -> T {
 		// assert that T and U are comparable types
-		static_assert(xns::is_scalar<T>, "): DIFF: TYPES MUST BE ARITHMETIC :(");
+		static_assert(xns::is_scalar<T>, "): DIFF: types must be scalar :(");
 		// return difference
 		return a > b ? a - b : b - a;
 	}
@@ -37,11 +39,11 @@ namespace xns {
 
 	// -- A B S ---------------------------------------------------------------
 
-	template <class T>
-	constexpr T abs(const T& value) {
+	template <typename T>
+	constexpr auto abs(const T& value) noexcept -> T {
 
 		// assert that T is an scalar type
-		static_assert(xns::is_scalar<T>, "): T MUST BE SCALAR :(");
+		static_assert(xns::is_scalar<T>, "): ABS: types must be scalar :(");
 
 		// check T is floating point
 		if constexpr (xns::is_floating<T>) {
@@ -50,18 +52,15 @@ namespace xns {
 		}
 		// check T is signed
 		if constexpr (xns::is_signed<T>) {
-			// check value is negative
-			if (value < 0) {
-				// check for overflow
-				if (value == xns::limits::min<T>()) {
-					// return max value of T
-					return xns::limits::max<T>();
-				}
-				// return absolute value
-				return -value;
-			}
+			// return absolute value
+			return value < 0 ? value == xns::limits::min<T>()
+									  ? xns::limits::max<T>()
+									  : -value
+									  :  value;
 		}
-		return value;
+		else {
+			return value;
+		}
 
 	}
 
@@ -71,27 +70,29 @@ namespace xns {
 	template <typename T>
 	inline constexpr auto max(const T& a, const T& b) -> const T& {
 		// assert that T and U are comparable types
-		static_assert(xns::is_comparable<T>, "): MAX: TYPES MUST BE COMPARABLE :(");
+		static_assert(xns::is_comparable<T>, "): MAX: types must be comparable :(");
 		// return max value
 		return a > b ? a : b;
 	}
+
 
 	// -- M I N ---------------------------------------------------------------
 
 	template <typename T>
 	inline constexpr auto min(const T& a, const T& b) -> const T& {
 		// assert that T and U are comparable types
-		static_assert(xns::is_comparable<T>, "): MIN: TYPES MUST BE COMPARABLE :(");
+		static_assert(xns::is_comparable<T>, "): MIN: types must be comparable :(");
 		// return min value
 		return a < b ? a : b;
 	}
+
 
 	// -- C L A M P -----------------------------------------------------------
 
 	template <typename T>
 	inline constexpr auto clamp(const T& value, const T& min, const T& max) -> T {
 		// assert that T and U are comparable types
-		static_assert(xns::is_comparable<T>, "): CLAMP: TYPES MUST BE COMPARABLE :(");
+		static_assert(xns::is_comparable<T>, "): CLAMP: types must be comparable :(");
 		// return clamped value
 		return value < min ? min : value > max ? max : value;
 	}
@@ -99,12 +100,10 @@ namespace xns {
 
 	// -- C E I L -------------------------------------------------------------
 
-	template <class T>
-	constexpr T ceil(const T& value) {
-
+	template <typename T>
+	constexpr auto ceil(const T& value) noexcept -> T {
 		// assert that T is an floating point type
-		static_assert(xns::is_floating<T>, "): T MUST BE FLOATING POINT :(");
-
+		static_assert(xns::is_floating<T>, "): CEIL: T must be a floating point type :(");
 		// check sign
 		return value > 0 ? static_cast<T>(static_cast<xns::umax>(value)) + 1
 						 : static_cast<T>(static_cast<xns::smax>(value));
@@ -113,18 +112,80 @@ namespace xns {
 
 	// -- F L O O R -----------------------------------------------------------
 
-	template <class T>
-	constexpr T floor(const T& value) {
-
+	template <typename T>
+	constexpr auto floor(const T& value) noexcept -> T {
 		// assert that T is an floating point type
-		static_assert(xns::is_floating<T>, "): T MUST BE FLOATING POINT :(");
-
+		static_assert(xns::is_floating<T>, "): FLOOR: T must be a floating point type :(");
 		// check sign
 		return value > 0 ? static_cast<T>(static_cast<xns::umax>(value))
 						 : static_cast<T>(static_cast<xns::smax>(value)) - 1;
 	}
 
 
+	// -- T O  R A D I A N S --------------------------------------------------
+
+	/* convert degrees to radians */
+	template <typename T>
+	auto to_radians(const T& degrees) noexcept -> T {
+		return degrees * 0.01745329251994329576923690768489;
+	}
+
+	/* convert radians to degrees */
+	template <typename T>
+	auto to_degrees(const T& radians) noexcept -> T {
+		return radians * 57.295779513082320876798154814105;
+	}
+
+
+	template <typename T>
+	auto sqrt(const T& value) noexcept -> T {
+
+		if (value <= 0) return 0;
+		double tol = 1e-6;  // Tolérance pour la précision de la racine carrée
+		double guess = value;
+		double diff = 1;
+
+		while (diff > tol) {
+			double newGuess = (guess + value / guess) / 2;
+			diff = abs(newGuess - guess);
+			guess = newGuess;
+		}
+		return guess;
+	}
+
+	template <typename T, typename E>
+	auto pow(const T& value, const E& exponent) noexcept -> T {
+		static_assert(xns::is_arithmetic<T>, "): POW: T must be an arithmetic type :(");
+		static_assert(xns::is_integral<E>,   "): POW: E must be an integral type :(");
+		if (exponent == 0) return 1;
+		if (exponent == 1) return value;
+		T result = value;
+		for (T i = 1; i < exponent; ++i)
+			result *= value;
+		return result;
+	}
+
+	template <typename T>
+	auto cos(const T& value) noexcept -> T {
+		T x = value;
+		x *= 0.15915494309189535;
+		x -= 0.25  +  xns::floor(x + 0.25);
+		x *= 16.0  * (xns::abs(x) - 0.5);
+//#if EXTENDED_PRECISION
+		x += 0.225 * x * (xns::abs(x) - 1.0);
+//#endif
+		return x;
+	}
+
+	template <typename T>
+	auto sin(const T& value) noexcept -> T {
+		return xns::cos(value - 1.5707963267948966);
+	}
+
+	template <typename T>
+	auto tan(const T& value) noexcept -> T {
+		return xns::sin(value) / xns::cos(value);
+	}
 
 }
 
