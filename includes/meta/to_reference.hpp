3022d1dbc3e17e956a781ee0970bad5dfc88dca2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-11-29 19:59:09 +0100

    minor changes

--- includes/meta/to_reference.hpp
+++ includes/meta/to_reference.hpp
@@ -11,18 +11,16 @@ namespace xns {
 	// -- T O  L V A L U E  R E F E R E N C E ---------------------------------
 
 	/* forward lvalue helper */
-	template <class T>
+	template <typename T>
 	using to_lvalue = xns::remove_reference<T>&;
 
 
 	// -- T O  R V A L U E  R E F E R E N C E ---------------------------------
 
 	/* forward rvalue helper */
-	template <class T>
+	template <typename T>
 	using to_rvalue = xns::remove_reference<T>&&;
-
-
 
 }
 
-#endif
+#endif // XNS_TO_REFERENCE_HEADER
