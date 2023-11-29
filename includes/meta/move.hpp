4ce56c742eea3fdde7a5014d6ba00b86898ca23c
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-11-29 19:59:24 +0100

    add assertion to const values

--- includes/meta/move.hpp
+++ includes/meta/move.hpp
@@ -2,17 +2,21 @@
 #define XNS_MOVE_HEADER
 
 #include "to_reference.hpp"
+#include "is_const.hpp"
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
 namespace xns {
 
+
 	// -- M O V E -------------------------------------------------------------
 
 	/* move */
 	template <typename T>
-	inline constexpr xns::to_rvalue<T> move(T&& obj) noexcept {
+	inline constexpr auto move(T&& obj) noexcept -> xns::to_rvalue<T> {
+		// assert that T is not const (not following the standard)
+		static_assert(not xns::is_const<T>, "): MOVE: cannot move const object :(");
 		// remove reference from T and return as rvalue reference
 		return static_cast<xns::to_rvalue<T>>(obj);
 	}
