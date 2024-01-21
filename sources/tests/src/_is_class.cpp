c527e1244b302beacc0db0bd59510fb93851db45
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-21 05:38:39 +0100

    new tests

--- sources/tests/src/_is_class.cpp
+++ sources/tests/src/_is_class.cpp
@@ -1,9 +1,41 @@
 #include "unit_tests.hpp"
 #include "is_class.hpp"
 
+class test_class {
+	int u;
+};
+
+struct test_struct {
+	float f;
+};
+
+union test_union {
+	int i;
+	float f;
+};
+
+void test_function(void) {
+}
+
+enum test_enum {
+	one,
+	two,
+	three
+};
+
 /* unit test */
 template <>
 int UT::unit_tests<"is_class">(void) {
+
+	std::cout << "is_class [int]: " << xns::is_class<int> << std::endl;
+	std::cout << "is_class [test_class]: " << xns::is_class<test_class> << std::endl;
+	std::cout << "is_class [test_struct]: " << xns::is_class<test_struct> << std::endl;
+	std::cout << "is_class [test_union]: " << xns::is_class<test_union> << std::endl;
+	std::cout << "is_class [test_enum]: " << xns::is_class<test_enum> << std::endl;
+	std::cout << "is_class [void(*)()]: " << xns::is_class<decltype(test_function)> << std::endl;
+	std::cout << "is_class [void]: " << xns::is_class<void> << std::endl;
+	std::cout << "is_class [void*]: " << xns::is_class<void*> << std::endl;
+
 	return 0;
 }
 
--- sources/tests/src/_is_union.cpp
+++ sources/tests/src/_is_union.cpp
@@ -1,9 +1,50 @@
 #include "unit_tests.hpp"
 #include "is_union.hpp"
 
+class test_class {
+	union {
+		int u;
+		float f;
+	};
+};
+
+union test_union {
+	int i;
+	float f;
+
+};
+
+void test_function(void) {
+}
+
+enum test_enum {
+	one,
+	two,
+	three
+};
+
 /* unit test */
 template <>
 int UT::unit_tests<"is_union">(void) {
+
+	// is_union
+
+	int i = 0;
+	test_class c;
+	test_union u;
+
+	auto func = test_function;
+
+	std::cout << "is_union [int]: " << xns::is_union<int> << std::endl;
+	std::cout << "is_union [test_class]: " << xns::is_union<test_class> << std::endl;
+	std::cout << "is_union [test_union]: " << xns::is_union<test_union> << std::endl;
+	std::cout << "is_union [test_enum]: " << xns::is_union<test_enum> << std::endl;
+	std::cout << "is_union [void(*)()]: " << xns::is_union<decltype(func)> << std::endl;
+
+
+
+
+
 	return 0;
 }
 
