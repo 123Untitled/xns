fbab570136d696ebb9eae4b9f93e9f7d9647d649
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-22 14:46:01 +0100

    update tests

--- sources/tests/src/_shared_ptr.cpp
+++ sources/tests/src/_shared_ptr.cpp
@@ -1,10 +1,46 @@
 #include "unit_tests.hpp"
 #include "shared_ptr.hpp"
 
+#include <memory>
+#include <iostream>
+
+class _B; // Déclaration anticipée de la classe B
+
+class _A {
+public:
+    std::shared_ptr<_B> b_ptr;
+    ~_A() { std::cout << "A destructeur\n"; }
+};
+
+class _B {
+public:
+    std::shared_ptr<_A> a_ptr;
+    ~_B() { std::cout << "B destructeur\n"; }
+};
+
+
+
 /* unit test */
 template <>
 int UT::unit_tests<"shared_ptr">(void) {
-	// code here...
+
+    auto a = std::make_shared<_A>();
+    auto b = std::make_shared<_B>();
+
+    a->b_ptr = b;
+    b->a_ptr = a;
+
+
+	//xns::shared_ptr<int> a;
+	//xns::shared_ptr<int> b;
+	//{
+	//	xns::shared_ptr<int> c = xns::make_shared<int>(5);
+	//	b = c;
+	//	a = b;
+	//}
+
+
+
 	return 0;
 }
 
