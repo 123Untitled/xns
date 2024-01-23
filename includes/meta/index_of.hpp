60d16642958075dc16602944757ec484ece9913e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-23 03:40:10 +0100

    minor changes

--- includes/meta/index_of.hpp
+++ includes/meta/index_of.hpp
@@ -13,10 +13,10 @@ namespace xns {
 	// -- I N D E X  O F ------------------------------------------------------
 
 	/* index of */
-	template <class T, class... A>
-	consteval xns::size_t index_of(void) {
+	template <typename T, typename... A>
+	consteval auto index_of(void) noexcept -> xns::size_t {
 
-		static_assert(xns::is_one_of<T, A...>, "): INDEX_OF: TYPE NOT IN PACK :(");
+		static_assert(xns::is_one_of<T, A...>, "(xns::index_of) T is not in parameter pack.");
 
 		xns::size_t idx = 0;
 		bool found = false;
