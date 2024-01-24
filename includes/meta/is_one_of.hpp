ef450c3f644e5bfbe9c0b89b32a59b964038c0f6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-24 23:24:12 +0100

    simplify is_one_of

--- includes/meta/is_one_of.hpp
+++ includes/meta/is_one_of.hpp
@@ -1,7 +1,6 @@
-#ifndef XNS_HAS_TYPE_HEADER
-#define XNS_HAS_TYPE_HEADER
+#ifndef XNS_IS_ONE_OF_HPP
+#define XNS_IS_ONE_OF_HPP
 
-#include "integral_constant.hpp"
 #include "is_same.hpp"
 
 
@@ -9,24 +8,12 @@
 
 namespace xns {
 
-
-	// -- D E T A I L ---------------------------------------------------------
-
-	namespace impl {
-
-		template <class T, class... A>
-		struct is_one_of : public xns::bool_constant<(xns::is_same<T, A> || ...)> {};
-
-	}
-
-
 	// -- I S  O N E  O F -----------------------------------------------------
 
-	/* has type concept */
-	template <class T, class... A>
-	concept is_one_of = impl::is_one_of<T, A...>::value;
+	/* is one of concept */
+	template <typename T, typename... A>
+	concept is_one_of = (xns::is_same<T, A> || ...);
 
 }
 
-
-#endif
+#endif // XNS_IS_ONE_OF_HPP
