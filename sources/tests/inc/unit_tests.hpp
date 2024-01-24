64f0f861a3b8aa28ea0a6510fae1a2e5f3dc947b
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-24 23:25:27 +0100

    add test function

--- sources/tests/inc/unit_tests.hpp
+++ sources/tests/inc/unit_tests.hpp
@@ -12,6 +12,26 @@
 //#include "color.hpp"
 //#include "escape.hpp"
 
+namespace ut {
+
+	template <bool expected, bool result>
+	auto test(const char* msg) -> int {
+		constexpr const char* symbol[]{
+			"[\x1b[33mv\x1b[0m] ",
+			"[\x1b[31mx\x1b[0m] " };
+		// valid test
+		if constexpr (result == expected) {
+			std::cout << symbol[0] << msg << std::endl;
+			return 0;
+		}
+		// invalid test
+		else {
+			std::cout << symbol[1] << msg <<
+				" -> (expected: " << expected << ", result: " << result << ")" << std::endl;
+			return -1;
+		}
+	}
+}
 
 namespace UT {
 
