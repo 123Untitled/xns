ab1476c4a07a141abc2d353926a52b9af12a59f5
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-24 23:23:24 +0100

    add cpp version and endian macros

--- includes/other/config.hpp
+++ includes/other/config.hpp
@@ -5,6 +5,23 @@
 #define __has_builtin(__x) 0
 #endif
 
+
+#ifndef XNS_CPP_VERSION
+#if   __cplusplus <= 201103L
+#define XNS_CPP_VERSION 11
+#elif __cplusplus <= 201402L
+#define XNS_CPP_VERSION 14
+#elif __cplusplus <= 201703L
+#define XNS_CPP_VERSION 17
+#elif __cplusplus <= 202002L
+#define XNS_CPP_VERSION 20
+#elif __cplusplus <= 202302L
+#define XNS_CPP_VERSION 23
+#else
+#define _LIBCPP_STD_VER 26
+#endif
+#endif // XNS_CPP_VERSION
+
 // check if __always_inline__
 #if not defined(XNS_FORCE_INLINE)
 	#if (defined(__GNUC__) && __GNUC__ >= 4) || defined(__clang__)
@@ -15,6 +32,33 @@
 		#define XNS_FORCE_INLINE inline
 	#endif
 #endif
+
+
+#ifdef __LITTLE_ENDIAN__
+#	if __LITTLE_ENDIAN__
+#		define XNS_LITTLE_ENDIAN
+#	endif
+#endif // __LITTLE_ENDIAN__
+
+#ifdef __BIG_ENDIAN__
+#	if __BIG_ENDIAN__
+#		define XNS_BIG_ENDIAN
+#	endif
+#endif // __BIG_ENDIAN__
+
+#ifdef __BYTE_ORDER__
+#	if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#		define XNS_LITTLE_ENDIAN
+#	elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#		define XNS_BIG_ENDIAN
+#	endif
+#endif // __BYTE_ORDER__
+
+#if !defined(XNS_LITTLE_ENDIAN) && !defined(XNS_BIG_ENDIAN)
+#	error "unknown endian"
+	static_assert(false, "unknown endian");
+#endif
+
 
 
 #endif // CONFIG_HEADER
