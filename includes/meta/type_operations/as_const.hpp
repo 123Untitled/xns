704ea379781e48735edb4fa11575512218b82f90
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-24 23:15:43 +0100

    add as_const

---
+++ includes/meta/type_operations/as_const.hpp
@@ -0,0 +1,25 @@
+#ifndef XNS_AS_CONST_HPP
+#define XNS_AS_CONST_HPP
+
+#include "add_const.hpp"
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- A S  C O N S T -------------------------------------------------------
+
+	/* as const */
+	template <typename T>
+	inline constexpr auto as_const(T& t) noexcept -> xns::add_const<T>& {
+		return t;
+	}
+
+	/* as const */
+	template <typename T>
+	void as_const(const T&&) = delete;
+
+} // namespace xns
+
+#endif // XNS_AS_CONST_HPP
