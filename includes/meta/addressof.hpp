e308d3f8888ac9506bedd68337127f00c91f9fd5
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-29 12:09:10 +0100

    remove implementation and use compiler builtin

--- includes/meta/addressof.hpp
+++ includes/meta/addressof.hpp
@@ -1,10 +1,17 @@
-#ifndef XNS_ADDRESSOF_HEADER
-#define XNS_ADDRESSOF_HEADER
+/*****************************************************************************/
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*****************************************************************************/
 
-#include "is_class.hpp"
-#include "is_union.hpp"
-#include "is_array.hpp"
-#include "is_scalar.hpp"
+#pragma once
+
+#ifndef XNS_ADDRESSOF_HPP
+#define XNS_ADDRESSOF_HPP
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
@@ -13,35 +20,12 @@ namespace xns {
 
 	// -- A D D R E S S O F ---------------------------------------------------
 
-
-	// not finished !!!
-
-	/* addressof */
-	template <typename T> requires (xns::is_class<T> == true || xns::is_union<T> == true)
-	inline constexpr auto addressof(T& value) noexcept {
-		// cast to char& to get address
-		return reinterpret_cast<T*>(&(reinterpret_cast<char&>(value)));
-	}
-
-	/* addressof */
-	template <typename T> requires (xns::is_array<T> == true)
-	inline constexpr auto addressof(T& value) noexcept {
-		// just return address of first element
-		return &(value[0]);
-	}
-
-	/* addressof */
-	template <typename T> requires (xns::is_scalar<T> == true)
-	inline constexpr auto addressof(T& value) noexcept {
-		// just return address
-		return &value;
-	}
-
-
-	/* deleted const addressof */
+	/* address of */
 	template <typename T>
-	auto addressof(const T&&) = delete;
+	inline constexpr auto addressof(T& value) noexcept -> T* {
+		return __builtin_addressof(value);
+	}
 
-}
+} // namespace xns
 
-#endif
+#endif // XNS_ADDRESSOF_HPP
