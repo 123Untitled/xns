b95c3f659752f18c22f952e9cfb59d9ccda02473
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-29 12:08:39 +0100

    clean is_member_pointer

--- includes/meta/type_categories/is_member_pointer.hpp
+++ includes/meta/type_categories/is_member_pointer.hpp
@@ -1,7 +1,18 @@
-#ifndef XNS_IS_MEMBER_POINTER_HEADER
-#define XNS_IS_MEMBER_POINTER_HEADER
+/*****************************************************************************/
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*****************************************************************************/
 
-#include "integral_constant.hpp"
+#pragma once
+
+#ifndef XNS_IS_MEMBER_POINTER_HPP
+#define XNS_IS_MEMBER_POINTER_HPP
+
 #include "remove.hpp"
 
 
@@ -10,35 +21,27 @@
 namespace xns {
 
 
-	// -- D E T A I L ---------------------------------------------------------
+	// -- detail --------------------------------------------------------------
 
 	namespace impl {
 
 		/* is member pointer */
-		template <class T>
-		struct is_member_pointer         : public xns::no  {};
+		template <typename T>
+		constexpr bool is_member_pointer = false;
 
 		/* specialisation */
-		template <class T, class U>
-		struct is_member_pointer<T U::*> : public xns::yes {};
+		template <typename T, typename U>
+		constexpr bool is_member_pointer<T U::*> = true;
 
-		/* helper */
-		template<class T>
-		struct is_member_pointer_helper  : public is_member_pointer<xns::remove_cv<T>> {};
-
-	}
+	} // namespace impl
 
 
 	// -- I S  M E M B E R  P O I N T E R -------------------------------------
 
 	/* is member pointer concept */
 	template<class T>
-	concept is_member_pointer = impl::is_member_pointer_helper<T>::value;
+	concept is_member_pointer = impl::is_member_pointer<xns::remove_cv<T>>;
 
+} // namespace xns
 
-
-}
-
-
-
-#endif
+#endif // XNS_IS_MEMBER_POINTER_HPP
