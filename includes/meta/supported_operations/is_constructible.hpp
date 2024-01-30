6a033ac5da9d04da2cd853e3b0103349470b3a05
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-01-30 23:19:38 +0100

    move into supported_operations directory

--- includes/meta/supported_operations/is_constructible.hpp
+++ includes/meta/supported_operations/is_constructible.hpp
@@ -1,10 +1,19 @@
-#ifndef XNS_IS_CONSTRUCTIBLE_HEADER
-#define XNS_IS_CONSTRUCTIBLE_HEADER
+/*****************************************************************************/
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*****************************************************************************/
 
-// local headers
-#include "move.hpp"
-#include "forward.hpp"
-#include "is_convertible.hpp"
+#pragma once
+
+#ifndef XNS_IS_CONSTRUCTIBLE_HPP
+#define XNS_IS_CONSTRUCTIBLE_HPP
+
+#include "declval.hpp"
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
@@ -26,56 +35,9 @@ namespace xns {
 		T{xns::declval<A>()...};
 	};
 
-	/* is copyable concept */
-	template <class T>
-	concept is_copy_constructible = xns::is_constructible<T, const T&>;
-
-	/* is moveable concept */
-	template <class T>
-	concept is_move_constructible = xns::is_constructible<T, T&&>;
-
-	/* is default constructible concept */
-	template <class T>
-	concept is_default_constructible = xns::is_constructible<T>;
-
-	/* is assignable concept */
-	template <class T, class U>
-	concept is_assignable = requires(T t) {
-		{ t = xns::declval<U>() } -> xns::is_convertible<T&>;
-	};
-
-	/* is copy assignable concept */
-	template <class T>
-	concept is_copy_assignable = xns::is_assignable<T, const T&>;
-
-	/* is move assignable concept */
-	template <class T>
-	concept is_move_assignable = xns::is_assignable<T, T&&>;
 
 
 
+} // namespace xns
 
-}
-
-#endif // IS_CONSTRUCTIBLE_HEADER
-
-
-
-
-// is_nothrow_constructible
-// is_nothrow_default_constructible
-// is_nothrow_copy_constructible
-// is_nothrow_move_constructible
-// is_nothrow_assignable
-// is_nothrow_copy_assignable
-// is_nothrow_move_assignable
-// is_nothrow_destructible
-
-// is_trivially_copy_assignable
-// is_trivially_assignable
-// is_trivially_move_constructible
-// is_trivially_copy_constructible
-// is_trivially_default_constructible
-// is_trivially_move_assignable
-// is_trivially_constructible
-// is_trivially_destructible
+#endif // IS_CONSTRUCTIBLE_HPP
--- includes/meta/supported_operations/is_destructible.hpp
+++ includes/meta/supported_operations/is_destructible.hpp
@@ -1,9 +1,17 @@
-#ifndef XNS_IS_DESTRUCTIBLE_HEADER
-#define XNS_IS_DESTRUCTIBLE_HEADER
+/*****************************************************************************/
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*****************************************************************************/
 
-// local headers
-#include "config.hpp"
-#include "declval.hpp"
+#pragma once
+
+#ifndef XNS_IS_DESTRUCTIBLE_HPP
+#define XNS_IS_DESTRUCTIBLE_HPP
 
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
@@ -13,11 +21,90 @@ namespace xns {
 
 	// -- I S  D E S T R U C T I B L E ----------------------------------------
 
+	/* is destructible */
 	template <typename T>
-	concept is_destructible = requires {
-		{ xns::declval<T&>().~T() };
-	};
+	concept is_destructible = __is_destructible(T);
 
-}
+	/* are destructible */
+	template <typename... T>
+	concept are_destructible = (xns::is_destructible<T> && ...);
 
-#endif // IS_DESTRUCTIBLE_HEADER
+} // namespace xns
+
+#endif // XNS_IS_DESTRUCTIBLE_HPP
+
+
+
+
+/* my implementation of is_destructible */
+/*template <typename T>
+concept is_destructible = requires {
+	{ xns::declval<T&>().~T() };
+};*/
+
+
+/* stdlib implementation of is_destructible */
+
+
+//  if it's a reference, return true
+//  if it's a function, return false
+//  if it's   void,     return false
+//  if it's an array of unknown bound, return false
+//  Otherwise, return "declval<_Up&>().~_Up()" is well-formed
+//    where _Up is remove_all_extents<_Tp>::type
+
+//template <class>
+//struct __is_destructible_apply {
+//  typedef int type;
+//};
+//
+//template <typename _Tp>
+//struct __is_destructor_wellformed {
+//  template <typename _Tp1>
+//  static true_type __test(typename __is_destructible_apply<decltype(std::declval<_Tp1&>().~_Tp1())>::type);
+//
+//  template <typename _Tp1>
+//  static false_type __test(...);
+//
+//  static const bool value = decltype(__test<_Tp>(12))::value;
+//};
+//
+//template <class _Tp, bool>
+//struct __destructible_imp;
+//
+//template <class _Tp>
+//struct __destructible_imp<_Tp, false>
+//    : public integral_constant<bool, __is_destructor_wellformed<__remove_all_extents_t<_Tp> >::value> {};
+//
+//template <class _Tp>
+//struct __destructible_imp<_Tp, true> : public true_type {};
+//
+//template <class _Tp, bool>
+//struct __destructible_false;
+//
+//template <class _Tp>
+//struct __destructible_false<_Tp, false> : public __destructible_imp<_Tp, is_reference<_Tp>::value> {};
+//
+//template <class _Tp>
+//struct __destructible_false<_Tp, true> : public false_type {};
+//
+//template <class _Tp>
+//struct is_destructible : public __destructible_false<_Tp, is_function<_Tp>::value> {};
+//
+//template <class _Tp>
+//struct is_destructible<_Tp[]> : public false_type {};
+//
+//template <>
+//struct is_destructible<void> : public false_type {};
+//
+//#  if _LIBCPP_STD_VER >= 17
+//template <class _Tp>
+//inline constexpr bool is_destructible_v = is_destructible<_Tp>::value;
+//#  endif
+//
+//#endif // __has_builtin(__is_destructible)
+//
+//_LIBCPP_END_NAMESPACE_STD
+//
+//#endif // _LIBCPP___TYPE_TRAITS_IS_DESTRUCTIBLE_H
+
