85491840feadbb50f12a1ab43606eae16c6f4043
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-02-08 16:01:47 +0100

    add type_properties.hpp

---
+++ includes/type_traits/type_properties/type_properties.hpp
@@ -0,0 +1,31 @@
+/*****************************************************************************/
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*****************************************************************************/
+
+#pragma once
+
+#ifndef XNS_TYPE_PROPERTIES_HPP
+#define XNS_TYPE_PROPERTIES_HPP
+
+
+// -- T Y P E  P R O P E R T I E S --------------------------------------------
+
+#include "is_const.hpp"
+#include "is_empty.hpp"
+#include "is_empty_base.hpp"
+#include "is_final.hpp"
+#include "is_referenceable.hpp"
+#include "is_signed.hpp"
+#include "is_trivial.hpp"
+#include "is_trivially_copyable.hpp"
+#include "is_unsigned.hpp"
+#include "is_volatile.hpp"
+
+#endif // XNS_TYPE_PROPERTIES_HPP
+
