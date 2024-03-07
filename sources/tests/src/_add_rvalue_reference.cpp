d059bab12dd6acaaa1cb535c1b5c966a7c2d68a9
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-03-07 16:43:04 +0100

    add unit test for add_rvalue_reference

---
+++ sources/tests/src/_add_rvalue_reference.cpp
@@ -0,0 +1,14 @@
+#include "unit_tests.hpp"
+#include "add_rvalue_reference.hpp"
+
+/* unit test */
+int unit_tests_add_rvalue_reference(void) {
+	return 0;
+}
+
+#if defined(XNS_TEST_ADD_RVALUE_REFERENCE)
+int main(void) {
+	return unit_tests_add_rvalue_reference();
+}
+#endif
+
