d660df4cce20eac613132e10f387e27f9295776f
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-03-09 13:16:04 +0100

    add unit test for is_referenceable

---
+++ sources/tests/src/_is_referenceable.cpp
@@ -0,0 +1,14 @@
+#include "unit_tests.hpp"
+#include "is_referenceable.hpp"
+
+/* unit test */
+int unit_tests_is_referenceable(void) {
+	return 0;
+}
+
+#if defined(XNS_TEST_IS_REFERENCEABLE)
+int main(void) {
+	return unit_tests_is_referenceable();
+}
+#endif
+
