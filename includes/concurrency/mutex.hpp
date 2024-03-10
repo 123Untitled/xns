58f5d0a74dea6b8244e1c1bcfb4dba934d4b1d07
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-03-10 07:36:39 +0100

    update

---
+++ includes/concurrency/mutex.hpp
@@ -0,0 +1,87 @@
+#ifndef XNS_MUTEX_HEADER
+#define XNS_MUTEX_HEADER
+
+// local headers
+#include "macros.hpp"
+#include "types.hpp"
+
+// operating system headers
+#include <pthread.h>
+
+
+// -- X N S  N A M E S P A C E ------------------------------------------------
+
+namespace xns {
+
+
+	// -- forward declarations ------------------------------------------------
+
+	/* lock */
+	class guard_lock;
+
+
+	// -- M U T E X -----------------------------------------------------------
+
+	class mutex final {
+
+
+		// -- friends ---------------------------------------------------------
+
+		/* guard_lock as friend */
+		friend class guard_lock;
+
+
+		public:
+
+			// -- forward declarations ----------------------------------------
+
+			/* attr */
+			class attr;
+
+
+			// -- public types ------------------------------------------------
+
+			/* self type */
+			using self = xns::mutex;
+
+
+			// -- public lifecycle --------------------------------------------
+
+			/* default constructor */
+			mutex(void) noexcept;
+
+			/* attribute constructor */
+			mutex(const attr&);
+
+			/* not assignable */
+			XNS_NOT_ASSIGNABLE(mutex);
+
+			/* destructor */
+			~mutex(void) noexcept = default; /* need to check how mutex was initialized */
+
+
+			// -- public classes ----------------------------------------------
+
+			/* attr */
+			class attr final {
+
+				// -- friends ---------------------------------------------
+
+				/* mutex as friend */
+				friend class mutex;
+
+			}; // class attr
+
+
+		private:
+
+			// -- private members ---------------------------------------------
+
+			/* mutex */
+			::pthread_mutex_t _mutex;
+
+	}; // class mutex
+
+} // namespace xns
+
+#endif // XNS_MUTEX_HEADER
---
+++ sources/core/concurrency/mutex.cpp
@@ -0,0 +1,29 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "concurrency/mutex.hpp"
+
+
+// -- public lifecycle --------------------------------------------------------
+
+/* default constructor */
+xns::mutex::mutex(void) noexcept
+: _mutex (PTHREAD_MUTEX_INITIALIZER) {
+}
+
+/* attribute constructor */
+xns::mutex::mutex(const attr& attribute)
+: _mutex{} {
+	// initialize mutex
+	//if (::pthread_mutex_init(&_mutex, &attr._attr) != 0)
+	//	throw;
+}
