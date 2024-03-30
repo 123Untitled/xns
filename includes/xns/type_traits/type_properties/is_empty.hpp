2b4191fb934520129cc44a4dcf756ac2eece40d5
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-03-30 10:37:35 +0100

    minor changes

--- includes/xns/type_traits/type_properties/is_empty.hpp
+++ includes/xns/type_traits/type_properties/is_empty.hpp
@@ -14,7 +14,12 @@
 #define XNS_IS_EMPTY_HEADER
 
 #include "xns/config/config.hpp"
+#include "xns/type_traits/type_transformations/conditional.hpp"
+#include "xns/type_traits/type_trait_constants/integral_constant.hpp"
 
+#if not XNS_HAS_BUILTIN(__is_empty)
+#	error "compiler does not support __is_empty"
+#endif
 
 // -- X N S  N A M E S P A C E ------------------------------------------------
 
@@ -23,15 +28,9 @@ namespace xns {
 
 	// -- I S  E M P T Y ------------------------------------------------------
 
-#if XNS_HAS_BUILTIN(__is_empty)
-
 	/* is empty */
-	template <typename T>
-	concept is_empty = __is_empty(T);
-
-#else
-#	error "compiler does not support __is_empty"
-#endif
+	template <typename ___type>
+	concept is_empty = __is_empty(___type);
 
 } // namespace xns
 
