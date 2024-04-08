d280bf5d2d053a7e8449fbbbe0d6ae8c0270745c
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-04-08 18:30:19 +0200

    split min/max methods for different types

--- includes/xns/utility/numeric_limits.hpp
+++ includes/xns/utility/numeric_limits.hpp
@@ -20,6 +20,8 @@
 #include "xns/type_traits/types.hpp"
 
 #include "xns/type_traits/type_categories/is_integral.hpp"
+#include "xns/type_traits/type_categories/is_floating_point.hpp"
+#include "xns/type_traits/type_categories/is_bool.hpp"
 #include "xns/type_traits/type_categories/is_scalar.hpp"
 #include "xns/type_traits/type_properties/is_signed.hpp"
 #include "xns/type_traits/type_properties/is_unsigned.hpp"
@@ -32,14 +34,14 @@
 namespace xns {
 
 
-	template <typename T>
+	template <typename ___type>
 	class limits final {
 
 
 		// -- assertions ------------------------------------------------------
 
-		/* assert that T is an scalar type */
-		static_assert(xns::is_scalar<T>, "limits: T must be an integral type");
+		/* assert that ___type is an scalar type */
+		static_assert(xns::is_scalar<___type>, "limits: ___type must be an integral type");
 
 
 		public:
@@ -47,72 +49,79 @@ namespace xns {
 			// -- public types ------------------------------------------------
 
 			/* self type */
-			using self = xns::limits<T>;
+			using self = xns::limits<___type>;
 
 
 			// -- public static methods ---------------------------------------
 
-			/* max */
-			static consteval auto max(void) -> T {
-
-				if constexpr (xns::is_same<T, bool>)
-					return true;
-
-				else if constexpr (xns::is_floating_point<T>) {
-
-					if constexpr (sizeof(T) == 4)
-						// return IEEE 754 single precision max
-						return 0x1.fffffep+127f;
-
-					else if constexpr (sizeof(T) == 8)
-						// return IEEE 754 double precision max
-						return 0x1.fffffffffffffp+1023;
-				}
-
-				else {
-
-					// number of bits in type
-					constexpr T bits = static_cast<T>((sizeof(T) * 8) - xns::is_signed<T>);
-
-					T count = 0;
-
-					// loop through bits
-					for (T i = 0; i < bits; ++i)
-						// add shifted bit to max
-						count |= static_cast<T>(static_cast<T>(1) << i);
-
-					return count;
-				}
+			/* max bool */
+			static consteval auto max(void) noexcept -> ___type requires xns::is_bool<___type> {
+				return true;
 			}
 
-			/* min */
-			static consteval auto min(void) -> T {
-
-				if constexpr (xns::is_same<T, bool>)
-					return false;
-
-				else if constexpr (xns::is_floating_point<T>) {
-
-					if constexpr (sizeof(T) == 4)
-						// return IEEE 754 single precision min
-						return -0x1.fffffep+127f;
-
-					else if constexpr (sizeof(T) == 8)
-						// return IEEE 754 double precision min
-						return -0x1.fffffffffffffp+1023;
-				}
-				else {
-					T max = self::max();
-					return ~max;
-				}
+			/* min bool */
+			static consteval auto min(void) noexcept -> ___type requires xns::is_bool<___type> {
+				return false;
 			}
+
+
+			/* max integral */
+			static consteval auto max(void) noexcept -> ___type requires (xns::is_integral<___type>
+																   && not xns::is_bool<___type>) {
+
+				// number of bits in type
+				constexpr ___type bits = static_cast<___type>((sizeof(___type) * xns::bits_per_byte) - xns::is_signed<___type>);
+
+				___type count = 0;
+
+				// loop through bits
+				for (___type i = 0; i < bits; ++i)
+					// add shifted bit to max
+					count |= static_cast<___type>(static_cast<___type>(1) << i);
+
+				return count;
+			}
+
+			/* min integral */
+			static consteval auto min(void) noexcept -> ___type requires (xns::is_integral<___type>
+																   && not xns::is_bool<___type>) {
+				___type max = self::max();
+				return ~max;
+			}
+
+
+			/* max floating point */
+			static consteval auto max(void) noexcept -> ___type requires xns::is_floating_point<___type> {
+
+				if constexpr (sizeof(___type) == 4)
+					// return IEEE 754 single precision max
+					return 0x1.fffffep+127f;
+
+				else if constexpr (sizeof(___type) == 8)
+					// return IEEE 754 double precision max
+					return 0x1.fffffffffffffp+1023;
+			}
+
+			/* min floating point */
+			static consteval auto min(void) noexcept -> ___type requires xns::is_floating_point<___type> {
+
+				if constexpr (sizeof(___type) == 4)
+					// return IEEE 754 single precision min
+					return -0x1.fffffep+127f;
+
+				else if constexpr (sizeof(___type) == 8)
+					// return IEEE 754 double precision min
+					return -0x1.fffffffffffffp+1023;
+			}
+
+
 
 			/* max number of digits */
-			template <xns::is_base B = xns::dec>
+			template <xns::is_base ___base = xns::dec>
 			static consteval auto digits(void) -> decltype(sizeof(0)) {
 
 				// max value of type
-				auto limit = self::max();
+				auto ___lm = self::max();
 
 				// number of digits
 				decltype(sizeof(0)) digits = 0;
@@ -120,7 +129,7 @@ namespace xns {
 				do { // increment digits
 					++digits;
 					// divide max by base
-				} while (limit /= B::base);
+				} while (___lm /= ___base::base);
 
 				return digits;
 			}
@@ -153,18 +162,18 @@ namespace xns {
 
 
 
-			template <typename U>
-			static inline auto in_range(const U& value) -> bool {
+			template <typename ___u>
+			static auto in_range(const ___u& value) noexcept -> bool {
 
 				// check types are integral
-				static_assert(xns::is_integral<U>, "in_range: type must be integral");
+				static_assert(xns::is_integral<___u>, "in_range: type must be integral");
 
 				constexpr auto min = self::min();
 				constexpr auto max = self::max();
 
 
 				// remove: comparison of integers of different signs: warning
-				return (T)value >= (T)min && (T)value <= (T)max;
+				return (___type)value >= (___type)min && (___type)value <= (___type)max;
 			}
 
 
