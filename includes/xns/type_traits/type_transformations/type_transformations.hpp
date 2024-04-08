131e7faa942ff1b5aa5c43dc72ed638745047032
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-04-08 18:33:10 +0200

    add type_transformations index header

---
+++ includes/xns/type_traits/type_transformations/type_transformations.hpp
@@ -0,0 +1,52 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                       :::    ::: ::::    :::  ::::::::                    */
+/*                      :+:    :+: :+:+:   :+: :+:    :+:                    */
+/*                      +:+  +:+  :+:+:+  +:+ +:+                            */
+/*                      +#++:+   +#+ +:+ +#+ +#++:++#++                      */
+/*                    +#+  +#+  +#+  +#+#+#        +#+                       */
+/*                  #+#    #+# #+#   #+#+# #+#    #+#                        */
+/*                 ###    ### ###    ####  ########                          */
+/*                                                                           */
+/*****************************************************************************/
+
+#pragma once
+
+#ifndef XNS_TYPE_TRANSFORMATIONS_HEADER
+#define XNS_TYPE_TRANSFORMATIONS_HEADER
+
+/* decay */
+#include "xns/type_traits/type_transformations/decay.hpp"
+
+/* remove_cvref */
+#include "xns/type_traits/type_transformations/remove_cvref.hpp"
+
+/* enable_if */
+#include "xns/type_traits/type_transformations/enable_if.hpp"
+
+/* void_t */
+#include "xns/type_traits/type_transformations/void_t.hpp"
+
+/* conditional */
+#include "xns/type_traits/type_transformations/conditional.hpp"
+
+/* common_type */
+#include "xns/type_traits/type_transformations/common_type.hpp"
+
+/* common_reference */
+//#include "xns/type_traits/type_transformations/common_reference.hpp"
+
+/* underlying_type */
+//#include "xns/type_traits/type_transformations/underlying_type.hpp"
+
+/* result_of */
+//#include "xns/type_traits/type_transformations/result_of.hpp"
+
+/* invoke_result */
+//#include "xns/type_traits/type_transformations/invoke_result.hpp"
+
+/* type_identity */
+#include "xns/type_traits/type_transformations/type_identity.hpp"
+
+
+#endif // XNS_TYPE_TRANSFORMATIONS_HEADER
