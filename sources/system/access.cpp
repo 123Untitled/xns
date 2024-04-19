ce0c432c964297e8a6600294f9e60f84c928f615
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-04-19 05:19:13 +0200

    update

--- sources/system/access.cpp
+++ sources/system/access.cpp
@@ -1,34 +1,33 @@
 #include "xns/system/access.hpp"
+#include "xns/system/environment.hpp"
+#include "xns/string/string.hpp"
+#include <unistd.h>
 
 
-// -- A C C E S S -------------------------------------------------------------
 
-
-// -- public static methods ---------------------------------------------------
-
-/* check execution access */
-bool xns::access::x_ok(const xns::string& path) noexcept {
+/* x ok */
+auto xns::access::x_ok(const xns::string& path) noexcept -> bool {
 	return ::access(path.data(), X_OK) == 0;
 }
 
-/* check read access */
-bool xns::access::r_ok(const xns::string& path) noexcept {
+/* r ok */
+auto xns::access::r_ok(const xns::string& path) noexcept -> bool {
 	return ::access(path.data(), R_OK) == 0;
 }
 
-/* check write access */
-bool xns::access::w_ok(const xns::string& path) noexcept {
+/* w ok */
+auto xns::access::w_ok(const xns::string& path) noexcept -> bool {
 	return ::access(path.data(), W_OK) == 0;
 }
 
-/* check existence */
-bool xns::access::f_ok(const xns::string& path) noexcept {
+/* f ok */
+auto xns::access::f_ok(const xns::string& path) noexcept -> bool {
 	return ::access(path.data(), F_OK) == 0;
 }
 
 
-/* get execution access */
-bool xns::access::sys_x_ok(xns::string& exec) {
+/* sys x ok */
+auto xns::access::sys_x_ok(xns::string& exec) -> bool {
 
 	auto paths{xns::env::paths()};
 
@@ -54,52 +53,4 @@ bool xns::access::sys_x_ok(xns::string& exec) {
 
 	}
 	return false;
-}
-
-
-// -- public constructors -----------------------------------------------------
-
-/* default constructor */
-xns::access::access(void) noexcept
-//:
-{
-	// code here...
-}
-
-/* copy constructor */
-xns::access::access(const access& other) noexcept
-//:
-{
-	// code here...
-}
-
-/* move constructor */
-xns::access::access(access&& other) noexcept
-//:
-{
-	// code here...
-}
-
-/* destructor */
-xns::access::~access(void) noexcept {
-	// code here...
-}
-
-
-// -- public assignment operators ---------------------------------------------
-
-/* copy assignment operator */
-xns::access& xns::access::operator=(const access& other) noexcept {
-	// check for self assignment
-	if (this != &other) {
-	} // return self reference
-	return *this;
-}
-
-/* move assignment operator */
-xns::access& xns::access::operator=(access&& other) noexcept {
-	// check for self assignment
-	if (this != &other) {
-	} // return self reference
-	return *this;
 }
